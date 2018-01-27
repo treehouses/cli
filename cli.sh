b9cd5488e7464678a73a09316434217ea79275a1
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-01-26 22:51:45 -0300

    treehouses password && sshkeyadd

--- cli.sh
+++ cli.sh
@@ -40,7 +40,6 @@ $PART_START
 p
 w
 EOF
-  ASK_TO_REBOOT=1
 
   # now set up an init.d script
 cat <<\EOF > /etc/init.d/resize2fs_once &&
@@ -72,33 +71,81 @@ EOF
   chmod +x /etc/init.d/resize2fs_once &&
   update-rc.d resize2fs_once defaults &&
   if [ "$INTERACTIVE" = True ]; then
-    echo "Success: Root partition has been resized.\nThe filesystem will be enlarged upon the next reboot"
+    echo -e "Success: Root partition has been resized.\\nThe filesystem will be enlarged upon the next reboot"
     exit 0
   fi
 }
 
 function rename () {
-  CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+  CURRENT_HOSTNAME=$(cat /etc/hostname | tr -d " \t\n\r")
   echo $1 > /etc/hostname
   sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$1/g" /etc/hosts
   hostname $1
+  echo "Success: the hostname has been modified"
+  exit 1
+}
+
+function password () {
+  echo "pi:$1" | chpasswd
+  echo "password change success"
+  exit 1
+}
+
+function sshkeyadd () {
+  mkdir -p /root/.ssh /home/pi/.ssh
+  chmod 700 /root/.ssh /home/pi/.ssh
+
+  echo "$@" >> /home/pi/.ssh/authorized_keys
+  chmod 600 /home/pi/.ssh/authorized_keys
+  chown -R pi:pi /home/pi/.ssh
+
+  echo "$@" >> /root/.ssh/authorized_keys
+  chmod 600 /root/.ssh/authorized_keys
+
+  echo "====== Added to 'pi' and 'root' user's authorized_keys ======"
+  echo "$@"
 }
 
 function help {
-  echo "Usage: $0 " >&2
+  echo "Usage: $(basename $0) " >&2
   echo
-  echo "   expandfs              expands the partition of the RPI image to the maximum of the SDcard"
-  echo "   rename <hostname>     expands the partition of the RPI image to the maximum of the SDcard"
+  echo "   expandfs                  expands the partition of the RPI image to the maximum of the SDcard"
+  echo "   rename <hostname>         changes hostname"
+  echo "   password <password>       change the password for 'pi' user"
+  echo "   sshkeyadd <public_key>    add a public key to 'pi' and 'root' user's authorized_keys"
   echo
   exit 0
 }
 
+function checkroot {
+  if [[ $EUID -ne 0 ]];
+  then
+      echo "Error: Must be run with root permissions"
+      exit 0
+  fi
+}
+
+
 case $1 in
   expandfs)
+    checkroot
     expandfs
     ;;
   rename)
+    checkroot
     rename $2
+    ;;
+  password)
+    checkroot
+    password $2
+    ;;
+  sshkeyadd)
+    checkroot
+    shift
+    sshkeyadd $@
+    ;;
+  version)
+    version
     ;;
   *)
     help

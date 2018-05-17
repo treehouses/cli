8265fe3341efc0f776e879a9a0c33b7954bda693
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-05-16 23:46:25 -0300

    treehouses bridge

--- cli.sh
+++ cli.sh
@@ -17,6 +17,8 @@ function help {
   echo "   wifi <ESSID> [password]                  connects to a wifi network"
   echo "   staticwifi <ip> <mask> <gateway> <dns>   configures rpi wifi interface to a static ip address"
   echo "              <ESSID> [password]"
+  echo "   bridge <ESSID> <hotspotESSID>            configures the rpi to bridge the wlan interface over a hotspot"
+  echo "          <password> [hotspotPassword]"
   echo "   container <none|docker|balena>           enables (and start) the desired container"
   echo "   bluetooth <on|off>                       switches between bluetooth hotspot mode / regular bluetooth and starts the service"
   echo "   hotspot <ESSID> [password]               creates a mobile hotspot"
@@ -182,6 +184,9 @@ function wifi {
   cp "$TEMPLATES/network/dnsmasq/default" /etc/dnsmasq.conf
   cp "$TEMPLATES/rc.local/default" /etc/rc.local
 
+  cp "$TEMPLATES/network/10-wpa_supplicant" /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant
+  rm -rf /etc/udev/rules.d/90-wireless.rules
+
   {
     echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev"
     echo "update_config=1"
@@ -320,6 +325,9 @@ function hotspot {
   cp "$TEMPLATES/network/hostapd/default" /etc/default/hostapd
   cp "$TEMPLATES/rc.local/hotspot" /etc/rc.local
 
+  cp "$TEMPLATES/network/10-wpa_supplicant" /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant
+  rm -rf /etc/udev/rules.d/90-wireless.rules
+
   if [ -n "$password" ];
   then
     cp "$TEMPLATES/network/hostapd/password" /etc/hostapd/hostapd.conf
@@ -420,6 +428,10 @@ function default {
   cp "$TEMPLATES/rc.local/default" "/etc/rc.local"
   cp "$TEMPLATES/network/dnsmasq/default" "/etc/dnsmasq.conf"
   cp "$TEMPLATES/network/dhcpcd/default" "/etc/dhcpcd.conf"
+
+  cp "$TEMPLATES/network/10-wpa_supplicant" /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant
+  rm -rf /etc/udev/rules.d/90-wireless.rules
+
   rm -rf /etc/hostapd.conf
   rm -rf /etc/network/interfaces.d/*
   rm -rf /etc/rpi-wifi-country
@@ -443,6 +455,10 @@ function upgrade {
 function staticwifi {
   cp "$TEMPLATES/network/interfaces/modular" /etc/network/interfaces
   cp "$TEMPLATES/network/wlan0/static" /etc/network/interfaces.d/wlan0
+
+  cp "$TEMPLATES/network/10-wpa_supplicant" /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant
+  rm -rf /etc/udev/rules.d/90-wireless.rules
+
   sed -i "s/IPADDRESS/$1/g" /etc/network/interfaces.d/wlan0
   sed -i "s/NETMASK/$2/g" /etc/network/interfaces.d/wlan0
   sed -i "s/GATEWAY/$3/g" /etc/network/interfaces.d/wlan0
@@ -487,6 +503,77 @@ function staticwifi {
 
   echo "Success: the wifi settings have been changed, a reboot is required in order to see the changes"
 }
+
+# treehouses bridge MyNetwork HotspotNetwork  MyNetworkPassword HotspotPassword
+
+function bridge {
+  wifiessid="$1"
+  hotspotessid="$2"
+  wifipassword="$3"
+  hotspotpassword="$4"
+  macaddress=$(cat /sys/class/net/wlan0/address)
+  channels=(1 6 11)
+  channel=${channels[$((RANDOM % ${#channels[@]}))]};
+
+  if [ -z "$hotspotessid" ];
+  then
+    echo "a hotspot essid is required"
+    exit 1
+  fi
+
+  cp "$TEMPLATES/network/dnsmasq/bridge" "/etc/dnsmasq.conf"
+  cp "$TEMPLATES/network/interfaces/modular" /etc/network/interfaces 
+  cp "$TEMPLATES/network/wlan0/bridge" /etc/network/interfaces.d/wlan0
+
+  if [ -z "$hotspotpassword" ];
+  then
+    cp "$TEMPLATES/network/hostapd/bridge_no_password" /etc/hostapd/hostapd.conf
+  else
+    cp "$TEMPLATES/network/hostapd/bridge_password" /etc/hostapd/hostapd.conf
+    sed -i "s/PASSWORD/$hotspotpassword/g" /etc/hostapd/hostapd.conf
+  fi
+
+  cp "$TEMPLATES/network/hostapd/default" /etc/default/hostapd
+  sed -i "s/CHANNEL/$channel/g" /etc/hostapd/hostapd.conf
+  sed -i "s/ESSID/$hotspotessid/g" /etc/hostapd/hostapd.conf
+
+  {
+    echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev"
+    echo "update_config=1"
+    wificountry="US"
+    if [ -r /etc/rpi-wifi-country ];
+    then
+      wificountry=$(cat /etc/rpi-wifi-country)
+    fi
+    echo "country=$wificountry"
+  } > /etc/wpa_supplicant/wpa_supplicant.conf
+
+  if [ -z "$wifipassword" ];
+  then
+    {
+        echo "network={"
+        echo "  ssid=\"$wifinetwork\""
+        echo "  key_mgmt=NONE"
+        echo "}"
+    } >> /etc/wpa_supplicant/wpa_supplicant.conf
+  else
+    {
+      echo "network={"
+      echo "  ssid=\"$wifiessid\""
+      echo "  psk=\"$wifipassword\""
+      echo "}"
+    } >> /etc/wpa_supplicant/wpa_supplicant.conf
+  fi
+
+  cp "$TEMPLATES/network/90-wireless.rules" /etc/udev/rules.d/90-wireless.rules
+  rm -rf /lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant
+  cp "$TEMPLATES/rc.local/bridge" /etc/rc.local
+  sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>/dev/null
+  iptables -t nat -A POSTROUTING -s 192.168.2.0/24 ! -d 192.168.2.0/24 -j MASQUERADE
+
+  echo "the bridge has been built ;), a reboot is required to apply changes"
+}
+
 
 case $1 in
   expandfs)
@@ -559,6 +646,10 @@ case $1 in
   upgrade)
     checkroot
     upgrade
+    ;;
+  bridge)
+    checkroot
+    bridge "$2" "$3" "$4" "$5"
     ;;
   *)
     help

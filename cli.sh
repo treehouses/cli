3d2e420cbb5c161f3406716e7f594f31647aba87
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-07-11 21:48:40 -0300

    update upgrade with -f (fixes #119) (fixes #121) (#120)

--- cli.sh
+++ cli.sh
@@ -127,13 +127,17 @@ function help {
       ;;
     upgrade)
       echo ""
-      echo "Usage: $(basename "$0") upgrade"
+      echo "Usage: $(basename "$0") upgrade [-f]"
       echo "" 
       echo "Upgrades $(basename "$0") package using npm"
       echo ""
       echo "Example:"
       echo " $(basename "$0") upgrade"
       echo "    This will upgrade the $(basename "$0") package using npm and will try to install the latest version of $(basename "$0") running on your system"
+      echo "    If the latest version if installed it will not try to reinstall it"
+      echo ""
+      echo " $(basename "$0") upgrade -f"
+      echo "    This will do the same as upgrade, but the version will not be checked. Meaning that this will force the installation of the latest version"
       echo ""
       ;;
     bridge)
@@ -704,6 +708,15 @@ function default {
 }
 
 function upgrade {
+  if ! [[ "$*" = *"-f"* ]];
+  then
+    last_version=$(npm show @treehouses/cli version)
+    if [ "$last_version" = "$(version)" ];
+    then
+      echo "$(basename "$0") is already up to date."
+      exit
+    fi
+  fi
   npm install -g '@treehouses/cli'
 }
 
@@ -927,7 +940,8 @@ case $1 in
     ;;
   upgrade)
     checkroot
-    upgrade
+    shift
+    upgrade "$@"
     ;;
   bridge)
     checkroot

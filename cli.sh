47c26d1329af3f86c751a6b5f6689447234ebad3
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-08-03 22:45:56 -0300

    codeclimate fixes

--- cli.sh
+++ cli.sh
@@ -3,30 +3,55 @@
 SCRIPTPATH=$(realpath "$0")
 SCRIPTFOLDER=$(dirname "$SCRIPTPATH")
 
+# shellcheck source=modules/globals.sh
 source "$SCRIPTFOLDER/modules/globals.sh"
+# shellcheck source=modules/bluetooth.sh
 source "$SCRIPTFOLDER/modules/bluetooth.sh"
+# shellcheck source=modules/bridge.sh
 source "$SCRIPTFOLDER/modules/bridge.sh"
+# shellcheck source=modules/container.sh
 source "$SCRIPTFOLDER/modules/container.sh"
+# shellcheck source=modules/default.sh
 source "$SCRIPTFOLDER/modules/default.sh"
+# shellcheck source=modules/detectrpi.sh
 source "$SCRIPTFOLDER/modules/detectrpi.sh"
+# shellcheck source=modules/ethernet.sh
 source "$SCRIPTFOLDER/modules/ethernet.sh"
+# shellcheck source=modules/expandfs.sh
 source "$SCRIPTFOLDER/modules/expandfs.sh"
+# shellcheck source=modules/help.sh
 source "$SCRIPTFOLDER/modules/help.sh"
+# shellcheck source=modules/hotspot.sh
 source "$SCRIPTFOLDER/modules/hotspot.sh"
+# shellcheck source=modules/image.sh
 source "$SCRIPTFOLDER/modules/image.sh"
+# shellcheck source=modules/led.sh
 source "$SCRIPTFOLDER/modules/led.sh"
+# shellcheck source=modules/locale.sh
 source "$SCRIPTFOLDER/modules/locale.sh"
+# shellcheck source=modules/password.sh
 source "$SCRIPTFOLDER/modules/password.sh"
+# shellcheck source=modules/rename.sh
 source "$SCRIPTFOLDER/modules/rename.sh"
+# shellcheck source=modules/ssh.sh
 source "$SCRIPTFOLDER/modules/ssh.sh"
+# shellcheck source=modules/sshkeyadd.sh
 source "$SCRIPTFOLDER/modules/sshkeyadd.sh"
+# shellcheck source=modules/sshtunnel.sh
 source "$SCRIPTFOLDER/modules/sshtunnel.sh"
+# shellcheck source=modules/staticwifi.sh
 source "$SCRIPTFOLDER/modules/staticwifi.sh"
+# shellcheck source=modules/timezone.sh
 source "$SCRIPTFOLDER/modules/timezone.sh"
+# shellcheck source=modules/upgrade.sh
 source "$SCRIPTFOLDER/modules/upgrade.sh"
+# shellcheck source=modules/version.sh
 source "$SCRIPTFOLDER/modules/version.sh"
+# shellcheck source=modules/vnc.sh
 source "$SCRIPTFOLDER/modules/vnc.sh"
+# shellcheck source=modules/wifi.sh
 source "$SCRIPTFOLDER/modules/wifi.sh"
+# shellcheck source=modules/wificountry.sh
 source "$SCRIPTFOLDER/modules/wificountry.sh"
 
 

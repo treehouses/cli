973eb3b2d473e9c3bf5d1284a07a74deb8468e4b
Ignacio Rodriguez <ignacio@sugarlabs.org>
2018-09-26 19:12:04 -0300

    right channel

--- modules/feedback.sh
+++ modules/feedback.sh
@@ -2,7 +2,7 @@
 
 function feedback {
   message="$1"
-  curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer <token>" "https://api.gitter.im/v1/rooms/5ba5af3cd73408ce4fa8fcfb/chatMessages" -d "{\"text\":\"$(hostname) ($(curl ifconfig.io -s)) said:\n$message\"}" > /dev/null
+  curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer <token>" "https://api.gitter.im/v1/rooms/5ba584b9d73408ce4fa8fb19/chatMessages" -d "{\"text\":\"$(hostname) ($(curl ifconfig.io -s)) said:\n$message\"}" > /dev/null
   echo "Thanks for the feedback!"
 }
 

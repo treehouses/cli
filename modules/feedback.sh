6064f5aec773b5aa5c3ecc55736b333073da5d4a
Ignacio Rodriguez <ignacio@sugarlabs.org>
2018-09-26 22:35:39 -0300

    detectrpi

--- modules/feedback.sh
+++ modules/feedback.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 token="adfab56b2f10b85f94db25f18e51a4b465dbd670"
-#\`$(treehouses detectrpi)\`
+
 function feedback {
   message="$*"
-  curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $token" "https://api.gitter.im/v1/rooms/5ba5af3cd73408ce4fa8fcfb/chatMessages" -d "{\"text\":\"\`$(hostname)\` \`$(curl ifconfig.io -s)\` \`$(treehouses version)\` \`RPI3B+\` \`$(cat /boot/version.txt)\`:\n$message\"}" > /dev/null
+  curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $token" "https://api.gitter.im/v1/rooms/5ba5af3cd73408ce4fa8fcfb/chatMessages" -d "{\"text\":\"\`$(hostname)\` \`$(curl ifconfig.io -s)\` \`$(treehouses version)\` \`$(treehouses detectrpi)\` \`$(cat /boot/version.txt)\`:\n$message\"}" > /dev/null
   echo "Thanks for the feedback!"
 }
 

3c6ee10fb6d510c4c69e7a05bff4da5885a5fd14
Stuart Powers <stuart.powers@gmail.com>
2018-10-10 19:53:00 -0400

    add clone help info

--- modules/help.sh
+++ modules/help.sh
@@ -36,6 +36,7 @@ function help_default {
   echo "   networkmode                              outputs the current network mode"
   echo "   button <off|bluetooth>                   gives the gpio pin 18 an action"
   echo "   feedback <message>                       sends feedback"
+  echo "   clone                                    clones the current SD onto a secondary SDcard"
   echo
 }
 

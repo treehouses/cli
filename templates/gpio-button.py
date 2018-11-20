bb1cea55a83b1758eb07bcf1ec6ced88ac03bd93
mpj <11813607+empeje@users.noreply.github.com>
2018-11-21 00:32:04 +0700

    update treehouses button (fixes #193) (#194)

--- templates/gpio-button.py
+++ templates/gpio-button.py
@@ -5,6 +5,9 @@ import RPi.GPIO as GPIO
 import time
 import subprocess
 
+ESTIMATED_STATE_DIFF_DURATION = 1
+ESTIMATED_PIN_CALLBACK_DURATION = 15
+
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -12,7 +15,9 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 prevState = None
 while True:
   state = GPIO.input(18)
+  # sleep in the loop is related to how fast the command in the block run
   if prevState == state:
+    time.sleep(ESTIMATED_STATE_DIFF_DURATION)
     continue
   elif state == 1:
     print "Pin is on"
@@ -20,13 +25,13 @@ while True:
       subprocess.check_output(['bash', '/etc/gpio-button-action-on.sh'])
     except:
       pass
-    time.sleep(15)
+    time.sleep(ESTIMATED_PIN_CALLBACK_DURATION)
   else:
     print "Pin is off"
     try:
       subprocess.check_output(['bash', '/etc/gpio-button-action-off.sh'])
     except:
       pass
-    time.sleep(15)
+    time.sleep(ESTIMATED_PIN_CALLBACK_DURATION)
 
   prevState = state

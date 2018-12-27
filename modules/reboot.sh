c09e6c6abe62689d97c5937c1b1bba4dd3f591f9
dogi <dogi@users.noreply.github.com>
2018-12-26 21:27:37 -0500

    Update reboot.sh

--- modules/reboot.sh
+++ modules/reboot.sh
@@ -11,12 +11,12 @@ function reboot {
 
 function reboot_help {
   echo ""
-  echo "Usage: $(basename "$0") reboot"
+  echo "Usage: $(basename "$0") rebootneeded"
   echo ""
   echo "Shows if a reboot is required to apply the configuration changes done by this command"
   echo ""
   echo "Example:"
-  echo "  $(basename "$0") reboot"
+  echo "  $(basename "$0") rebootneeded"
   echo "      output: true"
   echo ""
 }

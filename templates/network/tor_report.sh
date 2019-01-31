ef77dff5b9beb9b4f4eb183541a866cf72477791
Ignacio Rodríguez <ignacio@sugarlabs.org>
2019-01-30 21:37:12 -0300

    fix cc complains

--- templates/network/tor_report.sh
+++ templates/network/tor_report.sh
@@ -2,7 +2,7 @@
 
 function report {
   tor_ports=$(treehouses tor list | sed '1d' | sed "s/ <=> /:/g" | tr "\n" " " | sed 's/.$//')
-  while read channel; do
+  while read -r channel; do
     export gitter_channel="$channel"
     treehouses feedback "$(treehouses tor)\n$tor_ports"
   done < /etc/tor_report_channels.txt

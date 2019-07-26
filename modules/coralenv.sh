e89bef5d50e7c4e4f0ffcfa2be5c1e44e8d1af7e
vers4ce <vkafetzopoulos@gmail.com>
2019-07-25 21:01:13 -0400

    Update coralenv.sh

--- modules/coralenv.sh
+++ modules/coralenv.sh
@@ -41,13 +41,13 @@ case "$param" in
 
 # Start the demo until next reboot
  "demo-on" )
-    nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &;
+    nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &
     echo "Success: the Coral Environmental board is now displaying sensor information.";
     echo "The board's display will turn off on reboot.";;
 
 # Starts the demo, and activates it on reboot
   "demo-always-on" )
-    nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &;
+    nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &
     sed -i 's/exit 0/nohup python3 \/usr\/lib\/python3\/dist-packages\/coral\/enviro\/enviro_demo\.py \&>\/dev\/null \&/g' /etc/rc.local;
     sed -i '$ a exit 0' /etc/rc.local;
     echo "Success: the Coral Environmental board is now displaying sensor information.";
@@ -58,33 +58,33 @@ case "$param" in
      sed -i '/nohup python3 \/usr\/lib\/python3\/dist-packages\/coral\/enviro\/enviro_demo\.py \&>\/dev\/null \&/d' /etc/rc.local;
      pkill enviro_demo;
      echo "Success: the Coral Environmental board demo is now stopped.";
-     echo "The board's display will turn off on reboot." ;
+     echo "The board's display will turn off on reboot." ;;
     
 # Prints the help page.
  "help" )
-    echo ""
-    echo "Usage: $(basename "$0") coralenv <demo-on|demo-always-on|demo-off|help>"
-    echo ""
-    echo "Controls the Environmental Board"
-    echo ""
-    echo "Example:"
-    echo "  $(basename "$0") coralenv demo-on"
-    echo "      Starts the Coral Environmental board demo."
-    echo "      The Coral Environmental board will be displaying sensor information."
-    echo "      The board's display will turn off on reboot."
-    echo ""
-    echo "  $(basename "$0") coralenv demo-always-on"
-    echo "      Starts the Coral Environmental board demo."
-    echo "      The board's display will persist on reboot."
-    echo ""
-    echo "  $(basename "$0") coralenv demo-off"
-    echo "      Stops the demo and the board's display will turn off on reboot."
-    echo ""
-    echo "  $(basename "$0") coralenv help"
-    echo "      This help."
+    echo "";
+    echo "Usage: $(basename "$0") coralenv <demo-on|demo-always-on|demo-off|help>";
+    echo "";
+    echo "Controls the Environmental Board";
+    echo "";
+    echo "Example:";
+    echo "  $(basename "$0") coralenv demo-on";
+    echo "      Starts the Coral Environmental board demo.";
+    echo "      The Coral Environmental board will be displaying sensor information.";
+    echo "      The board's display will turn off on reboot.";
+    echo "";
+    echo "  $(basename "$0") coralenv demo-always-on";
+    echo "      Starts the Coral Environmental board demo.";
+    echo "      The board's display will persist on reboot.";
+    echo "";
+    echo "  $(basename "$0") coralenv demo-off";
+    echo "      Stops the demo and the board's display will turn off on reboot.";
+    echo "";
+    echo "  $(basename "$0") coralenv help";
+    echo "      This help.";;
   
   * )
-    echo "Error: only 'demo-on', 'demo-always-on', 'demo-off' and 'help' options are supported";
+    echo "Error: only 'demo-on', 'demo-always-on', 'demo-off' and 'help' options are supported";;
     
     esac
     

7ab90fa03dd45b4a691d136d5722b33b952818e9
dogi <dogi@users.noreply.github.com>
2019-07-25 23:55:34 -0400

    Update coralenv.sh

--- modules/coralenv.sh
+++ modules/coralenv.sh
@@ -8,7 +8,7 @@ function coralenv {
     while true; do
       read -n 1 -pr "Do you want to install the prerequisite packages for the Coral Environmental Board? (y/n)" answer
       case "$answer" in
-        "y"* )
+        "y"*)
           echo "deb https://packages.cloud.google.com/apt coral-cloud-stable main" | tee /etc/apt/sources.list.d/coral-cloud.list;
           curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -;
           apt update;
@@ -17,17 +17,17 @@ function coralenv {
           while reboot_needed; do
             read -n 1 -pr "Would you like to reboot now? (y/n)" answerreboot;
             case "$answerreboot" in
-              "y"* )
+              "y"*)
                 reboot
                 ;;
-              * )
+              *)
                 exit 1
                 ;;
             esac
           done
           exit 1
           ;;
-        "n"* )
+        "n"*)
           echo "To install them manually, run:";    
           echo "echo \"deb https://packages.cloud.google.com/apt coral-cloud-stable main\" | sudo tee /etc/apt/sources.list.d/coral-cloud.list"
           echo "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -"
@@ -35,7 +35,7 @@ function coralenv {
           echo "sudo apt install python3-coral-enviro"
           exit 1
           ;;
-        * )
+        *)
           echo "Please answer (y)es or (n)o."
           ;;
       esac
@@ -44,25 +44,25 @@ function coralenv {
 
 if [ -e /sys/bus/iio/devices/iio:device0 ]; then # Checks if board is attached
   case "$param" in
-    "demo-on" ) # Start the demo until next reboot
+    "demo-on") # Start the demo until next reboot
       nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &
       echo "Success: the Coral Environmental board is now displaying sensor information.";
       echo "The board's display will turn off on reboot."
       ;;
-    "demo-always-on" ) # Starts the demo, and activates it on reboot
+    "demo-always-on") # Starts the demo, and activates it on reboot
       nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &
       sed -i 's/exit 0/nohup python3 \/usr\/lib\/python3\/dist-packages\/coral\/enviro\/enviro_demo\.py \&>\/dev\/null \&/g' /etc/rc.local;
       sed -i '$ a exit 0' /etc/rc.local;
       echo "Success: the Coral Environmental board is now displaying sensor information.";
       echo "The board's display will persist on reboot."
       ;;
-    "demo-off" ) # Stops the demo and deactivates it on reboot
+    "demo-off") # Stops the demo and deactivates it on reboot
       sed -i '/nohup python3 \/usr\/lib\/python3\/dist-packages\/coral\/enviro\/enviro_demo\.py \&>\/dev\/null \&/d' /etc/rc.local;
       pkill enviro_demo;
       echo "Success: the Coral Environmental board demo is now stopped.";
       echo "The board's display will turn off on reboot."
       ;;
-    * )
+    *)
       echo "Error: only 'demo-on', 'demo-always-on' and 'demo-off' options are supported"
       ;;
     esac

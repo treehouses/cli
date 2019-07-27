32dea6a6995ff66506f75845a4e1d50edd0506a9
vers4ce <vkafetzopoulos@gmail.com>
2019-07-26 23:14:10 -0400

    Update coralenv.sh

--- modules/coralenv.sh
+++ modules/coralenv.sh
@@ -2,44 +2,18 @@
 
 function coralenv {
   param=$1
-
+  cronjob='@reboot nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &'
+  
   if [ ! -d /usr/share/doc/python3-coral-enviro ] ; then
     warn "Error: the Coral python environment is not installed"
-    while true; do
-      read -n 1 -pr "Do you want to install the prerequisite packages for the Coral Environmental Board? (y/n)" answer # TODO envelop in its own function
-      case "$answer" in
-        "y"*)
-          echo "deb https://packages.cloud.google.com/apt coral-cloud-stable main" | tee /etc/apt/sources.list.d/coral-cloud.list;
-          curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -;
-          apt update;
-          apt install -y python3-coral-enviro;
-          reboot_needed;
-          while reboot_needed; do
-            read -n 1 -pr "Would you like to reboot now? (y/n)" answerreboot;
-            case "$answerreboot" in
-              "y"*)
-                reboot
-                ;;
-              *)
-                exit 1
-                ;;
-            esac
-          done
-          exit 1
-          ;;
-        "n"*)
-          echo "To install them manually, run:";    
-          echo "echo \"deb https://packages.cloud.google.com/apt coral-cloud-stable main\" | sudo tee /etc/apt/sources.list.d/coral-cloud.list"
-          echo "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -"
-          echo "sudo apt update"
-          echo "sudo apt install python3-coral-enviro"
-          exit 1
-          ;;
-        *)
-          echo "Please answer (y)es or (n)o."
-          ;;
-      esac
-    done
+    echo "You can install it using the command:"
+    echo "$(basename "$0") coralenv install"
+    echo ""
+    echo "To install them manually, run:";    
+    echo "echo \"deb https://packages.cloud.google.com/apt coral-cloud-stable main\" | sudo tee /etc/apt/sources.list.d/coral-cloud.list"
+    echo "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -"
+    echo "sudo apt update"
+    echo "sudo apt install python3-coral-enviro"
   fi
 
 if [ -e /sys/bus/iio/devices/iio:device0 ]; then # Checks if board is attached
@@ -51,19 +25,26 @@ if [ -e /sys/bus/iio/devices/iio:device0 ]; then # Checks if board is attached
       ;;
     "demo-always-on") # Starts the demo, and activates it on reboot
       nohup python3 /usr/lib/python3/dist-packages/coral/enviro/enviro_demo.py &>/dev/null &
-      sed -i 's/exit 0/nohup python3 \/usr\/lib\/python3\/dist-packages\/coral\/enviro\/enviro_demo\.py \&>\/dev\/null \&/g' /etc/rc.local;
-      sed -i '$ a exit 0' /etc/rc.local;
+      ( crontab -l | grep -v -F "$cronjob" ; echo "$cronjob" ) | crontab -
       echo "Success: the Coral Environmental board is now displaying sensor information.";
       echo "The board's display will persist on reboot."
       ;;
     "demo-off") # Stops the demo and deactivates it on reboot
-      sed -i '/nohup python3 \/usr\/lib\/python3\/dist-packages\/coral\/enviro\/enviro_demo\.py \&>\/dev\/null \&/d' /etc/rc.local;
+      ( crontab -l | grep -v -F "$croncmd" ) | crontab -
       pkill enviro_demo;
       echo "Success: the Coral Environmental board demo is now stopped.";
       echo "The board's display will turn off on reboot."
       ;;
+    "install") # Installs the prequisite packages
+      echo "deb https://packages.cloud.google.com/apt coral-cloud-stable main" | tee /etc/apt/sources.list.d/coral-cloud.list;
+      curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -;
+      apt update;
+      apt install -y python3-coral-enviro;
+      reboot_needed;
+      echo "Please reboot your Raspberry Pi."
+      ;;
     *)
-      echo "Error: only 'demo-on', 'demo-always-on' and 'demo-off' options are supported"
+      echo "Error: only 'demo-on', 'demo-always-on', 'demo-off' and 'install' options are supported"
       ;;
     esac
   else

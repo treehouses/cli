8a2fc0b31ce571667d35a0f2252705bf1e7d61f9
Kid243 <dijimbakabs@msn.com>
2019-07-31 16:56:00 -0500

    Update memory.sh

--- modules/memory.sh
+++ modules/memory.sh
@@ -3,24 +3,24 @@
 function memory {
   if [ "$1" == "total" ] ; then
     memory_total
-    echo 'Your rpi has (($total)) B of total memory';
+    echo 'Your rpi has "$total" B of total memory';
     exit 0
   fi
 
   if [ "$1" == "used" ] ; then
     memory_used
-    echo 'Your rpi has (($used + $buff/cache)) B of used memory';
+    echo 'Your rpi has ""$used"+"$buff/cache"" B of used memory';
     exit 0
   fi
 
   if [ "$1" == "free" ] ; then
     memory_free
-    echo 'Your rpi has (($free)) B of free memory';
+    echo 'Your rpi has "$free" B of free memory';
     exit 0
   fi
 
   free
-  echo 'Your rpi has (($total)) B of total memory with (($used + $buff/cache)) B used and $free B avalaible'
+  echo 'Your rpi has "$total" B of total memory with ""$used"+"$buff/cache"" B used and "$free" B avalaible'
 }
 
 function memory_total {

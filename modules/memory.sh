ca704a780806aab7ae4bd8aeda78b7017a57274a
Kid243 <dijimbakabs@msn.com>
2019-07-31 20:18:31 -0500

    Update memory.sh

--- modules/memory.sh
+++ modules/memory.sh
@@ -3,36 +3,40 @@
 function memory {
   if [ "$1" == "total" ] ; then
     memory_total
-    echo "Your rpi has  B of total memory";
+    echo "Your rpi has $((t)) B of total memory";
     exit 0
   fi
 
   if [ "$1" == "used" ] ; then
     memory_used
-    echo "Your rpi has $(free) B of used memory";
+    echo "Your rpi has $((ubc)) B of used memory";
     exit 0
   fi
 
   if [ "$1" == "free" ] ; then
     memory_free
-    echo "Your rpi has $(free) B of free memory";
+    echo "Your rpi has $((f)) B of free memory";
     exit 0
   fi
 
-  free
+  memory_total
+  memory_used
+  memory_free
+  
   echo "Your rpi has $(free) B of total memory with $(free) B used and $(free) B avalaible"
 }
 
 function memory_total {
-  free | grep Mem | cut -c 13-19
+  free | grep Mem | cut -c 13-19 > t
 }
 
 function memory_used {
-  free | grep Mem | cut -c 20-31
-  free | grep Mem | cut -c 56-72
+  free | grep Mem | cut -c 20-31 > u
+  free | grep Mem | cut -c 56-72 > bc
+  ubc = echo "$((u + bc))"
 }
 
 function memory_free {
-  free | grep Mem | cut -c 32-43
+  free | grep Mem | cut -c 32-43 > f
 }
 

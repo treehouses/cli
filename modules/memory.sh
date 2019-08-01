c22078313fcc35ab091e8be956cf105c2e5ebcb4
Kid243 <dijimbakabs@msn.com>
2019-07-31 23:39:27 -0500

    Adding memory help

--- modules/memory.sh
+++ modules/memory.sh
@@ -3,19 +3,19 @@
 function memory {
   if [ "$1" == "total" ] ; then
     memory_total
-    echo "Your rpi has $((t)) B of total memory";
+    echo "$((t))";
     exit 0
   fi
 
   if [ "$1" == "used" ] ; then
     memory_used
-    echo "Your rpi has $((ubc)) B of used memory";
+    echo "$((ubc))";
     exit 0
   fi
 
   if [ "$1" == "free" ] ; then
     memory_free
-    echo "Your rpi has $((f)) B of free memory";
+    echo "$((f))";
     exit 0
   fi
 
@@ -23,7 +23,7 @@ function memory {
   memory_used
   memory_free
   
-  echo "Your rpi has $((t)) B of total memory with $((ubc)) B used and $((f)) B avalaible"
+  echo "Your rpi has $((t)) bytes of total memory with $((ubc)) bytes used and $((f)) bytes avalaible"
 }
 
 function memory_total {
@@ -40,3 +40,23 @@ function memory_free {
   f=$(free | grep -i Mem | awk '{printf $4}')
 }
 
+function memory_help {
+  echo ""
+  echo "Usage: $(basename "$0") memory [total|used|free]"
+  echo ""
+  echo "Displays the various values for total, used, and free RAM memory."
+  echo ""
+  echo "Example:"
+  echo "  $(basename "$0") memory"
+  echo "      This will display in a single sentence 3 different RAM memory values in bytes for total, used and free memory."
+  echo ""
+  echo "  $(basename "$0") memory total"
+  echo "      This will return the numerical value for the total memory (value in bytes)."
+  echo ""
+  echo "  $(basename "$0") memory used"
+  echo "      This will return the numerical value for the used memory (value in bytes)."
+  echo ""
+  echo "  $(basename "$0") memory free"
+  echo "      This will return the numerical value for the remaining free memory (value in bytes)."
+  echo ""
+}

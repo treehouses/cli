4a18ef5a5818a335d390f824d1611cbed266300f
dogi <dogi@users.noreply.github.com>
2019-08-10 00:31:35 -0400

    Update networkmode.sh

--- modules/networkmode.sh
+++ modules/networkmode.sh
@@ -75,16 +75,16 @@ function get_ap_name {
 
 function get_ap_settings {
   if ! grep -q "wpa_passphrase=*" "/etc/hostapd/hostapd.conf"; then
-    echo "wlan0: ap essid: $(get_ap_name), ap has no password, ip: $(get_ipv4_ip wlan0),"
+    echo -n "wlan0: ap essid: $(get_ap_name), ap has no password, ip: $(get_ipv4_ip wlan0),"
   else
-    echo "wlan0: ap essid: $(get_ap_name), ap has password, ip: $(get_ipv4_ip wlan0),"
+    echo -n "wlan0: ap essid: $(get_ap_name), ap has password, ip: $(get_ipv4_ip wlan0),"
   fi
-  echo -n "eth0: ip: $(get_ipv4_ip eth0),"
   if [ "$1" == "ap local" ]; then
     echo " not sharing internet"
   else
     echo " sharing internet"
   fi
+  echo -n "eth0: ip: $(get_ipv4_ip eth0),"
 }
 
 function get_staticnetwork_info {

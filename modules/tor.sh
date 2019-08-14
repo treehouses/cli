7e7cef02d84c32c39c9f8adfea8ce8fc8e47336b
Kid243 <dijimbakabs@msn.com>
2019-08-14 18:24:37 -0500

    Update tor.sh

--- modules/tor.sh
+++ modules/tor.sh
@@ -106,7 +106,10 @@ function tor {
       rm -rf /etc/tor_report.sh /etc/cron.d/tor_report /etc/tor_report_channels.txt || true
       echo "OK."
     elif [ "$option" = "now" ]; then
-       echo "$(treehouses tor)\n$tor_ports\n\`$(date -u +"%Y-%m-%d %H:%M:%S %Z")\` $(treehouses networkmode)"
+       echo "$(treehouses tor)\n$tor_ports\n\"
+       grep -Poi "^HiddenServicePort \\K(.*) 127.0.0.1:(.*)\\b" /etc/tor/torrc | tac | sed -r 's/(.*?)127.0.0.1:(.*?)/\1 <=> \2/g'
+       echo "$(date -u +"%Y-%m-%d %H:%M:%S %Z")\"
+       echo "$(treehouses networkmode)"
     elif [ -z "$option" ]; then
       if [ -f "/etc/cron.d/tor_report" ]; then
         status="on"

2ccf026ebe4c182d433cb25891744d4ea485a9e0
Kid243 <dijimbakabs@msn.com>
2019-08-14 20:23:46 -0500

    Update tor_report.sh

--- templates/network/tor_report.sh
+++ templates/network/tor_report.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 function report {
-    tor_ports=$(treehouses tor list | sed '1d' | sed "s/ <=> /:/g" | tr "\n" " " | sed 's/.$//')
-    while read -r channel; do
+  while read -r channel; do
     export gitter_channel="$channel"
-    treehouses feedback "$(treehouses tor)\n$((tor_ports))\n\`$(date -u +"%Y-%m-%d %H:%M:%S %Z")\` $(treehouses networkmode)"
+    treehouses feedback "$(treehouses tor)\n$(tor_ports)\n\`$(date -u +"%Y-%m-%d %H:%M:%S %Z")\` $(treehouses networkmode)"
   done < /etc/tor_report_channels.txt
   echo "report"
 }
 
+function tor_ports {
+    treehouses tor list | sed '1d' | sed "s/ <=> /:/g" | tr "\n" " " | sed 's/.$//'
+    }
 
 prevStatus=$(cat /tmp/prev_tor_status 2>/dev/null || echo "offline")
 if ping -q -c 3 -W 1 1.1.1.1 >/dev/null; then

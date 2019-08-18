17672f8cdb693da28696f97db7a2565778e4bd52
Iris B <irisbeharaj@gmail.com>
2019-08-18 16:05:06 +0200

    Update rename.sh
    fixes issue with special characters and

--- modules/rename.sh
+++ modules/rename.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 function rename () {
-  CURRENT_HOSTNAME=$(< /etc/hostname tr -d " \\t\\n\\r")
-  echo "$1" > /etc/hostname
-  sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\\t$1/g" /etc/hosts
-  hostname "$1"
-  echo "Success: the hostname has been modified"
+  if
+    [ "$(expr "$1" : ".*[!@#\$%^\&*()_+].*")" -gt 0 ] || #checks for special characters
+    echo "$1" | grep -E '[ "]' >/dev/null || #Checks for spaces
+    [[ ${#1} -gt "64" ]]|| #Checks for length greater than 64
+    [ -z "$var" ]; #Checks if variable is empty
+  then
+    echo "Unsuccessful: Make sure to remove special characters and spaces."
+  else
+    CURRENT_HOSTNAME=$(< /etc/hostname tr -d " \\t\\n\\r")
+    echo "$1" > /etc/hostname
+    sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\\t$1/g" /etc/hosts
+    hostname "$1"
+    echo "Success: the hostname has been modified"
+  fi
 }
 
 function rename_help () {

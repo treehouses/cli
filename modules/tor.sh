b41c11b181440f9ca4f2909c445e5146dbb47c13
Kid243 <dijimbakabs@msn.com>
2019-08-22 00:04:44 -0500

    Update tor.sh

--- modules/tor.sh
+++ modules/tor.sh
@@ -31,8 +31,9 @@ function tor {
       exit 
     fi
     
-    if  ! [[ "$port" =~ ^[[:digit:]]*$ ]]; then
-      echo "Error: wrong port synthax"
+    #if  ! [[ "$port" =~ ^[[:digit:]]*$ ]]; then
+    if  ! [[ "$port" =~ ^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$ ]]; then
+      echo "Error: is not a port"
       exit
     fi
 

02c5e2e56bd2b9238889d3827b3206aae2475ee5
dogi <dogi@users.noreply.github.com>
2019-08-23 21:01:17 -0400

    Update sshkey.sh

--- modules/sshkey.sh
+++ modules/sshkey.sh
@@ -8,7 +8,7 @@ function sshkey () {
     chmod 700 /root/.ssh /home/pi/.ssh
 
     echo "$@" >> /home/pi/.ssh/authorized_keys
-    chmod 600 /home/pi/.ssh/authorized_keys      
+    chmod 600 /home/pi/.ssh/authorized_keys
     chown -R pi:pi /home/pi/.ssh
     
     if [ "$(detectrpi)" != "nonrpi" ]; then

3c601e80c084daff00d7b25113c620c87433125e
Kid243 <dijimbakabs@msn.com>
2019-08-28 23:13:58 -0500

    Update rename.sh

--- modules/rename.sh
+++ modules/rename.sh
@@ -8,7 +8,7 @@ function rename () {
     ! [[ "$1" =~ ^[[:alnum:]"-"]*$ ]] || #checks for special characters and spaces excluding "-"
     [[ ${#1} -gt "64" ]] || #Checks for length greater than 64
     [ -z "$1" ] || #Checks if variable is empty
-    [[ -z "$2" ]];
+    [[ ! -z "$2" ]];
   then
     echo "Unsuccessful: Make sure to remove special characters and spaces."
   else

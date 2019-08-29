fc5f738328a4e73409a11ba196262dd9ae66b247
Kid243 <dijimbakabs@msn.com>
2019-08-28 23:57:07 -0500

    Update rename.sh

--- modules/rename.sh
+++ modules/rename.sh
@@ -4,7 +4,7 @@ function rename () {
 
   if [ -n "$1" ]; then
      space="$2"
-     if [[ -z "$space" ]]; then
+     if [ ! -z "$space" ]; then
      echo "Unsuccessful: Make sure to remove space."
      fi
   fi

a97f31d02a6485118d8c2fb78b25fd63409791bc
Kid243 <dijimbakabs@msn.com>
2019-08-30 16:31:58 -0500

    Update rename.sh

--- modules/rename.sh
+++ modules/rename.sh
@@ -3,8 +3,11 @@
 function rename () { 
 
   if [ -n "$1" ]; then
-     shift
-     echo " $1 "
+     echo "Positional Parameters"
+echo '$0 = ' $0
+echo '$1 = ' $1
+echo '$2 = ' $2
+echo '$3 = ' $3
   fi
   
   #if [[ "$#" -ne 1 ]]; then

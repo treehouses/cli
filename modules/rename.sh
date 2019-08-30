d6d0e16a7c20fd37b5a7d3282f4a87f2f0f1cd21
Kid243 <dijimbakabs@msn.com>
2019-08-30 15:38:39 -0500

    Update rename.sh

--- modules/rename.sh
+++ modules/rename.sh
@@ -12,11 +12,11 @@ function rename () {
   fi
 
   if
-    [[ ${1:0:1} == "-" ]] || #checks beginning for "-"
-    [[ ${1: -1} == "-" ]] || #checks end for "-"
-    ! [[ "$1" =~ ^[[:alnum:]"-"]*$ ]] || #checks for special characters and spaces excluding "-"
-    [[ ${#1} -gt "64" ]] || #Checks for length greater than 64
-    [ -z "$1" ]; #Checks if variable is empty
+   # [[ ${1:0:1} == "-" ]] || #checks beginning for "-"
+   # [[ ${1: -1} == "-" ]] || #checks end for "-"
+   # ! [[ "$1" =~ ^[[:alnum:]"-"]*$ ]] || #checks for special characters and spaces excluding "-"
+   # [[ ${#1} -gt "64" ]] || #Checks for length greater than 64
+   # [ -z "$1" ]; #Checks if variable is empty
   then
     echo "Unsuccessful: Make sure to remove special characters."
   else

fe4cdc1bf457c000afd81ff7028ab441e170fc11
Kid243 <dijimbakabs@msn.com>
2019-08-30 15:39:49 -0500

    Update rename.sh

--- modules/rename.sh
+++ modules/rename.sh
@@ -11,21 +11,21 @@ function rename () {
     echo "Illegal number of parameters"
   fi
 
-  if
+#  if
    # [[ ${1:0:1} == "-" ]] || #checks beginning for "-"
    # [[ ${1: -1} == "-" ]] || #checks end for "-"
    # ! [[ "$1" =~ ^[[:alnum:]"-"]*$ ]] || #checks for special characters and spaces excluding "-"
    # [[ ${#1} -gt "64" ]] || #Checks for length greater than 64
    # [ -z "$1" ]; #Checks if variable is empty
-  then
+#  then
     echo "Unsuccessful: Make sure to remove special characters."
-  else
+#  else
    # CURRENT_HOSTNAME=$(< /etc/hostname tr -d " \\t\\n\\r")
     #echo "$1" > /etc/hostname
     #sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\\t$1/g" /etc/hosts
    # hostname "$1"
     echo "Success: the hostname has been modified"
-  fi
+#  fi
 }
 
 function rename_help () {

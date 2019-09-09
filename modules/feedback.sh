0dd6a20dda7f7ba3f1c5699f118fb8da6f781fe1
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2019-09-09 16:57:13 -0500

    add regex for ip4 and ip6

--- modules/feedback.sh
+++ modules/feedback.sh
@@ -9,10 +9,12 @@ fi
 
 function feedback {
   message="$*"
+  ip6_regex="^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))$"
+  ip4_regex="((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
   if ! [[ -z "$message" ]]; then
     ip_address=$(curl ifconfig.io -s)
-    if [[ $ip_address == *"DOCTYPE"* ]]; then
-      ip_address="no ip detection"
+    if [[ ! $ip_address =~ $ip6_regex ]] && [[ ! $ip_address =~ $ip4_regex ]]; then
+      ip_address="invalid address"
     fi
     if [ "$(detectrpi)" != "nonrpi" ]; then
       body="{\"text\":\"\`$(hostname)\` \`$ip_address\` \`$(version)\` \`$(detectrpi)\` \`$(cat /boot/version.txt)  \`:\\n$message\"}"

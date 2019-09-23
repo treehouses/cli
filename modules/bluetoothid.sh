153f13eff4cd407f00681318046a31b85c587aeb
dogi <dogi@users.noreply.github.com>
2019-09-23 15:04:24 -0400

    Update bluetoothid.sh

--- modules/bluetoothid.sh
+++ modules/bluetoothid.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 #print id code of bluetooth, subfunction prints hostname and bluetooth id
 
-  function bluetoothid () {
-    #echo the id of the bluetooth
-    bid=$(cat /etc/bluetooth-id)
-    id=${bid}
+function bluetoothid () {
+  #echo the id of the bluetooth
+  bid=$(cat /etc/bluetooth-id)
+  id=${bid}
 
-    #echo network name with bluetooth id
-    nname=$(uname -n)
-    name=${nname}
+  #echo network name with bluetooth id
+  nname=$(uname -n)
+  name=${nname}
 
-    case "$1" in
-      "")
+  case "$1" in
+    "")
       echo "Raspberry Pi's ID is: ${id}"
       ;;
-      "hostid")
+    "hostid")
       echo "Raspberry Pi's network Host ID is: ${name}-${id}"
       ;;
-    esac
+  esac
 }
 
-  function bluetoothid_help () {
-    echo ; echo "        Usage: $(basename "$0") bluetoothid [hostid]" ;
-    echo ; echo "                Displays Raspberry Pi's Bluetooth Host Number." ;
-    echo "          Optionally displays Networkname and ID as recieved through pairing." ;
-    echo ; echo "        Example:" ; "   $(basename "$0") bluetoothid hostid" ;
-    echo ; echo "                treehouses-9012" ;
+function bluetoothid_help () {
+  echo ;
+  echo "        Usage: $(basename "$0") bluetoothid [hostid]" ;
+  echo ;
+  echo "                Displays Raspberry Pi's Bluetooth Host Number." ;
+  echo "          Optionally displays Networkname and ID as recieved through pairing." ;
+  echo ;
+  echo "        Example:" ; "   $(basename "$0") bluetoothid hostid" ;
+  echo ;
+  echo "                treehouses-9012" ;
 }
-

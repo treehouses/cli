6a4321ad27678d17323762c052bce0acfa9902c6
root <root@travis-job-8c75e2d8-05d9-4a2f-8dc2-fe5df9bfdd88>
2019-09-25 22:32:52 +0000

    update tor.sh

--- modules/tor.sh
+++ modules/tor.sh
@@ -3,12 +3,23 @@
 function tor {
   check_missing_packages "tor" "curl"
 
+   if [ -z "$1" ]; then
+    cat "/var/lib/tor/treehouses/hostname"
+    exit 0
+  fi
   if { [ ! -d "/var/lib/tor/treehouses" ] || [ ! -f "/var/lib/tor/treehouses/hostname" ]; } && [ "$1" != "start" ] && [ "$1" != "add" ]; then
     echo "Error: the tor service has not been configured."
     echo "Run '$(basename "$0") tor start' to configure it."
     echo "Or '$(basename "$0") add [localPort]' to add a port and be able to use the service"
     exit 1
   fi
+
+  if [ "$1" = 'add' ] || [ "$1" = 'delete' ]; then
+    if [ "$#" -gt 3 ] ; then
+      echo "Error: please enter less then 4 arguments"
+      exit 1
+    fi
+  fi	  
 
   if [ -z "$1" ]; then
     cat "/var/lib/tor/treehouses/hostname"

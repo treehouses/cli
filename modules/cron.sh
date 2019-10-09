c7e8a20cd739291f4bd5344128baa278cccc2a25
CalebProvost <DHX664@gmail.com>
2019-10-09 10:59:28 -0400

    removed copy+paste leftovers which caused codeclimate issues

--- modules/cron.sh
+++ modules/cron.sh
@@ -5,29 +5,29 @@ function cron {
   case "$options" in
     "") #lists current cron tasks
         echo "List of cron jobs:"
-      if [ $(crontab -l | wc -c) -eq 0 ]; then
+      if [[ $(crontab -l | wc -c) -eq 0 ]]; then
         echo "The system has no cron jobs"
       else
-        $(echo crontab -l)
+        crontab -l
       fi
       ;;
 
     "0W") #adds/removes a daily reboot to system - RPi 0's will benefit from this
       if [[ $(crontab -l | grep "@daily") != "@daily reboot now" ]]; then
-        $((crontab -l ; echo "@daily reboot now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -)
+        (crontab -l ; echo "@daily reboot now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
         echo "\"Daily Reboot\" cron job established"
       elif [[ $(crontab -l | grep "@daily") == "@daily reboot now" ]]; then
-        $((crontab -l ; echo "@daily reboot now") 2>&1 | grep -v "no crontab" | grep -v "@daily" | sort | uniq | crontab -)
+        (crontab -l ; echo "@daily reboot now") 2>&1 | grep -v "no crontab" | grep -v "@daily" | sort | uniq | crontab -
         echo "\"Daily Reboot\" cron job removed"
       fi
       ;;
 
     "tor") #add/removes execution of 'tor notice now' every 3 days
       if [[ $(crontab -l | grep tor) != "0 */72 * * * treehouses tor notice now" ]]; then
-        $((crontab -l ; echo "0 */72 * * * treehouses tor notice now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -)
+        (crontab -l ; echo "0 */72 * * * treehouses tor notice now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
         echo "\"treehouses tor notice now\" cron job established"
       elif [[ $(crontab -l | grep tor) == "0 */72 * * * treehouses tor notice now" ]]; then
-         $((crontab -l ; echo "0 */72 * * * treehouses tor notice now") 2>&1 | grep -v "no crontab" | grep -v "tor" | sort | uniq | crontab -)
+         (crontab -l ; echo "0 */72 * * * treehouses tor notice now") 2>&1 | grep -v "no crontab" | grep -v "tor" | sort | uniq | crontab -
         echo "\"treehouses tor notice now\" cron job removed"
       fi
       ;;
@@ -35,15 +35,15 @@ function cron {
     "timestamp") #adds/removes timestamp logging every 15 minutes
       #Make log file if not found
       if [ ! -f /var/log/uptime.log ]; then
-        $(sudo touch /var/log/uptime.log)
+        sudo touch /var/log/uptime.log
         echo "created \"uptime.log\" in /var/log/"
       fi
 
       if [[ $(crontab -l | grep date) != "15 * * * * date >> /var/log/uptime.log" ]]; then
-        $((crontab -l ; echo "15 * * * * date >> /var/log/uptime.log") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -)
+        (crontab -l ; echo "15 * * * * date >> /var/log/uptime.log") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
         echo "\"timestamp /var/log/uptime.log every 15 minutes\" cron job established"
       elif [[ $(crontab -l | grep date) == "15 * * * * date >> /var/log/uptime.log" ]]; then
-        $((crontab -l ; echo "15 * * * * date >> /var/log/uptime.log") 2>&1 | grep -v "no crontab" | grep -v "date" | sort | uniq | crontab -)
+        (crontab -l ; echo "15 * * * * date >> /var/log/uptime.log") 2>&1 | grep -v "no crontab" | grep -v "date" | sort | uniq | crontab -
         echo "timestamping cron job removed"
       fi
       ;;

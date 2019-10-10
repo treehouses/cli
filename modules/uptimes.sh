2b13eec3c3fd84cb1d025ebfe19ffdbff724a3ca
Automationboi <himesh8chopra@gmail.com>
2019-10-10 20:23:53 +0530

    making changes recommended by codeclimate

--- modules/uptimes.sh
+++ modules/uptimes.sh
@@ -3,8 +3,8 @@ if [[ ! -f "../templates/up_time" ]]
 then
 uptime -s > ../templates/up_time
 else
-last_up_time=`cat ../templates/up_time`
-curr_up_time=`uptime -s`
+last_up_time=$(cat ../templates/up_time)
+curr_up_time=$(uptime -s)
 if [[ "$last_up_time" != "$curr_up_time" ]]
 then
 echo "Down Time was between $last_up_time and $curr_up_time"

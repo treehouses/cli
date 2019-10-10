2065a7221d442c6eea55ee3b58df4a5693dd8f6c
Automationboi <himesh8chopra@gmail.com>
2019-10-10 20:13:52 +0530

    adding the uptime calculation script

---
+++ modules/uptimes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ ! -f "../templates/up_time" ]]
+then
+uptime -s > ../templates/up_time
+else
+last_up_time=`cat ../templates/up_time`
+curr_up_time=`uptime -s`
+if [[ "$last_up_time" != "$curr_up_time" ]]
+then
+echo "Down Time was between $last_up_time and $curr_up_time"
+uptime -s > ../templates/up_time
+fi
+fi 

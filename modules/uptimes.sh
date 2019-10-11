7d55286c1d3c6830e195614467d6684e57d85644
Automationboi <himesh8chopra@gmail.com>
2019-10-12 04:41:53 +0530

    modified:   README.md 	modified:   cli.sh 	modified:   modules/help.sh 	modified:   modules/uptimes.sh modifications as per the suggestions

--- modules/uptimes.sh
+++ modules/uptimes.sh
@@ -2,12 +2,14 @@
 if [[ ! -f "../templates/up_time" ]]
 then
 uptime -s > ../templates/up_time
+uptime -s
 else
 last_up_time=$(cat ../templates/up_time)
 curr_up_time=$(uptime -s)
 if [[ "$last_up_time" != "$curr_up_time" ]]
 then
-echo "Down Time was between $last_up_time and $curr_up_time"
 uptime -s > ../templates/up_time
+echo "Down Time was between $last_up_time and $curr_up_time"
+echo "System has been up since $curr_up_time"
 fi
 fi 

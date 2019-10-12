f53d271204ca7617fc60c0b24be9a9586930c1ae
xavierh93 <xavierh93@hotmail.com>
2019-10-11 20:27:13 -0400

    added new command and missing_packages function

--- modules/discover.sh
+++ modules/discover.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 function discover {
+  check_missing_packages "nmap"
+  /usr/bin/nmap "$@"
+
   option=$1
   if [ $option = "ip" ] || [ $option = "ping" ]
   then
@@ -12,15 +15,17 @@ function discover {
       nmap --iflist | grep DC:A6:32
       nmap --iflist | grep B8:27:EB
       ;;
-    ip)
+    scan)
       nmap -v -A -T4  $ip
       ;;
-    hostif)
+    interface)
       nmap --iflist
       ;; 
     ping)
       nmap -sP $ip
       ;;
+    ports)
+      nmap --open $ip
     *)
       echo "Unknown operation provided." 1>&2
       discover_help
@@ -31,7 +36,7 @@ function discover {
 
 function discover_help {
   echo ""
-  echo "Usage: $(basename "$0") discover <rpi|ip|hostif|ping ip|[ipaddress|url]>"
+  echo "Usage: $(basename "$0") discover <rpi|scan|hostinterface|ping|ports ip|ping|ports[ipaddress|url]>"
   echo ""
   echo "Scans the network provdied and shows the open ports. Can scan for all raspberry pis on the network as well."
   echo ""
@@ -40,10 +45,13 @@ function discover_help {
   echo "    Detects raspberry pis on the network."
   echo " $(basename "$0") discover ip 192.168.7.149"
   echo "    Performs a network scan of the provided ip address."
-  echo " $(basename "$0") discover ip scanme.nmap.org"
+  echo " $(basename "$0") discover scan scanme.nmap.org"
   echo "    Performs a network scan of the provided url."
-  echo " $(basename "$0") discover hostif"
+  echo " $(basename "$0") discover interface"
   echo "    Displays the host interfaces and routes on the network."
   echo " $(basename "$0") discover ping 192.168.7.149"
   echo "    Displays servers and devices running on network provided."
+  echo " $(basename "$0") discover ports 192.168.7.149"
+  echo "    Displays open ports."
+  echo ""
 }

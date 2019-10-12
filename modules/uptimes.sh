286742ced8186f0026982020d9ee33ede3ae7505
Automationboi <himesh8chopra@gmail.com>
2019-10-12 13:23:39 +0530

    fixing few things in uptimes.sh

--- modules/uptimes.sh
+++ modules/uptimes.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-if [[ ! -f "../templates/up_time" ]]
+ SCRIPTPATH=$(realpath "$0")
+ SCRIPTFOLDER=$(dirname "$SCRIPTPATH")
+
+function uptimes(){
+if [[ ! -f "$SCRIPTFOLDER/templates/up_time" ]]
 then
-uptime -s > ../templates/up_time
-uptime -s
+uptime -s > "$SCRIPTFOLDER"/templates/up_time 
+up=$(uptime -s)
+echo "System is up since $up"
 else
-last_up_time=$(cat ../templates/up_time)
+last_up_time=$(cat "$SCRIPTFOLDER"/templates/up_time)
 curr_up_time=$(uptime -s)
 if [[ "$last_up_time" != "$curr_up_time" ]]
 then
-uptime -s > ../templates/up_time
+uptime -s > "$SCRIPTFOLDER"/templates/up_time
 echo "Down Time was between $last_up_time and $curr_up_time"
 echo "System has been up since $curr_up_time"
+else
+up=$(uptime -s)
+echo "System is up since $up"
 fi
 fi 
+}

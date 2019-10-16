b6c0175472eb6dbcb18617aa1befb16b809597ab
Caleb Provost <50122974+CalebProvost@users.noreply.github.com>
2019-10-16 12:41:26 -0400

    changing name to reboots to avoid system conflicts

--- cli.sh
+++ cli.sh
@@ -31,7 +31,7 @@ source "$SCRIPTFOLDER/modules/ntp.sh"
 source "$SCRIPTFOLDER/modules/password.sh"
 source "$SCRIPTFOLDER/modules/openvpn.sh"
 source "$SCRIPTFOLDER/modules/rebootneeded.sh"
-source "$SCRIPTFOLDER/modules/reboot.sh"
+source "$SCRIPTFOLDER/modules/reboots.sh"
 source "$SCRIPTFOLDER/modules/rename.sh"
 source "$SCRIPTFOLDER/modules/restore.sh"
 source "$SCRIPTFOLDER/modules/rtc.sh"
@@ -211,8 +211,8 @@ case $1 in
   rebootneeded)
     rebootneeded
     ;;
-  reboot)
-    reboot "$2" "$3"
+  reboots)
+    reboots "$2" "$3"
     ;;
   internet)
     internet

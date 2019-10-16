2263f8b87e14a9af22ba56e481027e07e111aa30
CalebProvost <DHX664@gmail.com>
2019-10-15 20:35:10 -0400

    used shutdown argument with reboot, removed it.

--- modules/reboot.sh
+++ modules/reboot.sh
@@ -8,16 +8,16 @@ function reboot () {
       ;;
     "now")
       echo "Rebooting now."
-      reboot now
+      sudo reboot
       ;;
     "in")
       echo "Rebooting in \"$2\". ctrl+c to cancel."
-      sleep "$2" ; reboot now
+      sleep "$2" ; sudo reboot
       ;;
     "cron")
       #add user's cron job to crontab
-      if [[ $(crontab -l | grep "$2 reboot now" ) != "$2" ]]; then
-        (crontab -l ; echo "$2 reboot now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+      if [[ $(crontab -l | grep "$2 reboot" ) != "$2" ]]; then
+        (crontab -l ; echo "$2 reboot") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
         echo "cron job with frequency \"$2\" added"
       elif [[ $(crontab -l | grep "$2") == "$2" ]]; then
         echo "cron job \"$2\" already established" ; echo "run \"$(basename "$0") help cron\" for cron commands"
@@ -25,15 +25,15 @@ function reboot () {
       ;;
     "daily")
       echo "System will reboot daily at 00:00"
-      (crontab -l ; echo "@daily reboot now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+      (crontab -l ; echo "@daily reboot") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
       ;;
     "weekly")
       echo "System will reboot weekly at 00:00"
-      (crontab -l ; echo "@weekly reboot now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+      (crontab -l ; echo "@weekly reboot") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
       ;;
     "monthly")
       echo "System will reboot monthly at 00:00"
-      (crontab -l ; echo "@monthly reboot now") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+      (crontab -l ; echo "@monthly reboot") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
       ;;
     "*")
       echo "Invalid input"

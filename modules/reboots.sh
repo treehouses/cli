57e4ea56499f3b6fccb9e91487794d7c101ca5d7
Caleb Provost <50122974+CalebProvost@users.noreply.github.com>
2019-10-16 12:45:42 -0400

    changing name to reboots to avoid system conflicts

--- modules/reboots.sh
+++ modules/reboots.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-function reboot {
+function reboots {
   case "$1" in
     "")
       echo "No timeframe selected" ; echo
-      reboot_help
+      reboots_help
       ;;
     "now")
       echo "Rebooting now."
@@ -17,12 +17,12 @@ function reboot {
     "cron")
       if [[ $(crontab -l | grep "$2" ) != "$2 reboot" ]]; then
         (crontab -l ; echo "$2 reboot") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
-        echo "Rebooting with frequency or \"$2\" added"
+        echo "Rebooting with frequency of \"$2\" added"
       elif [[ $(crontab -l | grep "$2") == "$2 reboot" ]]; then
         echo "Reboot frequency of \"$2\" already established"
         echo "To delete it, use: $(basename "$0") cron delete \"$2\""
       else
-        reboot_help
+        reboots_help
       fi
       ;;
     "daily")
@@ -33,7 +33,7 @@ function reboot {
         (crontab -l ; echo "@daily reboot") 2>&1 | grep -v "no crontab" | grep -v "@daily" | sort | uniq | crontab -
         echo "Job to reboot daily removed"
       else
-        reboot_help
+        reboots_help
       fi
       ;;
     "weekly")
@@ -44,7 +44,7 @@ function reboot {
         (crontab -l ; echo "@weekly reboot") 2>&1 | grep -v "no crontab" | grep -v "@weekly" | sort | uniq | crontab -
         echo "Job to reboot weekly removed"
       else
-        reboot_help
+        reboots_help
       fi
       ;;
     "monthly")
@@ -55,31 +55,31 @@ function reboot {
         (crontab -l ; echo "@monthly reboot") 2>&1 | grep -v "no crontab" | grep -v "@monthly" | sort | uniq | crontab -
         echo "Job to reboot monthly removed"
       else
-        reboot_help
+        reboots_help
       fi
       ;;
     "*")
       echo "Invalid input"
-      reboot_help
+      reboots_help
       ;;
   esac
 }
 
-function reboot_help {
+function reboots_help {
   echo ""
-  echo "  Usage: $(basename "$0") reboot <now|in|cron|daily|weekly|monthly>"
+  echo "  Usage: $(basename "$0") reboots <now|in|cron|daily|weekly|monthly>"
   echo ""
   echo "  Reboots system at selected time"
   echo ""
   echo "  Example:"
-  echo "  $(basename "$0") reboot daily"
+  echo "  $(basename "$0") reboots daily"
   echo "  System will reboot daily at 00:00"
   echo ""
-  echo "  $(basename "$0") reboot in 120"
+  echo "  $(basename "$0") reboots in 120"
   echo "  System will reboot in 120 seconds. ctrl+c to cancel."
   echo ""
-  echo "  $(basename "$0") reboot cron \"0 * * * *\""
-  echo "  cron job with frequency \"0 * * * *\" added"
+  echo "  $(basename "$0") reboots cron \"0 * * * *\""
+  echo "  Rebooting with frequency of \"0 * * * *\" added"
   echo ""
   echo "  Set frequency: * * * * *"
   echo "                 │ │ │ │ │"

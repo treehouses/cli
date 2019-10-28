c3cf71f72e27f9f181a0302d0a3b384cde23175d
pattanawadee88 <winyaratp@wit.edu>
2019-10-28 19:12:03 -0400

    add argument for wifi status

--- modules/wifistatus.sh
+++ modules/wifistatus.sh
@@ -8,11 +8,11 @@ function wifistatus {
   #   okay=(-60 -61 -62 -63 -64 -65 -66 -67 -68 -69)
   #   poor=(-70 -71 -72 -73 -74 -75 -76 -77 -78 -79)
   #   bad=(-80 -81 -82 -83 -84 -85 -86 -87 -88 -89 -90)
+  #display strength of signal in dBm and layman terms
+  signal=$(iwconfig wlan0 | sed -n 's/.*\(Signal level=-.*\)/\1/p' | sed -e 's/Signal level=//g' | sed -e 's/dBm//g')
+  signalStrength=$(iwconfig wlan0 | sed -n 's/.*\(Signal level=-.*\)/\1/p' | sed -e 's/Signal level=//g')
 
   case "$1" in
-    #display strength of signal in dBm and layman terms
-    signal=$(iwconfig wlan0 | sed -n 's/.*\(Signal level=-.*\)/\1/p' | sed -e 's/Signal level=//g' | sed -e 's/dBm//g')
-    signalStrength=$(iwconfig wlan0 | sed -n 's/.*\(Signal level=-.*\)/\1/p' | sed -e 's/Signal level=//g')
 
     "")
       #check if device has wifi

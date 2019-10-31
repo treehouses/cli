f5072e3c550bd8391f8c05360814693f603ef2ea
Iris B <irisbeharaj@gmail.com>
2019-10-30 23:42:26 -0400

    treehouses sshkey github (fixes #484) (#491)

--- modules/sshkey.sh
+++ modules/sshkey.sh
@@ -46,6 +46,57 @@ function sshkey () {
       rm /home/pi/.ssh/authorized_keys
     fi
     echo "all sshkeys are deleted."
+  elif [ "$1" == "github" ]; then
+    if [ -z "$2" ]; then
+      echo "Error: missing arguments"
+      echo "Usage: $(basename "$0") sshkey github <adduser|deleteuser|addteam>"
+      exit 1
+    fi
+    if [ "$2" == "adduser" ]; then
+      if [ -z "$3" ]; then
+        echo "Error: missing argument"
+        echo "Usage: $(basename "$0") sshkey adduser <username>"
+        exit 1
+      fi
+      keys=$(curl -s "https://github.com/$3.keys")
+      if [ ! -z "$keys" ]; then
+        keys=$(sed 's#$# '$3'#' <<< $keys)
+        sshkey add "$keys"
+      fi
+    elif [ "$2" == "deleteuser" ]; then
+      if [ -z "$3" ]; then
+        echo "Error: missing argument"
+        echo "Usage: $(basename "$0") sshkey deleteuser <username>"
+        exit 1
+      fi
+      githubusername="$3"
+      auth_files="/root/.ssh/authorized_keys /home/pi/.ssh/authorized_keys"
+      for file in $auth_files; do
+        if [ -f "$file" ]; then
+          if grep -q " $githubusername$" $file; then
+            sed -i "/ $githubusername$/d" $file
+	    echo "$githubusername's key(s) deleted from $file"
+          else
+            echo "$githubusername does not exist"
+          fi
+        else
+          echo "$file does not exist."
+        fi
+      done    
+    elif [ "$2" == "addteam" ]; then
+      if [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ]; then
+        echo "Error: missing arguments"
+        echo "Usage: $(basename "$0") sshkey github addteam <organization> <team_name> <access_token>"
+        exit 1
+      fi
+      teams=$(curl -s -X GET "https://api.github.com/orgs/$3/teams" -H "Authorization: token $5")
+      team_id=$(echo "$teams" | jq ".[] | select(.name==\"$4\").id")
+      members=$(curl -s -X GET "https://api.github.com/teams/$team_id/members" -H "Authorization: token $5" | jq ".[].login" -r)
+      while read -r member; do
+        sshkey github adduser "$member"
+      done <<< "$members"
+    fi
+#DEPRECATED####
   elif [ "$1" == "addgithubusername" ]; then
     if [ -z "$2" ]; then
       echo "Error: missing argument"
@@ -57,6 +108,8 @@ function sshkey () {
       keys=$(sed 's#$# '$2'#' <<< $keys)
       sshkey add "$keys"
     fi
+#############
+#DEPRECATED####
   elif [ "$1" == "deletegithubusername" ]; then
     if [ -z "$2" ]; then
       echo "Error: missing argument"
@@ -77,6 +130,8 @@ function sshkey () {
         echo "$file does not exist."
       fi
     done
+###############
+#DEPRECATED####
   elif [ "$1" == "addgithubgroup" ]; then
     if [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
       echo "Error: missing arguments"
@@ -89,9 +144,9 @@ function sshkey () {
     while read -r member; do
       sshkey addgithubusername "$member"
     done <<< "$members"
+###############
   fi
 }
-
 function sshkey_help () {
   echo ""
   echo "Usage: $(basename "$0") sshkey <add|list|delete|addgithubusername|addgithubgroup>"
@@ -111,14 +166,22 @@ function sshkey_help () {
   echo "  $(basename "$0") sshkey deleteall"
   echo "      Deletes all ssh keys"
   echo ""
+  echo "  $(basename "$0") sshkey github adduser|deleteuser <username>"
+  echo "      Downloads or deletes the public keys of the github username from/to the authorized_keys file."
+  echo ""
+  echo "  $(basename "$0") sshkey github addteam <organization> <team_name> <access_token>"
+  echo "      Downloads the public keys of the group members and adds them to authorized_keys"
+  echo "      A access_token is required to make this work, it can be generated in the following link"
+  echo "      https://github.com/settings/tokens"
+  echo ""
   echo "  $(basename "$0") sshkey addgithubusername <username>"
-  echo "      Downloads the public keys of the github username and adds them to authorized_keys"
+  echo "      (DEPRECATED) Downloads the public keys of the github username and adds them to authorized_keys"
   echo ""
   echo "  $(basename "$0") sshkey deletegithubusername <username>"
   echo "      Deletes all ssh keys related to this user"
   echo ""
   echo "  $(basename "$0") sshkey addgithubgroup <organization> <team_name> <access_token>"
-  echo "      Downloads the public keys of the group members and adds them to authorized_keys"
+  echo "      (DEPRECATED) Downloads the public keys of the group members and adds them to authorized_keys"
   echo "      A access_token is required to make this work, it can be generated in the following link"
   echo "      https://github.com/settings/tokens"
   echo ""

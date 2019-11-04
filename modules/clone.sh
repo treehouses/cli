1b99d9ccca8905694fd4fd121cfe3aa32738b58b
dogi <dogi@users.noreply.github.com>
2019-11-03 23:51:38 -0500

    Update clone.sh

--- modules/clone.sh
+++ modules/clone.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
 function clone {
-    device="$1"
-    if [ -z "$device" ]; then
-      device="/dev/sdb"
-    fi
+  device="$1"
+  if [ -z "$device" ]; then
+    device="/dev/sdb"
+  fi
 
-    a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
-    #echo "$a - /dev/mmcblk0"
+  a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
+  #echo "$a - /dev/mmcblk0"
 
-    b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
-    #echo "$b - /dev/sdb"
+  b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
+  #echo "$b - /dev/sdb"
 
-    if [ -z "$a" ] || [ -z "$b" ]; then
-      echo "Error: the device $device wasn't detected"
-      return 1
-    fi
+  if [ -z "$a" ] || [ -z "$b" ]; then
+    echo "Error: the device $device wasn't detected"
+    return 1
+  fi
 
-    if [ $b -lt $a ]; then
-      echo "Error: the device $device is not big enough"
-      return 1
-    fi
+  if [ $b -lt $a ]; then
+    echo "Error: the device $device is not big enough"
+    return 1
+  fi
 
-    if [ $a -eq $b ] || [ $a -lt $b ]; then
-      echo "copying...."
-      echo u > /proc/sysrq-trigger
-      dd if=/dev/mmcblk0 bs=1M of="$device"
-    fi
+  if [ $a -eq $b ] || [ $a -lt $b ]; then
+    echo "copying...."
+    echo u > /proc/sysrq-trigger
+    dd if=/dev/mmcblk0 bs=1M of="$device"
+  fi
 
-    echo ; echo "A reboot is needed to re-enable write permissions to OS."
-    echo "Rebooting in 5 seconds; ctrl+c to cancel reboot." && sleep 5 && reboot
+  echo ; echo "A reboot is needed to re-enable write permissions to OS."
 }
 
 function clone_help {

6f476479385f3fb99aefd131e186e4af7bc0d074
Xavier Elon Hollingsworth <Xavierh93@hotmail.com>
2019-11-13 23:37:02 -0500

    fixed trailing tabs

--- modules/clone.sh
+++ modules/clone.sh
@@ -26,41 +26,38 @@ function clone {
 		fi
 	fi
 
-		if [ -z "$device" ]; then
-			device="/dev/sdb"
-		fi
+	if [ -z "$device" ]; then
+		device="/dev/sdb"
+	fi
 
+	a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
+	#echo "$a - /dev/mmcblk0"
 
-		a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
-		#echo "$a - /dev/mmcblk0"
+	b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
+	#echo "$b - /dev/sdb"
 
-		b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
-		#echo "$b - /dev/sdb"
+	if [ -z "$a" ] || [ -z "$b" ]; then
+		echo "Error: the device $device wasn't detected"
+		return 1
+	fi
 
-		if [ -z "$a" ] || [ -z "$b" ]; then
-			echo "Error: the device $device wasn't detected"
-			return 1
-		fi
+	if [ $b -lt $a ]; then
+		echo "Error: the device $device is not big enough"
+		return 1
+	fi
 
-		if [ $b -lt $a ]; then
-			echo "Error: the device $device is not big enough"
-			return 1
-		fi
+	if [ $a -eq $b ] || [ $a -lt $b ]; then
+		echo "copying...."
+		echo u > /proc/sysrq-trigger
+		dd if=/dev/mmcblk0 bs=1M of="$device"
+	fi
 
-		if [ $a -eq $b ] || [ $a -lt $b ]; then
-			echo "copying...."
-			echo u > /proc/sysrq-trigger
-			dd if=/dev/mmcblk0 bs=1M of="$device"
-		fi
-
-
-
-		if [ $reboot ]; then
-			echo "Now rebooting..."
-			sudo reboot		
-		else
-			echo "A reboot is needed to re-enable write permissions to OS."
-		fi
+	if [ $reboot ]; then
+		echo "Now rebooting..."
+		sudo reboot		
+	else
+		echo "A reboot is needed to re-enable write permissions to OS."
+	fi
 }
 
 

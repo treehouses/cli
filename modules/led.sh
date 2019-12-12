dcf44565c4d56a42514ecc1ec33d753bde2a6e8c
pattanawadee88 <52762574+pattanawadee88@users.noreply.github.com>
2019-12-11 20:32:25 -0500

    remove treehouses led both (fixes #536) (#537)

--- modules/led.sh
+++ modules/led.sh
@@ -17,10 +17,6 @@ function led {
   elif [ "$color" = "red" ]; then
     led="$rLed"
     current="$currentRed"
-  elif [ "$color" = "both" ]; then
-    led green $trigger
-    led red $trigger
-    exit 0
   elif [ "$color" = "dance" ]; then
     checkroot
     dance > /dev/null
@@ -56,11 +52,7 @@ function led {
   fi
 
   if [ -z "$trigger" ]; then
-    if [ $led = $gLed ]; then
-      echo -e "$green: $currentGreen"
-    elif [ $led = $rLed ]; then
-      echo -e "$red: $currentRed"
-    fi
+     echo "$current"
   else
     checkroot
 
@@ -208,7 +200,7 @@ function newyear {
 
 function led_help {
   echo
-  echo "Usage: $(basename "$0") led [green|red|both] [mode]"
+  echo "Usage: $(basename "$0") led [green|red] [mode]"
   echo "       $(basename "$0") led [dance|thanksgiving|christmas|newyear]"
   echo
   echo "Sets or returns the led mode"
@@ -233,9 +225,6 @@ function led_help {
   echo "  $(basename "$0") led"
   echo "      This will return the status of the green/red (if present) leds"
   echo
-  echo "  $(basename "$0") led both"
-  echo "      This will return the status of both green/red (if present) leds"
-  echo
   echo "  $(basename "$0") led red"
   echo "      This will return the status of the red led"
   echo
@@ -244,9 +233,6 @@ function led_help {
   echo
   echo "  $(basename "$0") led red default-on"
   echo "      This will set the mode of the red led to default-on"
-  echo
-  echo "  $(basename "$0") led both none"
-  echo "      This will set the mode of the both leds to none"
   echo
   echo "  $(basename "$0") led dance"
   echo "      This will do a sequence with the green led"

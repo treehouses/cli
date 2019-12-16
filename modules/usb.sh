7180327e71bc7121b2495a9c8107134fd8e28163
kjong <justinkim532@gmail.com>
2019-12-16 12:57:09 -0800

    chmod

--- modules/usb.sh
+++ modules/usb.sh
@@ -4,19 +4,19 @@ function usb {
   command="$1"
 
   if [ "$command" = "on" ]; then
-    $TEMPLATES/hub-ctrl -h 2 -P 1 -p 1
-    $TEMPLATES/hub-ctrl -h 2 -P 2 -p 1
-    $TEMPLATES/hub-ctrl -h 2 -P 3 -p 1
-    $TEMPLATES/hub-ctrl -h 2 -P 4 -p 1
-    $TEMPLATES/hub-ctrl -h 1 -P 1 -p 1
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 1 -p 1
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 2 -p 1
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 3 -p 1
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 4 -p 1
+    chmod u+x $TEMPLATES/hub-ctrl -h 1 -P 1 -p 1
 
     echo "usb ports turned on"
   elif [ "$command" = "off" ]; then
-    $TEMPLATES/hub-ctrl -h 2 -P 1 -p
-    $TEMPLATES/hub-ctrl -h 2 -P 2 -p
-    $TEMPLATES/hub-ctrl -h 2 -P 3 -p
-    $TEMPLATES/hub-ctrl -h 2 -P 4 -p
-    $TEMPLATES/hub-ctrl -h 1 -P 1 -p
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 1 -p
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 2 -p
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 3 -p
+    chmod u+x $TEMPLATES/hub-ctrl -h 2 -P 4 -p
+    chmod u+x $TEMPLATES/hub-ctrl -h 1 -P 1 -p
 
     echo "usb ports turned off"
   else

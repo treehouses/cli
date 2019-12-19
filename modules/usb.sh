41a95cd948b3936f560a862b99c2d45dad65cb23
kjong <justinkim532@gmail.com>
2019-12-18 19:08:54 -0800

    add check for ethernet

--- modules/usb.sh
+++ modules/usb.sh
@@ -14,6 +14,19 @@ function usb {
 
     echo "usb ports turned on"
   elif [ "$command" = "off" ]; then
+    # check for connected ethernet
+    if [ `cat /sys/class/net/eth0/carrier` = "1" ]; then
+      read -p "The ethernet port on your Raspberry Pi is connected. Turning off usb power will interfere with your ethernet connection. Do you wish to continue?" yn
+      case $yn in
+        [Yy]*)
+          continue
+          ;;
+        [Nn]*)
+          exit
+          ;;
+      esac
+    fi
+
     $TEMPLATES/hub-ctrl -h 2 -P 1 -p
     $TEMPLATES/hub-ctrl -h 2 -P 2 -p
     $TEMPLATES/hub-ctrl -h 2 -P 3 -p

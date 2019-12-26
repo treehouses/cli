4dd7e69dac6e81b5ca23663551f9a6f52aaeb365
Nicholas Davis <nida8930@colorado.edu>
2019-12-25 19:14:14 -0600

    add mac subcommand to bluetooth.sh

--- modules/bluetooth.sh
+++ modules/bluetooth.sh
@@ -28,6 +28,11 @@ function bluetooth {
     restart_service bluealsa # restart the bluetooth audio service
 
     echo "Success: the bluetooth service has been switched to default, and the service has been stopped."
+  elif {"$status" = "mac" ]; then
+    macfile=/sys/kernel/debug/bluetooth/hci0/identity
+    macadd=$(cat ${macfile})
+    echo "${macadd:0:17}"
+
   else
     echo "Error: only 'on', 'off', 'pause' options are supported";
   fi

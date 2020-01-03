84ead95086bcb119455357cbe84c9d9dfd9cc26e
root <root@travis-job-b0798bd9-b736-440d-87e7-1a8ad2bfe77a>
2020-01-03 04:03:31 +0000

    add rpi3 mode

--- modules/usb.sh
+++ modules/usb.sh
@@ -3,38 +3,54 @@
 function usb {
   command="$1"
 
+  source detectrpi.sh
   chmod +x $TEMPLATES/hub-ctrl
 
-  if [ "$command" = "on" ]; then
-    $TEMPLATES/hub-ctrl -h 2 -P 1 -p 1
-    $TEMPLATES/hub-ctrl -h 2 -P 2 -p 1
-    $TEMPLATES/hub-ctrl -h 2 -P 3 -p 1
-    $TEMPLATES/hub-ctrl -h 2 -P 4 -p 1
-    $TEMPLATES/hub-ctrl -h 1 -P 1 -p 1
+  if [[ $(detectrpi) =~ '3' ]]; then
+    if [ "$command" = "on" ]; then
+      $TEMPLATES/hub-ctrl -h 1 -P 2 -p 1
 
-    echo "usb ports turned on"
-  elif [ "$command" = "off" ]; then
-    # check for connected ethernet
-    if [ "$(cat /sys/class/net/eth0/carrier)" = "1" ]; then
-      read -r -p "The ethernet port on your Raspberry Pi is connected. Turning off usb power will interfere with your ethernet connection. Do you wish to continue? Y or N" yn
-      case $yn in
-        [Yy]*)
-          ;;
-        [Nn]*)
-          exit
-          ;;
-      esac
+      echo "usb ports turned on"
+    elif [ "$command" = "off" ]; then
+      $TEMPLATES/hub-ctrl -h 1 -P 2 -p 0 
+
+      echo "usb ports turned off"
+    else
+      echo "unknown command"
     fi
 
-    $TEMPLATES/hub-ctrl -h 2 -P 1 -p
-    $TEMPLATES/hub-ctrl -h 2 -P 2 -p
-    $TEMPLATES/hub-ctrl -h 2 -P 3 -p
-    $TEMPLATES/hub-ctrl -h 2 -P 4 -p
-    $TEMPLATES/hub-ctrl -h 1 -P 1 -p
+  elif [[ $(detectrpi) =~ '4' ]]; then
+    if [ "$command" = "on" ]; then
+      $TEMPLATES/hub-ctrl -h 2 -P 1 -p 1
+      $TEMPLATES/hub-ctrl -h 2 -P 2 -p 1
+      $TEMPLATES/hub-ctrl -h 2 -P 3 -p 1
+      $TEMPLATES/hub-ctrl -h 2 -P 4 -p 1
+      $TEMPLATES/hub-ctrl -h 1 -P 1 -p 1
 
-    echo "usb ports turned off"
-  else
-    echo "unknown command"
+      echo "usb ports turned on"
+    elif [ "$command" = "off" ]; then
+      # check for connected ethernet
+      if [ "$(cat /sys/class/net/eth0/carrier)" = "1" ]; then
+        read -r -p "The ethernet port on your Raspberry Pi is connected. Turning off usb power will interfere with your ethernet connection. Do you wish to continue? Y or N" yn
+        case $yn in
+          [Yy]*)
+            ;;
+          [Nn]*)
+            exit
+            ;;
+        esac
+      fi
+
+      $TEMPLATES/hub-ctrl -h 2 -P 1 -p
+      $TEMPLATES/hub-ctrl -h 2 -P 2 -p
+      $TEMPLATES/hub-ctrl -h 2 -P 3 -p
+      $TEMPLATES/hub-ctrl -h 2 -P 4 -p
+      $TEMPLATES/hub-ctrl -h 1 -P 1 -p
+
+      echo "usb ports turned off"
+    else
+      echo "unknown command"
+    fi
   fi
 }
 

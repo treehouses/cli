a8a84f6ccf8f9af675ac5446d7b05cbe1b40a169
an pham <vincentphampham1993@gmail.com>
2020-01-10 22:20:07 -0500

    remove regex

--- modules/bridge.sh
+++ modules/bridge.sh
@@ -32,10 +32,6 @@ function bridge {
     fi
   fi
 
-  if [[ "$hotspotessid" =~ [^a-zA-Z_-] ]]; then
-    echo "Error: hotspot essid can only contains alphabet, dashes and underscores"
-    exit 1 
-  fi 
 
   if [ -n "$wifipassword" ];
   then

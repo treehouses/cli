e1a314ced94a32c0747f870e4a5474b582d92cf8
kjong <justinkim532@gmail.com>
2020-01-17 20:31:18 -0800

    docker name

--- modules/services.sh
+++ modules/services.sh
@@ -125,7 +125,7 @@ function services {
               ;;
             portainer)
               docker volume create portainer_data
-              docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer --name portainer
+              docker run --name portainer -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
               echo "portainer built and started"
               check_tor "9000"
               ;;

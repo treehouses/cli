a420319ff8f73f73478a39565b025a665936d487
sravyanittala <54302388+sravya71@users.noreply.github.com>
2020-01-31 18:50:58 +0530

    create potainer_yml.sh

---
+++ templates/services/portainer/portainer_yml.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#create portainer.yml
+
+mkdir -p /srv/portainer
+
+
+echo "services:"
+  echo "  portainer:"
+  echo "    image: treehouses/portainer"
+  echo "    ports:"
+  echo "      - \"9000:9000\""
+  echo "    volumes:"
+  echo "      - \"/var/run/docker.sock:/var/run/docker.sock\""
+  echo "      - \"portainer_data:/data portainer/portainer\""
+  echo "version: \"2\""
+} > /srv/portainer/portainer.yml

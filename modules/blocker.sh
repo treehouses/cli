e30e387d22b3dfc1d4aa23f68b84a6140f5de8d0
LordJashin32 <tristanlangford32@gmail.com>
2020-02-01 17:27:35 -0500

    never give up

--- modules/blocker.sh
+++ modules/blocker.sh
@@ -18,11 +18,11 @@ function apply_blocker {
       local down_url="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts"
       ;;      
   esac
-  local curr_hostn=hostname
-  cp "$TEMPLATES/hosts" $TEMPLATES/hosts1
-  sed -i "s/hostname/$curr_hostn/g" $TEMPLATES/hosts1
+  local curr_hostn=$(hostname)
+  cp "$TEMPLATES/hosts" "$TEMPLATES/hosts1"
+  sed -i "s/hostname/$curr_hostn/g" "$TEMPLATES/hosts1"
   cp "$TEMPLATES/hosts1" /etc/hosts
-  rm -f $TEMPLATES/hosts1
+  rm -f "$TEMPLATES/hosts1"
   if [ ! $BLOCKER = "0" ]; then
     wget -q "$down_url" -O - >> /etc/hosts
   fi

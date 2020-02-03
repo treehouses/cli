b163ec71a6b2637f771c5ca756d9e0232dcdcb34
kjong <justinkim532@gmail.com>
2020-02-03 13:07:07 -0800

    update autoruna and ymls

--- templates/services/nextcloud/nextcloud_yml.sh
+++ templates/services/nextcloud/nextcloud_yml.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 #create nextcloud.yml
-
 mkdir -p /srv/nextcloud/data
-
 {
- echo "services:"
- echo "  nextcloud:"
- echo "    image: treehouses/nextcloud"
- echo "    ports:"
- echo "      - \"8081:80\""
- echo "    volumes:"
- echo "      - \"/srv/nextcloud/data:/var/www/html/data\""
- echo "version: \"2\""
+  echo "services:"
+  echo "  nextcloud:"
+  echo "    image: treehouses/nextcloud"
+  echo "    ports:"
+  echo "      - \"8081:80\""
+  echo "    volumes:"
+  echo "      - \"/srv/nextcloud/data:/var/www/html/data\""
+  echo "version: \"2\""
 } > /srv/nextcloud/nextcloud.yml
-

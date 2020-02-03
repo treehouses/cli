961cd39bdb27f44a4057950828975e05b063fdc1
sravyanittala <54302388+sravya71@users.noreply.github.com>
2020-02-04 00:18:44 +0530

    Update nextcloud_yml.sh

--- templates/services/nextcloud/nextcloud_yml.sh
+++ templates/services/nextcloud/nextcloud_yml.sh
@@ -2,13 +2,16 @@
 
 #create nextcloud.yml
 
-mkdir -p /srv/nextcloud
+mkdir -p /srv/nextcloud/data
 
 {
  echo "services:"
  echo "  nextcloud:"
- echo "    image: nextcloud"
+ echo "    image: treehouses/nextcloud"
  echo "    ports:"
  echo "      - \"8081:80\""
+ echo "    volumes:"
+ echo "      - \"/srv/nextcloud/data:/var/www/html/data\""
  echo "version: \"2\""
 } > /srv/nextcloud/nextcloud.yml
+

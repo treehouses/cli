fb96823d58f5f6cceb11c240473bdcaca6317b72
Tristan <tristanlangford32@gmail.com>
2020-02-08 01:45:15 -0500

    Update blocker.sh

--- templates/blocker/blocker.sh
+++ templates/blocker/blocker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 # Credits: https://github.com/StevenBlack/hosts
 # The MIT License (MIT) Copyright © 2020 Steven Black
 # https://github.com/StevenBlack/hosts/blob/master/license.txt
+(cd $1 && ./blocker.sh)
 wget -q -O 1_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 wget -q -O 2_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts
 wget -q -O 3_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts

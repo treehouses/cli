1522de6d29bc028e5f94bd630f6f165cd6be7e78
Tristan <tristanlangford32@gmail.com>
2020-02-08 01:50:04 -0500

    Update blocker.sh

--- templates/blocker/blocker.sh
+++ templates/blocker/blocker.sh
@@ -2,9 +2,8 @@
 # Credits: https://github.com/StevenBlack/hosts
 # The MIT License (MIT) Copyright © 2020 Steven Black
 # https://github.com/StevenBlack/hosts/blob/master/license.txt
-(cd $1 && ./blocker.sh)
-wget -q -O 1_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-wget -q -O 2_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts
-wget -q -O 3_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts
-wget -q -O 4_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
-wget -q -O 5_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts
+wget -q -O ./templates/blocker/1_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+wget -q -O ./templates/blocker/2_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts
+wget -q -O ./templates/blocker/3_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts
+wget -q -O ./templates/blocker/4_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
+wget -q -O ./templates/blocker/5_hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts

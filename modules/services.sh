94064d9e00e1f49c9d5548d795f18930fb494385
sravyanittala <54302388+sravya71@users.noreply.github.com>
2020-02-27 11:55:19 +0530

    treehouses services ntopng with compose (fixes #669) (#674)
    Co-authored-by: dogi <dogi@users.noreply.github.com>
    Co-authored-by: Nida8930 <49969541+Nida8930@users.noreply.github.com>
    Co-authored-by: hiroTochigi <44413241+hiroTochigi@users.noreply.github.com>

--- modules/services.sh
+++ modules/services.sh
@@ -149,10 +149,10 @@ function services {
               check_tor "4000"
               ;;
             ntopng)            
-              docker volume create ntopng_data
-              docker run --name ntopng -d -p 8090:8090 -v /var/run/docker.sock:/var/run/docker.sock -v ntopng_data:/data jonbackhaus/ntopng --http-port=8090
-              echo "ntopng built and started"
-              check_tor "8090"
+              check_space "jonbackhaus/ntopng"
+              create_yml "ntopng"
+              docker_compose_up "ntopng"
+              check_tor "8084"
               ;;
             *)
               echo "unknown service"

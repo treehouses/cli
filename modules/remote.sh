4faa19db5d84d2b62b6bd6df32a9b6b2b5ed5ada
kjong <justinkim532@gmail.com>
2020-02-26 19:36:21 -0800

    update error message

--- modules/remote.sh
+++ modules/remote.sh
@@ -43,7 +43,7 @@ function remote {
     fi
   else
     echo "unknown command option"
-    echo "usage: $BASENAME remote [status | upgrade]"
+    echo "usage: $BASENAME remote [status | upgrade | services | version]"
   fi
 }
 

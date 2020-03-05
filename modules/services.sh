1daa370e06aaa0fd89a45730ef9f2021a425f296
dogi <dogi@users.noreply.github.com>
2020-03-05 00:20:57 -0500

    treehouses services couchdb (fixes #742) (#744)
    Co-authored-by: dogi <dogi@users.noreply.github.com>
    Co-authored-by: kjong <36876018+kjong@users.noreply.github.com>

--- modules/services.sh
+++ modules/services.sh
@@ -154,6 +154,12 @@ function services {
               docker_compose_up "ntopng"
               check_tor "8084"
               ;;
+            couchdb)
+              check_space "treehouses/couchdb"
+              create_yml "couchdb"
+              docker_compose_up "couchdb"
+              check_tor "5984"
+              ;;
             *)
               echo "unknown service"
               ;;
@@ -162,7 +168,7 @@ function services {
 
         down)
           case "$service_name" in
-            planet|kolibri|pihole|moodle|privatebin|nextcloud|portainer|netdata|mastodon|ntopng)
+            planet|kolibri|pihole|moodle|privatebin|nextcloud|portainer|netdata|mastodon|ntopng|couchdb)
               if [ ! -e /srv/${service_name}/${service_name}.yml ]; then
                 echo "yml file doesn't exit"
               else
@@ -178,7 +184,7 @@ function services {
 
         start)
           case "$service_name" in
-            planet|kolibri|pihole|moodle|privatebin|nextcloud|portainer|netdata|mastodon|ntopng)
+            planet|kolibri|pihole|moodle|privatebin|nextcloud|portainer|netdata|mastodon|ntopng|couchdb)
               if docker ps -a | grep -q $service_name; then
                 docker-compose -f /srv/${service_name}/${service_name}.yml start
                 echo "${service_name} started"
@@ -194,7 +200,7 @@ function services {
 
         stop)
           case "$service_name" in
-            planet|kolibri|pihole|moodle|privatebin|nextcloud|portainer|netdata|mastodon|ntopng)
+            planet|kolibri|pihole|moodle|privatebin|nextcloud|portainer|netdata|mastodon|ntopng|couchdb)
               if docker ps -a | grep -q $service_name; then
                 docker-compose -f /srv/${service_name}/${service_name}.yml stop
                 echo "${service_name} stopped"
@@ -350,6 +356,15 @@ function services {
               echo "Educational users can obtain commercial products at no cost please see here:"
               echo "https://www.ntop.org/support/faq/do-you-charge-universities-no-profit-and-research/\""
               ;;
+            couchdb)
+              echo "https://github.com/treehouses/rpi-couchdb"
+              echo "https://github.com/docker-library/docs/tree/master/couchdb"
+              echo
+              echo "\"Apache CouchDB lets you access your data where you need it by defining the"
+              echo "Couch Replication Protocol that is implemented by a variety of projects and products"
+              echo "that span every imaginable computing environment from globally distributed server-clusters,"
+              echo "over mobile phones to web browsers.\""
+              ;;
           esac
           ;;
 
@@ -496,6 +511,9 @@ function get_port {
     ntopng)
       echo "8090"
       ;;
+    couchdb)
+      echo "5984"
+      ;;
     *)
       echo "unknown service"
       ;;
@@ -516,6 +534,7 @@ function services_help {
   echo "  privatebin   PrivateBin is a minimalist, open source online pastebin"
   echo "  portainer    Portainer is a lightweight management UI for Docker environments"
   echo "  ntopng       Ntopng is a network traffic probe that monitors network usage"
+  echo "  couchdb      Apache CouchDB is an open-source document-oriented NoSQL database, implemented in Erlang."
   echo
   echo
   echo "Top-Level Commands:"

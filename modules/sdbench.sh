4d51f2bb838ba81f62846669d154c5d3e1618df2
LordJashin32 <tristanlangford32@gmail.com>
2020-03-05 07:37:33 +0300

    final redition

--- modules/sdbench.sh
+++ modules/sdbench.sh
@@ -1,13 +1,14 @@
 function sdbench {
-  DATAMB=${1:-128}
-  FILENM=~/test.dat
-  [ -f /flash/config.txt ] && CONFIG=/flash/config.txt || CONFIG=/boot/config.txt
-  trap 'rm -f "${FILENM}"' EXIT
-  WRCMD="rm -f ${FILENM} && sync && dd if=/dev/zero of=${FILENM} bs=1M count=${DATAMB} conv=fsync 2>&1 | grep -v records | sed 's/.*, //'"
-  RDCMD="echo 3 > /proc/sys/vm/drop_caches && sync && dd if=${FILENM} of=/dev/null bs=1M 2>&1 | grep -v records | sed 's/.*, //'"
-  WRRESULT="$(eval ${WRCMD})"
-  RDRESULT="$(eval ${RDCMD})"
-  echo "READ & WRITE - ${RDRESULT} & ${WRRESULT}"
+  local datamb filenm wrcmd rdcmd wrresult rdresult config
+  datamb=${1:-128}
+  filenm=~/test.dat
+  trap 'rm -f "${filenm}"' EXIT
+  wrcmd="rm -f ${filenm} && sync && dd if=/dev/zero of=${filenm} bs=1M count=${datamb} conv=fsync 2>&1 | grep -v records | sed 's/.*, //'"
+  rdcmd="echo 3 > /proc/sys/vm/drop_caches && sync && dd if=${filenm} of=/dev/null bs=1M 2>&1 | grep -v records | sed 's/.*, //'"
+  wrresult="$(eval ${wrcmd})"
+  rdresult="$(eval ${rdcmd})"
+  echo "read - ${rdresult}"
+  echo "write - ${wrresult}"
 }
 
 function sdbench_help {

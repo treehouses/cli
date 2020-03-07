ea41ded044cbf518552f24d11de50b7f75157838
Neethu-Mohan <neethumohan1212@gmail.com>
2020-03-07 13:29:51 +0000

    password error solved (fixes #754)

--- modules/password.sh
+++ modules/password.sh
@@ -1,6 +1,11 @@
 function password () {
+if [[ $1 == "" ]]
+then
+   log_and_exit1 "Error: Password not entered"
+else
   chpasswd <<< "pi:$1"
   echo "Success: the password has been changed"
+fi
 }
 
 function password_help () {

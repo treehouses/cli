fd53e27b4b3cd8745192bafdf90609a8565dccfa
Tristan <tristanlangford32@gmail.com>
2020-03-09 19:31:39 -0400

    treehouses bridge remove cat (#806)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/bridge.sh
+++ modules/bridge.sh
@@ -71,7 +71,7 @@ function bridge {
     wificountry="US"
     if [ -r /etc/rpi-wifi-country ];
     then
-      wificountry=$(cat /etc/rpi-wifi-country)
+      wificountry=$(</etc/rpi-wifi-country)
     fi
     echo "country=$wificountry"
   } > /etc/wpa_supplicant/wpa_supplicant.conf

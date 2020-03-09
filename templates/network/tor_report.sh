69c7e1dcaacae4865b849efeb840a168a4830b5e
lordjashin32 <tristanlangford32@gmail.com>
2020-03-09 22:28:00 +0300

    final

--- templates/network/tor_report.sh
+++ templates/network/tor_report.sh
@@ -10,7 +10,7 @@ function report {
 }
 
 prevStatus=$(cat /tmp/prev_tor_status 2>/dev/null || echo "offline")
-if ping -q -c 3 -W 1 1.1.1.1 >/dev/null && $(treehouses internet = "true"); then
+if ping -q -c 3 -W 1 www.gitter.im >/dev/null; then
   echo "rpi is online, was $prevStatus"
   if [ "$prevStatus" = "offline" ]; then
     echo "online" > /tmp/prev_tor_status

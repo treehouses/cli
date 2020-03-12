15fb0a968e8718b98863465f94bf2dc06efb38d5
kjong <justinkim532@gmail.com>
2020-03-11 17:27:54 -0700

    update

--- services/install-pihole.sh
+++ services/install-pihole.sh
@@ -30,7 +30,6 @@ function install {
     echo "    volumes:"
     echo "      - './etc-pihole/:/etc/pihole/'"
     echo "      - './etc-dnsmasq.d/:/etc/dnsmasq.d/'"
-    echo "    # run $(touch ./var-log/pihole.log) first unless you like errors"
     echo "    # - './var-log/pihole.log:/var/log/pihole.log'"
     echo "    dns:"
     echo "      - 127.0.0.1"

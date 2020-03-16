a3c427f337b2ec973beff541bb1f98a489dd737b
Anna X <vmnet8@gmail.com>
2020-03-16 06:12:50 +0000

    add container name for mariadb service

--- services/install-mariadb.sh
+++ services/install-mariadb.sh
@@ -10,6 +10,7 @@ function install {
     echo "services:"
     echo "  mariadb:"
     echo "    image: jsurf/rpi-mariadb"
+    echo "    container_name: rpi-mariadb"
     echo "    ports:"
     echo "      - 3306:3306"
     echo "    environment:"

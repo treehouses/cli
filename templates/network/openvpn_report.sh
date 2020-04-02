75bea52f0af07703369345990273df7dfa713ba5
Tristan <tristanlangford32@gmail.com>
2020-04-02 05:55:40 -0400

    `treehouses openvpn notice now` in report

--- templates/network/openvpn_report.sh
+++ templates/network/openvpn_report.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-function report {
-  while read -r channel; do
-    export gitter_channel="$channel"
-    treehouses feedback "$(treehouses openvpn | tail -n 1 | tr "\n" "#" | sed 's/#/\\n/g')vpn"
-  done < /etc/openvpn_report_channels.txt
-  echo "report"
-}
-
-
-
 prevStatus=$(cat /tmp/prev_openvpn_status 2>/dev/null || echo "offline")
 if ping -I tun0 -q -c 3 -W 1 1.1.1.1 >/dev/null; then
   echo "vpn is online, was $prevStatus"
   if [ "$prevStatus" = "offline" ]; then
     echo "online" > /tmp/prev_openvpn_status
-    report
+    treehouses openvpn notice now
   fi
 else
   echo "vpn is offline, was $prevStatus"

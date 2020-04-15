eda543ed672b57a0034e2c1a8254cb5fb3be2091
Tristan <tristanlangford32@gmail.com>
2020-04-15 18:59:47 -0400

    Update shutdown.sh

--- modules/shutdown.sh
+++ modules/shutdown.sh
@@ -8,21 +8,21 @@ function shutdown {
       /sbin/shutdown now
        ;;
     "now")
-      /sbin/shutdown now 
+      /sbin/shutdown now
       ;;
     "in")
       echo "Shutting down in $2 seconds. Press crtl+c to cancel"
       sleep "$2"
       /sbin/shutdown now
       ;;
-    "force") 
+    "force")
       /sbin/shutdown -f
       ;;
     *)
        echo "Error: only now, in and force options are supported"
-       ;;       
+       ;;
   esac
-} 
+}
 
 function shutdown_help {
   echo
@@ -36,14 +36,11 @@ function shutdown_help {
   echo
   echo "  $BASENAME shutdown now"
   echo "      System shutdown immediately"
-  echo 
+  echo
   echo "  $BASENAME shutdown in <time in seconds>"
   echo "      System will shutdown after the specified time. 'ctrl+c' to cancel"
-  echo 
+  echo
   echo "  $BASENAME shutdown force"
   echo "      System will force shutdown"
   echo
 }
-
-
-

511dbef5945ef90fd893aa39b8084e9916073c6c
root <root@travis-job-4d37ca42-4d75-470f-99cc-ef9f84dbd522>
2020-04-23 06:43:09 +0000

    modified convert.sh

--- modules/convert.sh
+++ modules/convert.sh
@@ -3,57 +3,28 @@ function convert {
   inputFile=$1
   outputFile=$2
   if [[ "$inputFile" != "" ]] && [[ "$outputFile" != "" ]]; then
-    inputFileType=${inputFile##*.}
-    outputFileType=${outFile##*.}
-    videoFileTypes=('mp4' 'avi' 'flv' 'wmv' 'mkv')
-    audioFileTypes=('mp3' 'wav' 'ogg')
-    for i in "${videoFileTypes[@]}"; do
-      if [ "$inputFileType" == $i ]; then
-        video
-      fi
-    done
-    for i in "${audioFileTypes[@]}"; do
-      if [ "$inputFileType" == $i ]; then
-        audio
-      fi
-    done
+    ffmpeg -i $inputFile $outputFile -hide_banner
+    status=$?
+    if [ "$status" == 0 ]; then
+      echo "$inputFile has been successfully converted to $outputFile"
+    else
+      echo "convertion unsuccessful"
+    fi
   elif [ $inputFile == "" ]; then
     echo "Error: no input file"
   elif [ $outputFile == "" ]; then
-    echo "Error :no otput file"
+    echo "Error: no output file"
   else
     convert_help
   fi
 }
-
-function video {
-  ffmpeg -i $inputFile $outputFile -hide_banner
-  status=$?https://github.com/treehouses/cli
-  if [ "$status" == 0 ]; then
-    echo "$inputFile has been successfully converted to $outputFile"
-  else
-    echo "convertion unsuccessful"
-  fi
-}
-
-function audio {
-  ffmpeg -i $inputFile $outputFile
-  status=$?
-  if [ "$status" == 0 ]; then
-    echo "$inputFile has been successfully converted to $outputFile"
-  else
-    echo "convertion unsuccessful"
-  fi
-}
-
-
 
 function convert_help {
   echo
   echo "Usage: $BASENAME convert"
   echo
   echo "Example:"
-  echo "  $BASENAME convert <video file input> <video file output>"
-  echo "      convert the input video file format to output video file format "
+  echo "  $BASENAME convert <input file format> <output file format>"
+  echo "      convert the input file format to output video format "
 }
 

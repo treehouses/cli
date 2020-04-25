088a1cec65b072720982bda9c5f81254b620d2d7
root <root@travis-job-a8a11abd-e324-4158-ac93-653cd844eb0c>
2020-04-24 17:15:30 -0700

    added detectwifi module

---
+++ modules/detectwifi.sh
@@ -0,0 +1,20 @@
+function detectwifi {
+  checkargn $# 0
+  if iwconfig wlan0 2>&1 | grep -q "No such device"; then
+    echo "false"
+  else 
+    echo "true"
+  fi
+}
+
+function detectwifi_help {
+  echo 
+  echo  "Usage: $BASENAME detectwifi"
+  echo 
+  echo  "Detects if wifi module is available and returns true or false"
+  echo
+  echo  "Example:"
+  echo  "$BASENAME detectwifi"
+  echo  "true"
+  echo
+}

56b0d2cba1b81db2950ad1b1250560f29f3aadfc
Tristan <tristanlangford32@gmail.com>
2020-04-27 18:30:31 -0400

    `treehouses upgrade bluetooth` (#1111)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/upgrade.sh
+++ modules/upgrade.sh
@@ -26,6 +26,15 @@ function upgrade {
       exit
     fi
     echo "true $last_version"
+  elif [ "$tag" == "bluetooth" ]; then
+    checkargn $# 1
+    checkroot
+    checkwrpi
+    checkinternet
+    cp /usr/local/bin/bluetooth-server.py "/usr/local/bin/bluetooth-server.py.$(date +'%Y%m%d%H%m%S')"
+    curl -s "https://raw.githubusercontent.com/treehouses/control/master/server.py" -o /usr/local/bin/bluetooth-server.py
+    bluetooth restart &>"$LOGFILE"
+    echo "Successfully updated and restarted bluetooth server"
   else
     npm install -g "@treehouses/cli@${tag}"
   fi
@@ -33,7 +42,7 @@ function upgrade {
 
 function upgrade_help {
   echo
-  echo "Usage: $BASENAME upgrade [tag] [--check]"
+  echo "Usage: $BASENAME upgrade [tag|bluetooth] [--check]"
   echo
   echo "Upgrades $BASENAME package using npm"
   echo
@@ -47,5 +56,8 @@ function upgrade_help {
   echo
   echo " $BASENAME upgrade --check"
   echo "    checks if there is a new version of the package, outputs false if there isnt, outputs true + version if there is"
+  echo
+  echo " $BASENAME upgrade bluetooth"
+  echo "    This will upgrade the bluetooth server to the latest if internet is available and restart bluetooth"
   echo
 }

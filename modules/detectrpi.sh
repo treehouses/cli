114c966986fe0db43e25b2877796c883430f7957
lordjashin32 <tristanlangford32@gmail.com>
2020-04-30 00:45:47 +0000

    add detectrpi back

---
+++ modules/detectrpi.sh
@@ -0,0 +1,14 @@
+function detectrpi_help {
+  echo
+  echo "Usage: $BASENAME detectrpi"
+  echo
+  echo "Detects the hardware version"
+  echo
+  echo "Example:"
+  echo "  $BASENAME detectrpi"
+  echo "      Prints the Raspberry Pi name"
+  echo
+  echo "  $BASENAME detectrpi model"
+  echo "      Prints the model number of the RPi"
+  echo
+}

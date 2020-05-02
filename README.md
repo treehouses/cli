54f327eccbc02a900c05d7bf32189c606ff05990
Lawrence Lin <35390215+law-lin@users.noreply.github.com>
2020-05-02 03:38:48 -0400

    `treehouses power` (fixes #1052) (#1147)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- README.md
+++ README.md
@@ -74,6 +74,7 @@ led [green|red] [mode]                    sets the led mode
     [dance|thanksgiving|christmas]
     [newyear|lunarnewyear|valentine]
     [carnival|stpatricks|random]
+power [mode|current|freq]                 sets the power mode or check power mode/CPU frequency
 rtc <on|off> [rasclock|ds3231]            sets up the rtc clock specified
 ntp <local|internet>                      sets rpi to host timing locally or to get timing from a remote server
 networkmode                               outputs the current network mode
--- modules/help.sh
+++ modules/help.sh
@@ -45,7 +45,8 @@ Usage: treehouses
    led [green|red] [mode]                    sets the led mode
        [dance|thanksgiving|christmas]
        [newyear|lunarnewyear|valentine]
-       [carnival|stpatricks|random]
+       [carnival|stpatricks|random] 
+   power [freq |<mode>]                      sets the power scaling or check CPU frequency
    rtc <on|off> [rasclock|ds3231]            sets up the rtc clock specified
    ntp <local|internet>                      sets rpi to host timing locally or to get timing from a remote server
    networkmode                               outputs the current network mode

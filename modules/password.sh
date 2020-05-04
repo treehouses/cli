e2a82782063758eedaa898942dbc47cc2e5bd748
law-lin <lawrencelin101@gmail.com>
2020-05-04 15:17:55 -0400

    add disable/enable password functions

--- modules/password.sh
+++ modules/password.sh
@@ -1,23 +1,41 @@
-function password () {
+function password {
   checkrpi
   checkroot
   checkargn $# 1
   if [[ $1 == "" ]]; then
     log_and_exit1 "Error: Password not entered"
+  elif [ $1 == "disable" ]; then
+    disablepassword 
+  elif [ $1 == "enable" ]; then
+    enablepassword
   else
     chpasswd <<< "pi:$1"
     echo "Success: the password has been changed"
   fi
 }
 
+function disablepassword {
+  checkroot
+  passwd -l pi
+}
+
+function enablepassword {
+  checkroot
+  passwd -u pi
+}
+
 function password_help () {
   echo
   echo "Usage: $BASENAME password <password>"
+  echo "       $BASENAME password [disable|enable]"
   echo
   echo "Changes the password for 'pi' user"
   echo
   echo "Example:"
   echo "  $BASENAME password ABC"
   echo "      Sets the password for 'pi' user to 'ABC'."
-  echo
+  echo "  $BASENAME password disable"
+  echo "      Disables password authentication (only passphrase allowed)"
+  echo "  $BASENAME password enable"
+  echo "      Enables password authentication (password or passphrase allowed)"
 }

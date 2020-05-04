b072a386173fb941a10c40029684f5f50f98862c
root <root@travis-job-4d37ca42-4d75-470f-99cc-ef9f84dbd522>
2020-05-04 21:55:14 +0000

    help

--- modules/services.sh
+++ modules/services.sh
@@ -590,7 +590,8 @@ function services_help {
   echo "Service-Specific Commands:"
   echo
   echo "  Usage:"
-  echo "    $BASENAME services <service_name> install"
+  echo "    $BASENAME services <service_name>"
+  echo "                             ..... install"
   echo "                             ..... up"
   echo "                             ..... down"
   echo "                             ..... start"
@@ -605,6 +606,8 @@ function services_help {
   echo "                             ..... cleanup"
   echo "                             ..... icon"
   echo "                             ..... environment [edit [vim]]"
+  echo
+  echo "    <>                      shows overview of <service_name>"
   echo
   echo "    install                 installs and pulls <service_name>"
   echo

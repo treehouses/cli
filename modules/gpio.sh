0f16a896881e5d9a8f2f34dc1233f873afb88200
root <root@travis-job-a8a11abd-e324-4158-ac93-653cd844eb0c>
2020-05-04 11:38:06 -0700

    cleaner code

--- modules/gpio.sh
+++ modules/gpio.sh
@@ -6,7 +6,7 @@ function gpio {
   oldrpi="True"
   if [ ${#model} -gt 3 ]; then
     modelnum="${model:3:1}"
-    if [ "$modelnum" = "3" ] || [ "$modelnum" = "4" ] || [ "$modelnum" = "Z" ]; then
+    if [ "$(($modelnum))" -gt 2 ] || [ "$modelnum" = "Z" ]; then
       oldrpi="False"
     fi
   fi

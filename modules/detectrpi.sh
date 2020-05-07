e315e433c59962fd8bccc990fd6c37f41c318634
dogi <dogi@users.noreply.github.com>
2020-05-06 20:58:50 -0400

    Update detectrpi.sh

--- modules/detectrpi.sh
+++ modules/detectrpi.sh
@@ -1,17 +1,2 @@
-function detectrpi_help {
-  echo
-  echo "Usage: $BASENAME detectrpi"
-  echo
-  echo "Detects the hardware version"
-  echo
-  echo "Example:"
-  echo "  $BASENAME detectrpi"
-  echo "      Prints the Raspberry Pi name"
-  echo
-  echo "  $BASENAME detectrpi model"
-  echo "      Prints the model number of the RPi"
-  echo
-  echo "  $BASENAME detectrpi full"
-  echo "      Prints the full model of the RPi"
-  echo
-}
+# deprecated file which in combination with the loader
+# is making `treehouses detectrpi` happening

f83f2ea9fb19cb437cfb9b595430f6d5ac32e241
law-lin <lawrencelin101@gmail.com>
2020-05-06 20:05:56 -0400

    script for throttling

--- modules/power.sh
+++ modules/power.sh
@@ -13,6 +13,9 @@ function power {
       checkroot
       echo "Power mode is currently $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
       ;;
+    status)
+      checkroot
+      checkstatus
     default | ondemand)
       checkroot
       echo "Moves speed from min to max at about 90% load"
@@ -65,6 +68,49 @@ function changegovernor {
   fi
 }
 
+function checkstatus {
+  UNDERVOLTED=0x1
+  CAPPED=0x2
+  THROTTLED=0x4
+  HAS_UNDERVOLTED=0x10000
+  HAS_CAPPED=0x20000
+  HAS_THROTTLED=0x40000
+
+  #Text Colors
+  GREEN=`tput setaf 2`
+  RED=`tput setaf 1`
+  NC=`tput sgr0`
+
+  #Output Strings
+  GOOD="${GREEN}NO${NC}"
+  BAD="${RED}YES${NC}"
+
+  #Get Status, extract hex
+  STATUS=$(vcgencmd get_throttled)
+  STATUS=${STATUS#*=}
+
+  echo -n "Status: "
+  (($STATUS!=0)) && echo "${RED}${STATUS}${NC}" || echo "${GREEN}${STATUS}${NC}"
+
+  echo "Undervolted:"
+  echo -n "   Now: "
+  ((($STATUS&UNDERVOLTED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+  echo -n "   Run: "
+  ((($STATUS&HAS_UNDERVOLTED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+
+  echo "Throttled:"
+  echo -n "   Now: "
+  ((($STATUS&THROTTLED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+  echo -n "   Run: "
+  ((($STATUS&HAS_THROTTLED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+
+  echo "Frequency Capped:"
+  echo -n "   Now: "
+  ((($STATUS&CAPPED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+  echo -n "   Run: "
+  ((($STATUS&HAS_CAPPED)!=0)) && echo "${BAD}" || echo "${GOOD}"
+    vcgencmd get_throttled
+}
 function power_help {
   echo "Usage: $BASENAME power [mode]"
   echo "       $BASENAME power [current|freq]"

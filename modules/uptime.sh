a8bbcc856860f503d4aa2975ea4f5ece26d8ecb7
law-lin <lawrencelin101@gmail.com>
2020-05-09 00:33:57 -0400

    fix line 5

--- modules/uptime.sh
+++ modules/uptime.sh
@@ -2,7 +2,8 @@ function uptime {
   checkrpi
   checkargn $# 0
 
-  if [$(dpkg-query -W -f='${Status}' uptimed 2>/dev/null | grep -c "ok installed") -eq 0]; then
+  if [ $(dpkg-query -W -f='${Status}' uptimed 2>/dev/null | grep -c "ok installed") -eq 0]; 
+  then
     sudo apt-get install uptimed
     uptime
   else

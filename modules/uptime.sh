143c45fd75b2d0113911284b8df66ad06b4b3e97
law-lin <lawrencelin101@gmail.com>
2020-05-09 00:47:12 -0400

    add uptime -s

--- modules/uptime.sh
+++ modules/uptime.sh
@@ -1,22 +1,30 @@
 function uptime {
   checkrpi
-  checkargn $# 0
-
-  if ! dpkg-query -W -f='${Status}' uptimed | grep -q "ok installed";
-  then
-    sudo apt-get install uptimed
-    command uptime 
+  checkargn $# 1
+  if [[ $1 == "" ]]; then
+    if ! dpkg-query -W -f='${Status}' uptimed | grep -q "ok installed";
+    then
+        sudo apt-get install uptimed
+        command uptime 
+    else
+        command uptime
+    fi
+  elif [[ $1 == "boot"]]; then
+    command uptime -s
   else
-    command uptime
-  fi
+    echo "Unknown operation provided."  
+  fi  
 }
 
 function uptime_help {
-  echo "Usage: $BASENAME uptime"
+  echo "Usage: $BASENAME uptime [boot]"
   echo
   echo "Example:"
   echo "  $BASENAME uptime"
   echo "      This returns the uptime of the Raspberry Pi"
+  echo
+  echo "  $BASENAME uptime boot"
+  echo "      This returns when the Raspberry Pi was booted"
   echo
 
 }

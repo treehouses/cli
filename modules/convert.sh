3c89478a57c36f00bb458a40d7970685804676a7
lordjashin32 <tristanlangford32@gmail.com>
2020-05-14 06:50:38 -0400

    progress bar and better output

--- modules/convert.sh
+++ modules/convert.sh
@@ -1,21 +1,24 @@
 function convert {
+  local frames percent status
   checkargn $# 2
   inputFile=$1
   outputFile=$2
-  if [[ "$inputFile" != "" ]] && [[ "$outputFile" != "" ]]; then
-    ffmpeg -i $inputFile $outputFile -hide_banner -max_error_rate 0.0 &>"$LOGFILE"
-    status=$?
+  if [ -e "$inputFile" ] && [[ "$outputFile" != "" ]]; then
+    frames=$(ffprobe -v error -select_streams v:0 \
+      -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 $inputFile)
+    while read -r line || { status=$line && break; }; do
+      percent=$(bc <<<"scale=2; ($line / $frames) * 100")
+      echo -ne "  [$percent%] completed"\\r
+    done < <(ffmpeg -y -i $inputFile $outputFile -loglevel quiet \
+               -hide_banner -max_error_rate 0.0 -progress - -nostats \
+               | grep -oP --line-buffered '(?<=frame=)[0-9]+'; printf $?)
     if [ "$status" == 0 ]; then
       echo "$inputFile has been successfully converted to $outputFile"
     else
       echo "conversion unsuccessful"
     fi
-  elif [ $inputFile == "" ]; then
-    echo "Error: no input file"
-  elif [ $outputFile == "" ]; then
-    echo "Error: no output file"
   else
-    convert_help
+    echo "Error: invalid arguments"
   fi
 }
 

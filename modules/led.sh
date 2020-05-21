e3740eae500d3f1fd31d9a6d9b0b0cd8a8635b4a
darthnoward <me@darthnoward.com>
2020-05-21 09:17:53 +0800

    fixes tabbing syntax error

--- modules/led.sh
+++ modules/led.sh
@@ -134,18 +134,18 @@ function led {
      easter > "$LOGFILE"
      ;;
     lantern)
-        checkroot
-        echo "leds are set to lantern mode."
-        echo "Look at your RPi leds, both leds will be in this pattern..."
-        echo "Both LED: off 1 sec"
-        echo "Green LED: blink thrice"
-        echo "Red LED: off 0.5 sec, on 4 sec"
-        echo "Green LED: on 0.25 sec, off 0.25 sec"
-        echo "loop last step 3 times"
-        echo "Green LED: on 0.0125 sec, off 0.125 sec"
-        echo "loop last step 3 times"
-       lantern > "$LOGFILE"
-       ;;
+      checkroot
+      echo "leds are set to lantern mode."
+      echo "Look at your RPi leds, both leds will be in this pattern..."
+      echo "Both LED: off 1 sec"
+      echo "Green LED: blink thrice"
+      echo "Red LED: off 0.5 sec, on 4 sec"
+      echo "Green LED: on 0.25 sec, off 0.25 sec"
+      echo "loop last step 3 times"
+      echo "Green LED: on 0.0125 sec, off 0.125 sec"
+      echo "loop last step 3 times"
+     lantern > "$LOGFILE"
+      ;;
     random)
       checkroot
       random
@@ -541,41 +541,41 @@ function easter {
   led green "$current_green"
 }
 
-function lantern{
-    currentGreen = $(led "green")
-    currentRed = $(led "red")
+function lantern {
+  current_green=$(led "green")
+  current_red=$(led "red")
 
-    set_brightness 0 0; set_brightness 0 1
-    sleep 1
-    led green timer
-    sleep 3
-    led green none
-    sleep 0.5
-    set_brightness 1 1
-    sleep 4
-    set_brightness 1 0
+  set_brightness 0 0; set_brightness 0 1
+  sleep 1
+  led green timer
+  sleep 3
+  led green none
+  sleep 0.5
+  set_brightness 1 1
+  sleep 4
+  set_brightness 1 0
 
-    x=1
-    while [ $x -l 5 ]
-    do
-        set_brightness 0 1
-        sleep 0.25
-        set_brightness 0 0
-        sleep 0.25
-        x=$(( x+1 ))
+  x=1
+  while [ $x -l 5 ]
+  do
+    set_brightness 0 1
+    sleep 0.25
+    set_brightness 0 0
+    sleep 0.25
+    x=$(( x+1 ))
     done
 
-    while [ $x -l 9 ]
-    do
-        set_brightness 0 1
-        sleep 0.125
-        set_brightness 0 0
-        sleep 0.125
-        x=$(( x+1 ))
-    done
+  while [ $x -l 9 ]
+  do
+    set_brightness 0 1
+    sleep 0.125
+    set_brightness 0 0
+    sleep 0.125
+    x=$(( x+1 ))
+  done
 
-    led red "$current_red"
-    led green "$current_green"
+  led red "$current_red"
+  led green "$current_green"
 }
 
 function random {

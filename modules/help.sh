049c4e5f458cbe9654d62629cbdf39c543c19d2b
jayala-29 <jayala@ucsd.edu>
2020-05-21 22:12:46 -0700

    Update help.sh

--- modules/help.sh
+++ modules/help.sh
@@ -109,7 +109,6 @@ gpio                                      displays raspberry pi model and corres
 changelog                                 displays the most recent changes to treehouses
 shutdown [now|in|force]                   shutdown the system
 magazine magpi [all|latest|number]        downloads specific magpi issue as a pdf
-
 EOF
   echo "$helpdefault"
 }

04c1a353dc5f64b4d98a8d9606553abc03b09ff1
sravyanittala <54302388+sravya71@users.noreply.github.com>
2020-05-25 23:45:44 +0530

    Update services.sh

--- modules/services.sh
+++ modules/services.sh
@@ -624,6 +624,7 @@ function services_help {
   echo "  musicblocks     Music Blocks is a programming language and collection of manipulative tools for exploring musical and mathematical concepts in an integrative and fun way." 
   echo "  minetest        Minetest is an open source infinite-world block sandbox game engine with survival and crafting"
   echo "  invoiceninja    Invoiceninja is the leading self-host platform to create invoices."
+  echo "  domoticz        Domoticz is a home automation system that lets you monitor and configure various devices"
   echo
   echo
   echo "Top-Level Commands:"

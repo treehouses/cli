7d4033f9b9869a9b7133ed065102fd76e6c6f6c5
darthnoward <me@darthnoward.com>
2020-05-26 00:00:58 +0800

    fix wificountry not set when default network

--- modules/default.sh
+++ modules/default.sh
@@ -48,7 +48,7 @@ function default_network {
   rm -rf /etc/network/up-bridge.sh
   rm -rf /etc/network/eth0-shared.sh
   rm -rf /etc/network/mode
-  if [ ! -z "$(wificountry | grep not)" ]; then
+  if [ -z "$(wificountry | grep not)" ]; then
     config delete WIFICOUNTRY
   fi
 

ea5baec552413f3f42e7a0ab43d815f9af92f44a
law-lin <lawrencelin101@gmail.com>
2020-05-27 21:41:18 -0400

    add escape to dollar signs

--- services/install-grocy.sh
+++ services/install-grocy.sh
@@ -16,7 +16,7 @@ services:
       - TZ=\${TZ}
       - PASSWORD=\${PASSWORD}
       - DB_TYPE=\${DB_TYPE}
-      - DB_NAME=${DB_NAME}
+      - DB_NAME=\${DB_NAME}
       - DB_HOSTNAME=\${DB_HOSTNAME}
       - DB_USERNAME=\${DB_USERNAME}
       - DB_PASSWORD=\${DB_PASSWORD}

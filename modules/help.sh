b40fe49997ff98314d916e0db666f58835269300
root <root@travis-job-a8a11abd-e324-4158-ac93-653cd844eb0c>
2020-05-27 18:39:32 -0700

    hackerspace magazine

--- modules/help.sh
+++ modules/help.sh
@@ -107,7 +107,8 @@ inspire [joke|qotd|random]                displays quote based on user input
 convert <input file> <output file>        converts video and audio files 
 gpio                                      displays raspberry pi model and corresponding GPIO ports
 changelog                                 displays the most recent changes to treehouses
-magazine magpi [all|latest|number]        downloads specific magpi issue as a pdf
+magazine <hackspace|magpi> [all|latest]   downloads specific magazine issue as a pdf based on user input
+                           [number]       
 shutdown [now|in|force]                   shutdown the system
 EOF
   echo "$helpdefault"

31470f686b680679b4fbb7c376c69333dc63acc8
jayala-29 <jayala@ucsd.edu>
2020-05-30 01:29:07 -0700

    `treehouses magazine wireframe` (fixes #1373) (#1375)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/magazine.sh
+++ modules/magazine.sh
@@ -170,8 +170,89 @@ function magazine() {
       echo "All current issues of hackspace are saved in the $magtype directory"
       cd ..
     fi
+  elif [ "$magtype" = "wireframe" ]; then
+    if [ "$req" != "all" ]; then
+      if [ "$req" = "" ]; then
+        echo "Wireframe is a new fortnightly magazine that lifts the lid on video games. In every issue, we'll be looking at how games are made, who makes them, and even guide you through the process of making your own."
+        exit 0
+      fi
+      wget "https://wireframe.raspberrypi.org/issues"
+      mv ./issues ./issues.txt
+      latest="$(sed -n '186p' issues.txt)"
+      rm ./issues.txt
+      latest=${latest:25}
+      quoteloc="${latest%%\"*}"
+      ind=${#quoteloc}
+      latest=${latest:0:$ind}
+      if [ "$req" != "latest" ] && [ "$req" != "" ]; then
+        re='^[0-9]+$'
+        if ! [[ $req =~ $re ]] || [[ $req -lt 1 ]] || [[ $req -gt $latest ]]; then
+          echo "ERROR: Please enter a valid magazine number"
+          echo "       This can be any issue ranging from 1 to $latest" 
+          exit 1
+        fi
+        magnum=$req
+      fi
+      if [ "$req" = "latest" ]; then
+        magnum=$latest
+      fi
+      if [ ! -d "$magtype" ]; then
+        mkdir $magtype
+      fi
+      cd $magtype || return
+      if [ -f "Wireframe$magnum.pdf" ]; then
+        echo "Wireframe$magnum.pdf already exists, exiting..."
+        cd ..
+        exit 1
+      fi
+      echo "Fetching Wireframe$magnum.pdf..."
+      wget "https://wireframe.raspberrypi.org/issues/$magnum/pdf"
+      mv ./pdf ./pdf.txt
+      url="$(sed -n '10p' pdf.txt)"
+      rm ./pdf.txt
+      url=${url:44}
+      quoteloc="${url%%\"*}"
+      ind=${#quoteloc}
+      url=${url:0:$ind}
+      wget -bqc -O "Wireframe$magnum.pdf" $url
+      echo "Finished downloading Wireframe$magnum.pdf"
+      echo "Issue $magnum is saved in the $magtype directory"
+      cd ..
+    else
+      if [ ! -d "$magtype" ]; then
+        mkdir $magtype
+      fi
+      cd $magtype || return
+      wget "https://wireframe.raspberrypi.org/issues"
+      mv ./issues ./issues.txt
+      latest="$(sed -n '186p' issues.txt)"
+      rm ./issues.txt
+      latest=${latest:25}
+      quoteloc="${latest%%\"*}"
+      ind=${#quoteloc}
+      latest=${latest:0:$ind}
+      echo "Fetching all Wireframe magazines..."
+      for i in $(seq 1 $latest);
+      do
+        if [ -f "Wireframe$i.pdf" ]; then
+          continue
+        fi
+        wget "https://wireframe.raspberrypi.org/issues/$i/pdf"
+        mv ./pdf ./pdf.txt
+        url="$(sed -n '10p' pdf.txt)"
+        rm ./pdf.txt
+        url=${url:44}
+        quoteloc="${url%%\"*}"
+        ind=${#quoteloc}
+        url=${url:0:$ind}
+        wget -bqc -O "Wireframe$i.pdf" $url
+      done
+      echo "All current issues of wireframe are saved in the $magtype directory"
+      cd ..
+    fi
+
   else
-    echo "Please specify a valid magazine type, these include: magpi, hackspace"
+    echo "Please specify a valid magazine type, these include: magpi, hackspace, wireframe"
   fi
 }
 
@@ -179,7 +260,7 @@ function magazine_help {
   echo
   echo "  Usage:"
   echo
-  echo "    $BASENAME magazine <hackspace|magpi> [all|latest|number]"
+  echo "    $BASENAME magazine <hackspace|magpi|wireframe> [all|latest|number]"
   echo "        This downloads the specified issue of a magazine as a pdf with filename <mag_type>#.pdf based on user input"
   echo
   echo "  Examples:"

0e31a73c5699705edb663d47258f66ddf37f4def
kjong <36876018+kjong@users.noreply.github.com>
2020-06-19 14:47:32 -0700

    `treehouses help sshtunnel` improve (fixes #1473) (#1474)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/sshtunnel.sh
+++ modules/sshtunnel.sh
@@ -616,4 +616,73 @@ function sshtunnel_help {
   echo "      off                                      turns off auto-reporting to gitter"
   echo "      now                                      immediately reports to gitter"
   echo
+  echo "Adding a port using offsets:"
+  echo "  To add local port 100 with an offset of 200, run"
+  echo "      '$BASENAME sshtunnel add port offset 100 200 [host]'"
+  echo
+  echo "  The script will add an offset of 200 to the port interval for [host] and insert"
+  echo "  into /etc/tunnel"
+  echo "      '-R (port interval + 200):127.0.1.1:100 \\'"
+  echo
+  echo "  Resulting in"
+  echo "      Ports:"
+  echo "           local    ->   external"
+  echo "          ┌─ 100    ->     port interval + 200"
+  echo "          └─── Host: user@host # port interval"
+  echo
+  echo "Adding a port directly:"
+  echo "  To add local port 100 with external port 20000, run"
+  echo "      '$BASENAME sshtunnel add port actual 100 20000 [host]'"
+  echo
+  echo "  The script will directly insert into /etc/tunnel"
+  echo "      '-R 20000:127.0.1.1:100 \\'"
+  echo
+  echo "  Resulting in"
+  echo "      Ports:"
+  echo "           local    ->   external"
+  echo "          ┌─ 100    ->     20000"
+  echo "          └─── Host: user@host # port interval"
+  echo
+  echo "Removing a port:"
+  echo "  Ports to be removed are specified by their external port"
+  echo "      Ports:"
+  echo "           local    ->   external"
+  echo "          ┌─ 22     ->     20022"
+  echo "          ├─ 80     ->     20080"
+  echo "     ┌──> ├─ 2200   ->     20082"
+  echo "     │    └─── Host: user@host # port interval"
+  echo "     │"
+  echo "     └── to remove this port, run"
+  echo "      '$BASENAME sshtunnel remove port 20082 host'"
+  echo
+  echo "  Resulting in"
+  echo "      Ports:"
+  echo "           local    ->   external"
+  echo "          ┌─ 22     ->     20022"
+  echo "          ├─ 80     ->     20080"
+  echo "          └─── Host: user@host # port interval"
+  echo
+  echo "Adding a host:"
+  echo "  To add a host with port interval 12345, run"
+  echo "      '$BASENAME sshtunnel add host 12345 [host]'"
+  echo
+  echo "  This will add the default list of ports starting from port interval 12345"
+  echo
+  echo "  Resulting in"
+  echo "      Ports:"
+  echo "           local    ->   external"
+  echo "          ┌─ 22     ->     12367"
+  echo "          ├─ 80     ->     12425"
+  echo "          ├─ 2200   ->     12427"
+  echo "          └─── Host: user@host # 12345"
+  echo
+  echo "  If the monitoring port or monitoring port + 1 is currently in use by any other host,"
+  echo "  the monitoring port will be incremented by 1 until the two ports are clear."
+  echo
+  echo "Removing a host"
+  echo "  To remove a host, run"
+  echo "      '$BASENAME sshtunnel remove host <host>'"
+  echo
+  echo "  This will remove all current ports to the given host"
+  echo
 }

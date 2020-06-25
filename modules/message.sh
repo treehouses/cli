555fd973f5f78860941f39ddb4c9e4ae7eb9a694
root <wardah.b6@gmail.com>
2020-06-25 02:51:48 +0000

    update

--- modules/message.sh
+++ modules/message.sh
@@ -4,15 +4,15 @@ function message {
   token="$1"
   #echo $2
   #channeluri="$2"
-  channel="$2"
+  channelid="$2"
   shift
   shift
   message="$*" 
   if ! [[ -z "$message" ]]; then
     body="{\"text\":\"\n$message\"}"
    #echo $token
-   #echo $channel
-
+   #echo $channelid
+   channel=https://api.gitter.im/v1/rooms/$channelid/chatMessages
     curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $token"   "$channel" -d  "$body"> "$LOGFILE"
     echo "Thanks for the feedback!"
   else
@@ -22,12 +22,16 @@ function message {
 
 function message_help {
   echo
-  echo "Usage: $BASENAME message <token> <channel> <message>"
+  echo "Usage: $BASENAME message <token> <channelid> <message>"
+  echo
+  echo "You can get your token from https://developer.gitter.im/docs/welcome by signing in, it should show up immediately or by navigating to https://developer.gitter.im/apps"
+  echo
+  echo "You can get the id of the channel you want to message by going to https://api.gitter.im/v1/rooms?access_token=<your token> and find your channel id labelled \"id\": in the json response"
   echo
   echo "Shares feedback with the developers"
   echo
   echo "Example:"
-  echo "  $BASENAME message <token> <channel> \"Hi, you are very awesome\""
+  echo "  $BASENAME message <token> <channelid> \"Hi, you are very awesome\""
   echo "     Sends a message to gitter channel" 
   echo
 }

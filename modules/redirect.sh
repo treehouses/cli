f10c3fa5b5d170f550480187e8c29b2e9632657b
darthnoward <hhl055959@gmail.com>
2020-06-25 14:15:07 +0800

    fix code style

--- modules/redirect.sh
+++ modules/redirect.sh
@@ -9,25 +9,25 @@ function redirect {
   case "$1" in
     list)
         checkargn $# 1 
-        ls /etc/dnsmasq.d/ | grep -v README
+        ls /etc/dnsmasq.d/ --ignore="README"
         ;;
     add)
         checkargn $# 2
         echo "address=/$2/127.0.1.1" > /etc/dnsmasq.d/$2
         systemctl restart dnsmasq.service
+        echo "$2 added."
         ;;
     remove)
         checkargn $# 2
-        if ls /etc/dnsmasq.d/ | grep -q $2; then
-          for i in $(ls /etc/dnsmasq.d/ | grep -v README)
-          do
-            if [ "$i" == "$2" ]; then
-              rm -rf /etc/dnsmasq.d/$2
-              systemctl restart dnsmasq.service
-              exit 0
-            fi
-          done
-        fi 
+        for i in $(ls /etc/dnsmasq.d/ --ignore="README")
+        do
+          if [ "$i" == "$2" ]; then
+            rm -rf /etc/dnsmasq.d/$2
+            systemctl restart dnsmasq.service
+            echo "$2 removed."
+            exit 0
+          fi
+        done
         echo "$2 not found!"
         redirect_help
         exit 1
@@ -44,4 +44,5 @@ function redirect_help {
   echo
   echo "Usage:  $BASENAME redirect [list|add|remove] <url>"
   echo
+  echo 
 }

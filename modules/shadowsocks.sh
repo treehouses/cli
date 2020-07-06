b108c12a8f33cedf1e3da1ba7d079b644625da4d
darthnoward <hhl055959@gmail.com>
2020-07-06 23:30:13 +0800

    add treehouses shadowsocks enter

--- modules/shadowsocks.sh
+++ modules/shadowsocks.sh
@@ -65,11 +65,25 @@ function shadowsocks {
           if [ "$1" = "start" ]; then
             systemctl start shadowsocks-libev-local@$2.service
             echo "$2 started."
+            echo "Use \`$BASENAME shadowsocks enter $2\` to enter shell session proxied by shadowsocks client."
           else
             systemctl enable shadowsocks-libev-local@$2.service
             echo "$2 enabled."
           fi
           echo
+
+          if [ ! -f /etc/shadowsocks-libev/$2.conf ]; then
+            if grep -q local_port /etc/shadowsocks-libev/$2.json; then
+              cp "$TEMPLATES/network/proxychains4.conf" /etc/shadowsocks-libev/$2.conf
+              echo "socks5 127.0.0.1 $(grep local_port /etc/shadowsocks-libev/$2.json |\
+                awk '{print $2}' | sed 's/,//g')" >> /etc/shadowsocks-libev/$name_conf.conf
+            else
+              echo "Config not valid."
+              echo "You may be unable to enter proxied shell session."
+              echo 
+              exit 1
+            fi
+          fi
           exit 0
         fi
       done
@@ -99,7 +113,6 @@ function shadowsocks {
       echo $name
       echo
       read -p "Enter the name for your config:  " name_conf
-      echo
       if echo $name_conf | grep -q ".json"; then
         name_conf="$(echo $name_conf | sed 's/.json//g')"
       fi
@@ -115,23 +128,25 @@ function shadowsocks {
       else
         $EDITOR /etc/shadowsocks-libev/$name_conf.json
       fi
-      if [ -f /etc/shadowsocks-libev/$name_conf.json ]; then
-        echo "Config saved."
-        echo "Use \`$BASENAME shadowsocks start $name_conf\` to start client."
-      else
+      if [ ! -f /etc/shadowsocks-libev/$name_conf.json ]; then
         echo "Config not saved."
         echo
         exit 1
       fi
       echo
 
-      if grep -q local_port | /etc/shadowsocks-libev/$name_conf.json; then
+      if grep -q local_port /etc/shadowsocks-libev/$name_conf.json; then
         cp "$TEMPLATES/network/proxychains4.conf" /etc/shadowsocks-libev/$name_conf.conf
         echo "socks5 127.0.0.1 $(grep local_port /etc/shadowsocks-libev/$name_conf.json |\
           awk '{print $2}' | sed 's/,//g')" >> /etc/shadowsocks-libev/$name_conf.conf
+        echo "Config saved."
+        echo "Use \`$BASENAME shadowsocks start $name_conf\` to start client."
+        echo
+        exit 0
       else
         echo "The Config is not valid."
         echo "Abort."
+        echo
         exit 1
       fi
       ;;
@@ -145,6 +160,43 @@ function shadowsocks {
       fi
       rm -rf /etc/shadowsocks-libev/$name.json
       ;;
+
+    enter)
+      checkargn $# 2
+
+      if echo $2 | grep -q ".json"; then
+        name="$(echo $2 | sed 's/.json//g')"
+      else
+        name="$2"
+      fi
+
+      if [ ! -z "$(pidof ss-local)" ]; then
+        for pid in $(pidof ss-local)
+        do
+          running="$(ps ax | grep -E "^ *$pid" |\
+            awk '{print $7}' |\
+            sed -e 's/\/etc\/shadowsocks-libev\///g' -e 's/.json//g')"
+          if [ "$name" == "$running" ]; then
+            break
+          fi
+        done
+        echo "$name is not running!"
+        echo "Please do \`$BASENAME shadowsocks start $name\` first."
+        exit 1
+      else
+        echo "No instance of Shadowsocks client running!"
+        echo "Abort." && exit 1
+      fi
+      
+      if -f /etc/shadowsocks-libev/$name.conf; then
+        proxychains4 -q -f /etc/shadowsocks-libev/$name.conf $SHELL
+        echo "Session terminated."
+      else
+        echo "Proxychains4 configuration file not found."
+        echo "Abort."
+        exit 1
+      fi
+      exit 0 ;;
 
     *)
       echo "Error: No option as $1."

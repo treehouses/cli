7ba69d9da014098ad31c118f8933ce0318f5c1cc
darthnoward <hhl055959@gmail.com>
2020-07-11 06:25:07 +0000

    fix code style

--- modules/redirect.sh
+++ modules/redirect.sh
@@ -5,9 +5,8 @@ function redirect {
  
   if [ $# -eq 0 ]; then
     redirect_help
-    exit 1
+    exit 0
   fi
-
 
   case "$1" in
     list)
@@ -20,15 +19,13 @@ function redirect {
       do
         echo "$(cut -d "/" -f -2 $i)/$ip" > $i
       done
-      systemctl restart dnsmasq.service
-      echo "redirect started."
+      systemctl restart dnsmasq.service && echo "redirect started."
       ;;
     add)
       checkargn $# 2
       ip="$(check_ip)" || exit 1
       echo "address=/$2/$ip" > /etc/dnsmasq.d/$2
-      systemctl restart dnsmasq.service
-      echo "$2 added."
+      systemctl restart dnsmasq.service && echo "$2 added."
       ;;
     remove)
       checkargn $# 2
@@ -54,7 +51,7 @@ function redirect {
 }
 
 function check_ip {
-  ip="$(nmap -sn $(ip route show | grep "via" |\
+  ip="$(nmap -sn $(ip route show | grep 'via' |\
     awk '{print $3}')/24 2>/dev/null | grep -i "$(</etc/hostname)" |\
     awk '{print $6}' | sed -e 's/(//g' -e 's/)//g')" 
   if [ -z "$ip" ]; then

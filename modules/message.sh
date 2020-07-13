a6712f34788402bddad14317851182b0e4885fce
root <root@cli>
2020-07-13 21:30:09 +0000

    update format

--- modules/message.sh
+++ modules/message.sh
@@ -2,20 +2,21 @@ function message {
   chats="$1"
   case "$chats" in
     gitter)
-       echo "navigate to https://gitter.im/login/oauth/authorize?client_id=6c3ac0766e94e8b760e372e0da66e3ac4470ff3f&response_type=code&redirect_uri=http://localhost:7000/login/callback"
-       echo "Click 'Allow' and enter the code at the end of the redirect link: "
-       read code
+       case "$2" in
+         apikey)
+           echo "navigate to https://gitter.im/login/oauth/authorize?client_id=6c3ac0766e94e8b760e372e0da66e3ac4470ff3f&response_type=code&redirect_uri=http://localhost:7000/login/callback"
+           echo "Click 'Allow' and enter the code at the end of the redirect link: "
+           read code
        #if ![-z $api_token]; then
          #delete the variable
 
-        # apikey)
         #   conf_var_update "api_token" "$3"
         #   ;;
-	api_info=$(curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" "https://gitter.im/login/oauth/token" -d '{"client_id": "6c3ac0766e94e8b760e372e0da66e3ac4470ff3f", "client_secret": "4649fa1132fae15cff89737268046bc9e65536bc", "code": "'$code'", "redirect_uri": "http://localhost:7000/login/callback", "grant_type": "authorization_code"}')
-	token_info=$(echo $api_info | python -m json.tool | jq '.access_token' | tr -d '"')
-        conf_var_update "api_token" "$token_info"
+	   api_info=$(curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" "https://gitter.im/login/oauth/token" -d '{"client_id": "6c3ac0766e94e8b760e372e0da66e3ac4470ff3f", "client_secret": "4649fa1132fae15cff89737268046bc9e65536bc", "code": "'$code'", "redirect_uri": "http://localhost:7000/login/callback", "grant_type": "authorization_code"}')
+	   token_info=$(echo $api_info | python -m json.tool | jq '.access_token' | tr -d '"')
+           conf_var_update "api_token" "$token_info"
+	    ;;
 	#echo "$api_token"
-       case "$2" in
          sendto)
            #joins room
 	   group="$3"

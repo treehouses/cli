a76ca319dad2ac700fc3d94a0cc3a287410854f5
ComputerOnFire <ComputerOnFire@protonmail.com>
2020-07-15 21:52:40 -0500

    more specific filename for pkill

--- modules/upgrade.sh
+++ modules/upgrade.sh
@@ -44,7 +44,7 @@ function upgrade {
     fi
     cp /usr/local/bin/bluetooth-server.py "/usr/local/bin/bluetooth-server.py.$(date +'%Y%m%d%H%m%S')"
     curl -s "https://raw.githubusercontent.com/treehouses/control/${branch}/server.py" -o /usr/local/bin/bluetooth-server.py
-    pkill -f server.py
+    pkill -f bluetooth-server.py
     bluetooth restart &>"$LOGFILE"
     echo "Successfully updated and restarted bluetooth server"
   elif [ "$tag" == "cli" ]; then

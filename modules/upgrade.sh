4a609abfcc796d5366071df1d974ccf0e70d6e9c
ComputerOnFire <ComputerOnFire@protonmail.com>
2020-07-15 21:01:24 -0500

    try nohup

--- modules/upgrade.sh
+++ modules/upgrade.sh
@@ -43,11 +43,7 @@ function upgrade {
       fi
     fi
     cp /usr/local/bin/bluetooth-server.py "/usr/local/bin/bluetooth-server.py.$(date +'%Y%m%d%H%m%S')"
-    screen -dm bash -c 'curl -s "https://raw.githubusercontent.com/treehouses/control/${branch}/server.py" -o /usr/local/bin/bluetooth-server.py; sleep 5; bluetooth restart' &>$LOGFILE
-    #cp /usr/local/bin/bluetooth-server.py "/usr/local/bin/bluetooth-server.py.$(date +'%Y%m%d%H%m%S')"
-    #curl -s "https://raw.githubusercontent.com/treehouses/control/${branch}/server.py" -o /usr/local/bin/bluetooth-server.py
-    #sleep 5
-    #bluetooth restart
+    nohup (curl -s "https://raw.githubusercontent.com/treehouses/control/${branch}/server.py" -o /usr/local/bin/bluetooth-server.py; bluetooth restart &>"$LOGFILE") &
     echo "Successfully updated and restarted bluetooth server"
   elif [ "$tag" == "cli" ]; then
     checkroot

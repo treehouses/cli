63336ee0cb313ad016691f527b124f16148c4036
root <root@travis-job-d262451f-9e19-4e20-bc47-1e6c70c08100>
2020-07-17 22:54:32 +0000

    memory.sh file updated

--- modules/memory.sh
+++ modules/memory.sh
@@ -12,7 +12,7 @@ function memory_total() {
       t=$(free | grep -i Mem | awk '{printf $2}')
       ;;
     *)
-      echo "error: only '-g' and '-m' argument accepted (check 'treehouses help memory')"
+      echo "error: only 'gb' and 'mb' argument accepted (check 'treehouses help memory')"
       exit 1
       ;;
   esac

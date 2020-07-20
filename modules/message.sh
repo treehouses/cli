4d2233b2db089ca2e06a055307be79aa82bc8b62
warthog16 <45441785+warthog16@users.noreply.github.com>
2020-07-20 15:13:34 -0400

    Update message.sh
    added receive_from function

--- modules/message.sh
+++ modules/message.sh
@@ -29,6 +29,35 @@ function message {
             echo "No message was submitted."
           fi
           ;;
+        receive_from)
+          group="$3"
+          curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $api_token" "https://api.gitter.im/v1/rooms" -d '{"uri":"'$group'"}'>"$LOGFILE"
+          channelinfo=$(curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $api_token" "https://api.gitter.im/v1/rooms" -d '{"uri":"'$group'"}')
+          #finds channel id and removes double quotes
+          channelid=$(echo $channelinfo | python -m json.tool | jq '.id' | tr -d '"')
+          user_info=$(curl -s -H "Accept: application/json" -H "Authorization: Bearer $api_token" "https://api.gitter.im/v1/user")
+          user_id=$(echo $user_info | python -m json.tool | jq '.[].id' | tr -d '"')
+          i=0
+          length=$(curl -s -H "Accept: application/json" -H "Authorization: Bearer $api_token" "https://api.gitter.im/v1/user/$user_id/rooms/$channelid/unreadItems" | python -m json.tool | jq '.chat | length')
+          if [ $length == 0 ];then
+            echo "You have no unread messages at the moment"
+          fi
+          unread_info=$(curl -s -H "Accept: application/json" -H "Authorization: Bearer $api_token" "https://api.gitter.im/v1/user/$user_id/rooms/$channelid/unreadItems")
+          while [ "$i" -lt $length ]; do
+            message_id=$(echo $unread_info | python -m json.tool | jq '.chat['$i']' | tr -d '"')
+            message_info=$(curl -s -H "Accept: application/json" -H "Authorization: Bearer $api_token" "https://api.gitter.im/v1/rooms/$channelid/chatMessages/$message_id")
+            message=$(echo $message_info | python -m json.tool | jq '.text')
+            from_name=$(echo $message_info | python -m json.tool | jq '.fromUser.displayName' | tr -d '"')
+            from_user=$(echo $message_info | python -m json.tool | jq '.fromUser.username' | tr -d '"') 
+            sent_time=$(echo $message_info | python -m json.tool | jq '.sent' | tr -d '"')  
+            #displays message
+            echo "From: $from_name@$from_user"
+            echo "Message: $message"
+            echo "Sent: $sent_time"
+            curl -X POST -s -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $api_token" "https://api.gitter.im/v1/user/$user_id/rooms/$channelid/unreadItems" -d '{"chat":["'$message_id'"]}' > "$LOGFILE"
+            i=$((i+1))
+          done
+          ;;
         *)
           echo "This command does not exist, please look at the following:"
           message_help
@@ -44,7 +73,7 @@ function message {
 
 function message_help {
   echo
-  echo "Usage: $BASENAME message <chats> <apikey <key> | sendto <group> <message>>"
+  echo "Usage: $BASENAME message <chats> <apikey <key> | sendto <group> <message> | receive_from <group>>"
   echo
   echo "You can get your token from https://developer.gitter.im/docs/welcome by signing in, it should show up immediately or by navigating to https://developer.gitter.im/apps"
   echo
@@ -59,5 +88,8 @@ function message_help {
   echo
   echo "  $BASENAME message gitter sendto treehouses/Lobby \"Hi, you are very awesome\""
   echo "     Sends a message to a gitter channel"
+  echo
+  echo "  $BASENAME message gitter receive_from treehouses/Lobby" 
+  echo "     Receives unread messages from a gitter channel"
   echo
 }

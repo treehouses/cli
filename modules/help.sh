f776d9dd97e2947b0b4dba06eab82eb7f7cc4c5b
warthog16 <45441785+warthog16@users.noreply.github.com>
2020-07-20 15:20:10 -0400

    Update help.sh

--- modules/help.sh
+++ modules/help.sh
@@ -132,6 +132,7 @@ magazines                                 downloads specific magazine issue as a
 resolution <cea|dmt [modes]>              sets the screen resolution
 system [cpu|ram|disk|volt|temperature]    display real system informations
 message gitter <apikey|sendto>            sends message to service or sets api/channel info in config file
+               <receive_from>
 shutdown [now|in|force]                   shutdown the system
 EOF
   echo "$helpdefault"

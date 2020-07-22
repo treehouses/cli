d3e1f2782c8fefa1fc1c0604364e011163c6a6a8
Sarah Ramazani <62920905+sarahrams@users.noreply.github.com>
2020-07-21 20:09:57 -0700

    `treehouses services` check for `docker-compose` (fixes #1662) (#1689)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/services.sh
+++ modules/services.sh
@@ -1,4 +1,9 @@
 function services {
+  if [[ $(which docker-compose) == "" ]]; then
+    echo "\"docker-compose\" not found, please install first"
+    exit 1
+  fi
+
   local service_name command command_option service results installed
   local array running port_string found local_url tor_url
   service_name="$1"

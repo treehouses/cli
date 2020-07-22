84513a4aaa46720f8da5483b3d4b38c60b5323a2
Kaiyuan Fan <172881969@qq.com>
2020-07-22 00:03:30 -0400

    `treehouses services webssh` (fixes #1661) (#1677)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/help.sh
+++ modules/help.sh
@@ -94,6 +94,7 @@ services                                  executes the given command on the spec
    [cloud9]                               Cloud9 is a complete web based ide with terminal access
    [jellyfin]                             Jellyfin is a Free Software Media System that puts you in control of managing and streaming your media
    [pylon]                                Pylon is a web based integrated development environment built with Node.js as a backend
+   [webssh]                               Webssh is a simple web application to be used as an ssh client to connect to your ssh servers
 tor [list|ports|add|delete|deleteall]     deals with services on tor hidden network
     [start|stop|destroy|notice]
     [status|refresh]

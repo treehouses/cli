b4ebdde4c37a86b6449fbf083b9882371d490048
Sarah Ramazani <sarahrams1@gmail.com>
2020-07-29 14:33:48 -0700

    treehouses internet reverse

--- modules/internet.sh
+++ modules/internet.sh
@@ -1,15 +1,31 @@
 function internet {
-  checkargn $# 0
-  if ip route get 8.8.8.8 2>/dev/null 1>&2; then
-    echo "true"
-    exit 0
-  fi
-  echo "false"
+  checkargn $# 1
+
+  case "$1" in
+  "")
+    if ip route get 8.8.8.8 2>/dev/null 1>&2; then
+      echo "true"
+      exit 0
+    fi
+    echo "false"
+    ;;
+  
+  "reverse")
+    echo $(curl -s ipinfo.io) | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(ip)"'
+    echo $(curl -s ipinfo.io) | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(city|country|postal)"'| tr '\n' ',' | sed 's/,$/\n/'
+    echo $(curl -s ipinfo.io) | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(org|timezone)"'
+    ;;
+
+  *)
+    echo "ERROR: incorrect command"
+    internet_help
+    ;;
+  esac
 }
 
 function internet_help {
   echo
-  echo "Usage: $BASENAME internet"
+  echo "Usage: $BASENAME internet [reverse]"
   echo
   echo "Outputs true if the rpi can reach internet, or false if it doesn't"
   echo
@@ -17,5 +33,8 @@ function internet_help {
   echo "  $BASENAME internet"
   echo "      the rpi has access to internet -> output: true"
   echo "      the rpi doesn't have access to internet -> output: false"
+  echo
+  echo "  $BASENAME internet reverse"
+  echo "      this output the device's internet location information"
   echo
 }

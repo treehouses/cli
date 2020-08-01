a8535c6810b17cc6d44cc8e67245c2963713820a
dogi <dogi@users.noreply.github.com>
2020-08-01 04:06:37 -0400

    Update internet.sh

--- modules/internet.sh
+++ modules/internet.sh
@@ -11,12 +11,9 @@ function internet {
     ;;  
   "reverse")
     info=$(curl -s ipinfo.io)
-    if ip route get 8.8.8.8 2>/dev/null 1>&2; then
-      echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(ip)"'
-      echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(city|country|postal)"'| tr '\n' ',' | sed 's/,$/\n/'
-      echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(org|timezone)"'
-      exit 0
-    fi
+    echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(ip)"'
+    echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(city|country|postal)"'| tr '\n' ',' | sed 's/,$/\n/'
+    echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(org|timezone)"'
     ;;
   *)
     echo "ERROR: incorrect command"

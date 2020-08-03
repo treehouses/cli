c54051d1375668daf6cd89100bce7c843b7030a3
Sarah Ramazani <62920905+sarahrams@users.noreply.github.com>
2020-08-03 13:42:35 -0700

    `treehouses internet reverse` refactor (fixes #1737) (#1751)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/internet.sh
+++ modules/internet.sh
@@ -10,10 +10,10 @@ function internet {
     echo "false"
     ;;  
   "reverse")
-    info="$(curl -s ipinfo.io)"
-    echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(ip)"'
-    echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(city|country|postal)"'| tr '\n' ',' | sed 's/,$/\n/'
-    echo $info | grep -o '"[^"]*"\s*:\s*"[^"]*"' | grep -E '"(org|timezone)"'
+    info="$(curl -s ipinfo.io | grep -o '"[^"]*"\s*:\s*"[^"]*"')"
+    echo "$info" | grep -E '"(ip)"'
+    echo "$info" | grep -E '"(city|country|postal)"' | tr '\n' ',' | sed 's/,$/\n/' | sed 's/\",\"/\", \"/g'
+    echo "$info" | grep -E '"(org|timezone)"'
     ;;
   *)
     echo "ERROR: incorrect command"

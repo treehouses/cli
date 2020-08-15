32b7e5c9a54509aa1a744200eb0e6997346cc03f
Sarah Ramazani <sarahrams1@gmail.com>
2020-08-14 23:34:04 -0700

    update

--- modules/burnimage.sh
+++ modules/burnimage.sh
@@ -5,14 +5,15 @@ function burnimage {
 #   existed_device=$(lsblk /dev/sd* ${device})
   if [ "$option" == "list" ]; then
     lsblk
-  elif [ "$option" == "detect" ]; then
-    if lsblk /dev/sd* > /dev/null 2>&1; then 
-      echo "The device \"$existed_device\" exists"
-    else
-      echo "Error: the device \"$device\" was not detected"
-      exit 1
-    fi
-    elif [[ ( "$option" == "/dev/sda" || "$option" == "/dev/sdb" ) ]]; then # && [ lsblk /dev/sd* > /dev/null 2>&1 ]]; then
+#   elif [ "$option" == "detect" ]; then
+#     if lsblk /dev/sd* > /dev/null 2>&1; then 
+#       echo "The device \"$device\" exists"
+#     else
+#       echo "Error: the device \"$device\" was not detected"
+#       exit 1
+#     fi
+    elif [ ( "$option" == "/dev/sda" || "$option" == "/dev/sdb" ) ]; then # && [ lsblk /dev/sd* > /dev/null 2>&1 ]]; then
+    if lsblk /dev/sd* > /dev/null 2>&1; then
 #   elif [[ "$option" == "$device" && $(lsblk /dev/sd*) == "$device" ]]; then
     # existed_device=$(lsblk /dev/sd* ${device})
     # if [ "$2" == "/dev/sda" || "$2" == "/dev/sdb" ]; then
@@ -27,13 +28,14 @@ function burnimage {
         else
           echo "the image is up-to-date"
         fi
-    #   fi
+      fi
 
       if [ -f "latest.img.gz" ]; then
         echo "writing..."
         zcat "latest.img.gz" | sudo dd of=$option bs=1M conv=fsync
         echo "the image has been written, the treehouses image is still on $(pwd), you can remove or keep it for future burns"
       fi
+    fi
     fi
   else
     echo "Error: no command found"

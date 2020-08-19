6f298299bdcce6425a08325bb511381646c041fd
darthnoward <hhl055959@gmail.com>
2020-08-19 22:49:32 +0800

    add install with token

--- modules/tg-terminal.sh
+++ modules/tg-terminal.sh
@@ -1,12 +1,19 @@
 function tg-terminal {
   checkroot
   case $1 in
+  "")
+    systemctl is-active tg-terminal.service
+    ;;
   "install")
-    tg-terminal-install
+    tg-terminal-install "$2"
     ;;
   "start")
-    python3 /srv/tg-terminal/telegramShellBot.py & disown
+    systemctl start tg-terminal.service
+    echo "Bot started."
     ;;
+  "enable")
+    systemctl enable tg-terminal.service --quiet
+    echo "Bot enabled."
   *)
     echo "No option as $1."
     exit 1
@@ -15,13 +22,18 @@ function tg-terminal {
 }
 
 function tg-terminal-install {
+  checkargn $# 1
+  echo "Installing.."
+  rm -rf /srv/tg-terminal/
 	mkdir -p /srv/tg-terminal/shareFolder/
 	touch /srv/tg-terminal/users.txt
 	touch /srv/tg-terminal/log.txt
 	
 	wget -q -P /srv/tg-terminal/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/telegramShellBot.py || exit 1
 	wget -q -P /srv/tg-terminal/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/config.txt || exit 1
-	sed -i 's/token =/token = 1304518352:AAHYhhLGLZvTzXsYmt-A5oeEL6oUqCUvP6E/g' /srv/tg-terminal/config.txt
+  rm -f /etc/systemd/system/tg-terminal.service
+  wget -q -P /etc/systemd/system/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/tg-terminal.service || exit 1
+	sed -i 's/token =/token = $1/g' /srv/tg-terminal/config.txt
 	pip3 install -q pyTelegramBotApi || exit 1
   echo "tg-terminal install."	
 }

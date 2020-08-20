3c41b8fc8924790cff4112e570a2f1edf468151d
darthnoward <hhl055959@gmail.com>
2020-08-20 12:58:30 +0800

    add password option

--- modules/tg-terminal.sh
+++ modules/tg-terminal.sh
@@ -8,8 +8,10 @@ function tg-terminal {
     if [ -z "$2" ]; then
       echo "Please pass the token as the argument."
       exit 1
+    elif [ -z "$3"]; then
+      tg-terminal-install "$2" "treehouses"
     else
-      tg-terminal-install "$2"
+      tg-terminal-install "$2" "$3"
     fi
     ;;
   "uninstall")
@@ -47,15 +49,16 @@ function tg-terminal {
 function tg-terminal-install {
   echo "Installing.."
   tg-terminal uninstall > /dev/null 2>&1
-	mkdir -p /srv/tg-terminal/shareFolder/
-	touch /srv/tg-terminal/users.txt
-	touch /srv/tg-terminal/log.txt
-	
-	wget -q -P /srv/tg-terminal/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/telegramShellBot.py || exit 1
-	wget -q -P /srv/tg-terminal/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/config.txt || exit 1
+  mkdir -p /srv/tg-terminal/shareFolder/
+  touch /srv/tg-terminal/users.txt
+  touch /srv/tg-terminal/log.txt
+  
+  wget -q -P /srv/tg-terminal/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/telegramShellBot.py || exit 1
+  wget -q -P /srv/tg-terminal/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/config.txt || exit 1
   wget -q -P /etc/systemd/system/ https://raw.githubusercontent.com/darthnoward/remoteTelegramShell/master/tg-terminal.service || exit 1
-	sed -i "s/token =/token = $1/g" /srv/tg-terminal/config.txt
-	pip3 install -q pyTelegramBotApi || exit 1
+  sed -i "s/token =/token = $1/g" /srv/tg-terminal/config.txt
+  sed -i "s/password = treehouses/password = $2/g" /srv/tg-terminal/config.txt
+  pip3 install -q pyTelegramBotApi || exit 1
   echo "tg-terminal installed."	
 }
 

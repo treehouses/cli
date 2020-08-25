aa72900ee1a1adb51733a43642943403882ab883
dogi <dogi@users.noreply.github.com>
2020-08-25 00:01:41 -0400

    Update help.sh

--- modules/help.sh
+++ modules/help.sh
@@ -48,9 +48,10 @@ sshtunnel [add|remove|list|active|check]  helps adding sshtunnels
 led [green|red] [mode]                    sets the led mode
     [newyear|lunarnewyear|valentine]
     [carnival|lantern|stpatricks|easter]
-    [labourday|eid|dragonboat|independenceday]
-    [onam|diwali|thanksgiving|christmas]
-    [heavymetal|dance|kecak|random]
+    [labourday|eid|dragonboat]
+    [independenceday|onam|diwali]
+    [thanksgiving|christmas|heavymetal]
+    [dance|kecak|random]
 power [mode|current|freq|status]          sets the power mode or check power mode/CPU frequency
 uptime [boot|start|stop|log]              displays uptime and boot time of the Raspberry Pi
 rtc <on|off> [rasclock|ds3231]            sets up the rtc clock specified

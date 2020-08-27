d7e9e676adfc8e316ec7f5afb7abedea226c4f9c
root <wardah.b6@gmail.com>
2020-08-27 02:19:24 +0000

    treehouses message slack sendto (fixes #1810)

--- modules/message.sh
+++ modules/message.sh
@@ -64,6 +64,32 @@ function message {
           ;;
       esac
       ;;
+    slack)
+      case "$2" in
+        apikey)
+          echo "Please look at the following to see how to get the key:"
+          message_help
+          echo "Enter your key: "
+          read -r stoken
+          echo "token: $stoken"
+          conf_var_update "slacktoken" "$stoken"
+          ;;
+        sendto)
+           #token=$3
+           channel=$3
+           shift; shift; shift;
+           message=$*
+           echo "token: $stoken"
+           echo "token: $slacktoken"
+           echo "channel: $channel"
+           echo "message: $message"
+           #curl -X POST -H 'Authorization: Bearer '$token'' -H 'Content-type: application/json' https://slack.com/api/chat.postMessage --data '{  "channel": "'$channel'", "text": "$message", "as_user": true}'
+           curl -X POST -H 'Authorization: Bearer '$slacktoken' ' -H 'Content-type: application/json' https://slack.com/api/chat.postMessage --data "{  \"channel\": \"$channel\", \"text\": \"$message\", \"as_user\": true}" #works
+           #curl -X POST -H 'Authorization: Bearer '$token'' -H 'Content-type: application/json' 'https://slack.com/api/chat.postMessage' -d "{\"channel\": \"'$channel'\", \"text\": \"'$message'\", \"as_user\": \"true\"}"
+          #curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $token" "https://slack.com/api/chat.postMessage" -d '{"channel": "'$channel'", "text": "'"$message"'", "as_user": "true"}' #works
+           ;;
+      esac
+      ;;
     *)
       echo "This command does not exist, please look at the following:"
       message_help
@@ -81,6 +107,8 @@ function message_help {
   echo
   echo "Sends message to a chat service"
   echo
+  echo "For Slack, you will need to join http://join.slack.ole.org/ and create a Slack app at https://api.slack.com/apps give it a name and add it to the Open Learning Exchange Worskspace, click on Permissions under adding a feature, go to Scopes, go to User Token Scopes, and click on 'Add an OAuth Scope', find the scope: 'chat:write' and add it, then install the app to the workspace by going to the top of OAuth and Permissions, and click 'Install App to Workspace', you will have to reinstall if you make any changes to the app, which will regenerate your OAuth token, on the OAuth exchange page you will be redirected to, check the permissions and click Allow, then copy the OAuth Access token to your clipboard and you can now use it here to send messages  "
+  echo
   echo "Example:"
   echo
   echo "  $BASENAME message gitter apikey \"1234567890\""
@@ -91,5 +119,11 @@ function message_help {
   echo
   echo "  $BASENAME message gitter receivefrom treehouses/Lobby" 
   echo "     Receives unread messages from a gitter channel"
+  echo
+  echo "  $BASENAME message slack sendto \"abc-123-efghijklmnop\" \"C12345678\" \"Hi, you are very awesome\"" 
+  echo "     Sends a message to a slack channel using channel id"
+  echo
+  echo "  $BASENAME message slack sendto \"abc-123-efghijklmnop\" \"channel_name\" \"Hi, you are very awesome\"" 
+  echo "     Sends a message to a slack channel using channel name, eg, channel: #channel_name"
   echo
 }

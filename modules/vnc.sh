d67feb1dbb0325ee751c8a52b2549e20aea22bbb
victor <4152386+koxuan@users.noreply.github.com>
2020-08-29 08:24:45 +0000

    update output behavior of vnc

--- modules/vnc.sh
+++ modules/vnc.sh
@@ -25,16 +25,21 @@ function vnc {
 case "$option" in
   "")
     if [ "$bootoptionstatus" = "static" ] && [ "$vncservicestatus" = "inactive" ] && [ "$xservicestatus" = "inactive" ]; then
-      echo "VNC is disabled."
+      echo "off"
       echo "To enable it, use $BASENAME vnc on"
     elif [ "$bootoptionstatus" = "indirect" ] && [ "$vncservicestatus" = "active" ] && [ "$xservicestatus" = "active" ]; then
-      echo "You can now remotely access the system with a VNC client using the IP address: $ipaddress"
+      echo "on"
       echo "To disable it, use $BASENAME vnc off"
     elif [ "$bootoptionstatus" = "indirect" ] && [ "$vncservicestatus" = "active" ] && [ "$xservicestatus" = "failed" ]; then
-      echo "Please reboot your system."
+      echo "failed, raise issue on treehouses cli and describe behavior to get to here, thank you"
     elif [ "$bootoptionstatus" = "static" ] && [ "$vncservicestatus" = "inactive" ] && [ "$xservicestatus" = "active" ]; then
-      echo "Please reboot your system."
+      echo "pre-off"
+    elif [ "$bootoptionstatus" = "indirect" ] && [ "$vncservicestatus" = "active" ] && [ "$xservicestatus" = "inactive" ]; then
+      echo "pre-on"
     else
+	    echo $bootoptionstatus
+			echo $vncservicestatus
+			echo $xservicestatus
       echo "VNC server is not configured correctly. Please try $BASENAME vnc on to enable it, or $BASENAME vnc off to disable it."
       echo "Alternatively, you may try $BASENAME vnc status-service to verify the status of each specific required service."
     fi
@@ -47,9 +52,7 @@ case "$option" in
     start_service vncserver-x11-serviced.service
     systemctl set-default graphical.target
     reboot_needed
-    echo "Success: the vnc service has been started and enabled when the system boots."
-    echo "Please reboot the system for changes to take effect."
-    echo "You can then remotely access the system with a VNC client using the IP address: $ipaddress"
+		echo 'pre-on'
     ;;
   "off")
     sed -i '/hdmi_group=2/d' /boot/config.txt
@@ -60,7 +63,7 @@ case "$option" in
     systemctl set-default multi-user.target
     reboot_needed
     echo "Success: the vnc service has been stopped and disabled when the system boots."
-    echo "Please reboot the system for changes to take effect."
+		echo "pre-off"
     ;;
   "info")
     echo "The system boots into $isgraphical"

0891709d450b65c964b3a76629bd8ccd2db86323
Sarah Ramazani <62920905+sarahrams@users.noreply.github.com>
2020-08-30 20:57:34 -0700

    `treehouses picture` install instructions (fixes #1819) (#1824)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- modules/picture.sh
+++ modules/picture.sh
@@ -1,6 +1,6 @@
 function picture {
   # check if tiv binary exists
-  check_missing_binary tiv
+  check_missing_binary tiv "tiv is missing\ninstall instructions can be found in \nhttps://github.com/stefanhaustein/TerminalImageViewer"
 
   # check if imagemagick pkg is installed
   check_missing_packages imagemagick

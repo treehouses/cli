77600682ca3bab525890484cd8f6f8fa9905fc15
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-04 13:07:54 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -128,13 +128,13 @@ function remote {
       users=$(cat /etc/passwd | grep "/home" | cut -d: -f1)
       for i in ${users[@]}; 
       do 
-        for j in "$(treehouses ssh 2fa show $i )"; 
+        for j in "$( ssh 2fa show $i )"; 
         do
-          if [[ "$(treehouses ssh 2fa show $i )" == "SSH 2FA for nokey is disabled." ]]; then
+          if [[ "$( ssh 2fa show $i )" == "SSH 2FA for nokey is disabled." ]]; then
             echo "disabled"
             continue
           fi
-          json_fmt="{\"$i\":{\"secret key\":\"$($j | head -n 1 )\"}}"
+          json_fmt="$(echo \"$i\":{\"secret key\":\"$j | head -n 1 \"})"
           echo $json_fmt
         done
       done

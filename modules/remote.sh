5a01f018793fd6b6e62a139f85b6afaf1b6a0120
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-04 15:20:51 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -143,8 +143,8 @@ function remote {
           #   continue
           # fi
           # json_fmt="$i:$j"
-          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":[$k];"
-          echo -n $json_fmt | sed 's/.$//'
+          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":[$k],"
+          echo -n $json_fmt | awk '{printf "%s"",",$0}' | sed 's/,$//'
           done
         done
         

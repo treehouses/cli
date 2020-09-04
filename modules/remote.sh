7485f0c4de3a7699fc976fe32614f1323dc9db4a
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-04 04:03:21 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -104,6 +104,7 @@ function remote {
       checkargn $# 1
       users=$(cat /etc/passwd | grep "/home" | cut -d: -f1)
       # echo "$users"
+      i=0
       for i in "$users"
       do
         str="$(ssh 2fa show ${i++})"
@@ -112,8 +113,9 @@ function remote {
         else
           echo "$(ssh 2fa show $i)" > /dev/null 2>&1
         fi
-        json_fmt="\"${i++}\":$str"
+        json_fmt="\"$i\":$str"
         echo $json_fmt
+        ((i++))
       done
       # json_fmt="{\"pi\":{\"secret key\":\"$(ssh 2fa show pi | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')\",\"scratch codes\":[$(ssh 2fa show pi | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')]},\"ip\":{\"secret key\":\"$(ssh 2fa show ip | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')\",\"scratch codes\":[$(ssh 2fa show pi | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')]},\"nokey\":\"$(ssh 2fa show nokey | grep -o "disabled" )\"}"
       

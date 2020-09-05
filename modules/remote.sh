b59e91d0ed210639a3b68e80a8f5b207401766ee
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-04 18:08:59 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -126,13 +126,14 @@ function remote {
 
      checkargn $# 1
       users=$(cat /etc/passwd | grep "/home" | cut -d: -f1)
+      oper=","
       echo -n "{"
       for i in ${users[@]}; 
       
       do 
       
         str="$(ssh 2fa show $i | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')"
-        str2="$(ssh 2fa show $i | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')"
+        str2="$(ssh 2fa show $i | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')${oper}"
         
         for j in $str
         do
@@ -144,7 +145,7 @@ function remote {
           #   continue
           # fi
           # json_fmt="$i:$j"
-          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":[$k],"
+          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":[$k]"
           echo -n ${json_fmt%?}
           # sed 's/]/],/g' | sed 's/,}/}/g' 
           done

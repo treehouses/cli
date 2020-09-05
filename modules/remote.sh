75a9981ff2ebf1ffb517db7b67854762fd4813c3
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-05 01:44:14 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -110,8 +110,8 @@ function remote {
         if [[ "$showuser" == "SSH 2FA for $user is disabled." ]]; then
           outputpart="\"$user\":\"disabled\","
         else        
-          secret="$(echo $showuser | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')"
-          scratch="$(echo $showuser | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')"
+          secret="$(ssh 2fa show $user | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')"
+          scratch="$(ssh 2fa show $user | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')"
           outputpart="\"$user\":{\"secret key\":\"$secret\",\"scratch codes\":[$scratch]},"
         fi
         output="$output$outputpart"

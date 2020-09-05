97dcdaff49caf79127bdc0f191c6661ce2bfc9bb
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-04 18:11:08 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -133,7 +133,7 @@ function remote {
       do 
       
         str="$(ssh 2fa show $i | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')"
-        str2="$(ssh 2fa show $i | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')${oper}"
+        str2="$(ssh 2fa show $i | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')"
         
         for j in $str
         do
@@ -145,7 +145,7 @@ function remote {
           #   continue
           # fi
           # json_fmt="$i:$j"
-          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":[$k]"
+          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":[$k]${oper}"
           echo -n ${json_fmt%?}
           # sed 's/]/],/g' | sed 's/,}/}/g' 
           done

2e33a30ab3f9a39ce6262350361fa8a004648104
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-04 18:20:44 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -134,19 +134,19 @@ function remote {
       
         str="$(ssh 2fa show $i | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')"
         str2="$(ssh 2fa show $i | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')"
-        
+        str3="[$str2]${oper}"
         for j in $str
         do
         # IFS=","
-        for k in $str2
+        for k in $str3
         do
           # if [[ "$(ssh 2fa show $i )" == "SSH 2FA for nokey is disabled." ]]; then
           #   echo "disabled"
           #   continue
           # fi
           # json_fmt="$i:$j"
-          str3="[$str2]${oper}"
-          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":$str3"
+          
+          json_fmt="\"$i\":{\"secret key\":\"$j\"},\"scratch codes\":$k"
           echo -n ${json_fmt%?}
           # sed 's/]/],/g' | sed 's/,}/}/g' 
           done

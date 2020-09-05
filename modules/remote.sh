fd560e6170214a5a30ffd0dd025827762ed1e509
Sarah Ramazani <sarahrams1@gmail.com>
2020-09-04 21:04:21 -0700

    update

--- modules/remote.sh
+++ modules/remote.sh
@@ -132,7 +132,7 @@ function remote {
         # echo "${file}" | sed 's/,$//'
       done
       
-      echo -n "}"'\n'
+      echo -ne "}"'\n'
 # json_fmt="{\"pi\":{\"secret key\":\"$(ssh 2fa show pi | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')\",\"scratch codes\":[$(ssh 2fa show pi | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')]},\"ip\":{\"secret key\":\"$(ssh 2fa show ip | head -n 1 | sed 's/Secret Key://g' | sed -r 's/\s+//g')\",\"scratch codes\":[$(ssh 2fa show pi | awk 'NR>3' | sed 's/.*/"&"/' | awk '{printf "%s"",",$0}' | sed 's/,$//')]},\"nokey\":\"$(ssh 2fa show nokey | grep -o "disabled" )\"}"
       
 

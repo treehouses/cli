041455650f867970c1a8548261c6bf0c571d20e0
Sarah Ramazani <62920905+sarahrams@users.noreply.github.com>
2020-09-10 01:12:10 -0700

    Update password.sh

--- modules/password.sh
+++ modules/password.sh
@@ -3,8 +3,7 @@ function password {
   checkroot
   checkargn $# 1
   if [[ $1 == "" ]]; then
-    tree=$(pstree -ps $$)
-    if ! [[ $tree == *"python"* ]]; then
+    if ! [[ ${pstree -ps $$} == *"python"* ]]; then
       echo -ne "\U26A0 "
     fi
     log_and_exit1 "Error: Password not entered"

a09324d685971dd4ec79f64cb338a8c7b9b8efc0
Sarah Ramazani <62920905+sarahrams@users.noreply.github.com>
2020-09-10 01:18:47 -0700

    Update password.sh

--- modules/password.sh
+++ modules/password.sh
@@ -4,7 +4,7 @@ function password {
   checkargn $# 1
   if [[ $1 == "" ]]; then
     if ! [[ $(pstree -ps $$) == *"python"* ]]; then
-      echo -ne "\e[0;33m\U26A0\e[0m "
+      echo -ne "\e[0;33m\U26A0 \e[0m"
     fi
     log_and_exit1 "Error: Password not entered"
   elif [ $1 == "disable" ]; then

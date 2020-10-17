5801144e31c07631e43cbb9721458c4203ad3513
root <root@travis-job-a20df353-4023-4966-9598-5ba49ad65d3a>
2020-10-17 05:12:45 +0000

    update dhcp status

--- modules/dhcp.sh
+++ modules/dhcp.sh
@@ -7,16 +7,20 @@ function dhcp () {
       cat /var/lib/misc/dnsmasq.leases|cut -d' ' -f3-
       ;;
     "")
-      if ps xauf | grep dhcp > /dev/null 2>&1
-        then
-	  echo "dhcp is on"
-	else
-	  echo "dhcp is off"
+      if ps xauf | grep dhcp > /dev/null 2>&1; then
+        echo "dhcp is on"
+      else
+        echo "dhcp is off"
       fi
       ;;
     "status")
-      OUTPUT=$(cat /var/lib/misc/dnsmasq.leases|wc -l)
-      echo "dhcp devices: $OUTPUT"
+      if ps xauf | grep dhcp > /dev/null 2>&1; then
+        echo "dhcp is on"
+        OUTPUT=$(cat /var/lib/misc/dnsmasq.leases|wc -l)
+        echo "dhcp devices: $OUTPUT"
+      else
+        echo "dhcp is off"
+      fi
       ;;
     *)
       log_help_and_exit1 "Error: invalid" dhcp

1822b83e079dc040290049986eade60f4843fe00
root <root@travis-job-681b848b-0aaa-4e99-a6e1-b45b06b84b6c>
2020-11-13 18:52:14 +0000

    changes updated

--- modules/message.sh
+++ modules/message.sh
@@ -1,5 +1,6 @@
 function message {
   chats="$1"
+  last_read=1603741036.001400
   function check_apitoken {
     channelname=$1
     access_token=$(config | grep "$channelname" | cut -d "=" -f2)
@@ -262,38 +263,51 @@ function message {
             if [[ $3 == "" ]]; then
               log_comment_and_exit1 "ERROR: Group information is missing" "usage: $BASENAME message slack read <group>"
             fi
+            function check_channel {
+              channel_list=$(curl -s -F token=$access_token https://slack.com/api/conversations.list)
+              channel_list=($(echo $channel_list | python -m json.tool | jq '.channels[].id' | tr -d '"'))
+              for i in ${channel_list[@]}; do
+                if [ $i == $1 ]; then
+                  return 0
+                else
+                  log_comment_and_exit1 "ERROR: invalid channel id"
+                fi
+              done
+            }
             channel=$3
+            check_channel $3
             channel_info=$(curl -s -F token=$access_token -F channel=$channel https://slack.com/api/conversations.info)
-            #last_read=$(echo $channel_info | python -m json.tool | jq '.channel.last_read' | tr -d '"')
-            #echo "$last_read"
-            last_read=1603741036.001400
-            history=$(curl -s -F token=$access_token -F unreads=true -F channel=$channel https://slack.com/api/conversations.history)
-            time=($(echo $history | python -m json.tool | jq '.messages[].ts' | tr -d '"'))
+            last_read=$(echo $channel_info | python -m json.tool | jq '.channel.last_read' | tr -d '"')
+            channel_history=$(curl -s -F token=$access_token -F channel=$channel https://slack.com/api/conversations.history)
+            time=($(echo $channel_history | python -m json.tool | jq '.messages[].ts' 2> /dev/null | tr -d '"'))
             unread_time=()
             for i in "${time[@]}"; do
-              if [[ $last_read == $i ]]; then
+              if [ $last_read == $i ]; then
                 break
               else
                 unread_time+=($i)
               fi
             done
             unread_time=($(printf '%s\n' "${unread_time[@]}" | tac | tr '\n' ' '))
-            for i in "${unread_time[@]}"; do
-              msg_info=$(curl -s -F token=$access_token -F channel=$channel -F latest=$i -F limit=1 -F inclusive=true https://slack.com/api/conversations.history)
-              msg=$(echo $msg_info | python -m json.tool | jq '.messages[].text' | tr -d '"')
-              msg_length=$(echo ${msg_info} | jq '.messages[].text | length')
-              userid=$(echo $msg_info | python -m json.tool | jq '.messages[].user' | tr -d '"')
-              name_info=$(curl -s -F token=$access_token -F user=$userid -F latest=$i https://slack.com/api/users.info)
-              name=$(echo $name_info | python -m json.tool | jq '.user.real_name' | tr -d '"')
-              time_info=$(date -d @$i)
-              date=$(echo ${time_info} | cut -d " " -f1-3)
-              year=$(echo ${time_info} | cut -d " " -f6)
-              send_time=$(echo ${time_info} | cut -d " " -f4)
-              echo "Date: $date $year"
-              echo "Time: $send_time"
-              echo "From: $name"
-              echo "Message: $msg"
-            done
+            if [ ${#unread_time[@]} -eq 0 ]; then
+              echo "You have no unread messages at the moment"
+            else
+              for i in "${unread_time[@]}"; do
+                msg_info=$(curl -s -F token=$access_token -F channel=$channel -F latest=$i -F limit=1 -F inclusive=true https://slack.com/api/conversations.history)
+                msg=$(echo $msg_info | python -m json.tool | jq '.messages[].text' | tr -d '"')
+                userid=$(echo $msg_info | python -m json.tool | jq '.messages[].user' | tr -d '"')
+                name_info=$(curl -s -F token=$access_token -F user=$userid -F latest=$i https://slack.com/api/users.info)
+                name=$(echo $name_info | python -m json.tool | jq '.user.real_name' | tr -d '"')
+                time_info=$(date -d @$i)
+                date=$(echo ${time_info} | cut -d " " -f1-3)
+                year=$(echo ${time_info} | cut -d " " -f6)
+                send_time=$(echo ${time_info} | cut -d " " -f4)
+                echo "Date: $date $year"
+                echo "Time: $send_time"
+                echo "From: $name"
+                echo "Message: $msg"
+              done
+            fi
           fi
           ;;
         *)

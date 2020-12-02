d4b0929b4c9e799bdbd33de5770a6f4c60773942
Neethu-Mohan <neethumohan1212@gmail.com>
2020-12-03 01:12:45 +0400

    Update message.sh

--- modules/message.sh
+++ modules/message.sh
@@ -20,7 +20,7 @@ function message {
         channel_names=$(echo $channel_info | python -m json.tool | jq '.[].name' | tr -d '"')
         echo "$channel_names"
 }
-  function get_channel_slack{
+  function get_channel_slack {
         channel_list=$(curl -s -F token=$access_token -F types=public_channel,private_channel https://slack.com/api/users.conversations)
         channels=$(echo $channel_list | python -m json.tool | jq '.channels[].name' | tr -d '"')
         user_list=$(curl -s -F token=$access_token https://slack.com/api/users.list)

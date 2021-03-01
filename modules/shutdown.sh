c6c21f34e554104e8d75229bab506bf04c2e7351
rjpadilla <rjpadilla.dev@outlook.com>
2021-03-01 14:11:16 -0500

    update shutdown.sh (fixes #2048)

--- modules/shutdown.sh
+++ modules/shutdown.sh
@@ -19,7 +19,7 @@ function shutdown {
       /sbin/shutdown -f
       ;;
     *)
-       echo "Error: only now, in and force options are supported."
+       echo "Error: only the options 'now', 'in', and 'force' are supported"
        ;;
   esac
 }

f010a6df106de7108a3765157ca56d1f1a02e521
JLKwong <JLeeKwong@gmail.com>
2021-03-05 19:06:29 -0800

    update remote.sh to add reverse subsubcommand (fixes #2068)

--- modules/remote.sh
+++ modules/remote.sh
@@ -71,19 +71,12 @@ function remote {
       ;;
     "reverse")
       checkargn $# 2
-      export reverse=$(treehouses internet reverse | sed -e 's#",\ "#"\n"#g' | cut -d'"' -f4)
-      if [ -z "$2" ]; then
-        echo "$every_command"
-      elif [ "$2" = "json" ]; then
+      reverse=$(internet reverse | sed -e 's#",\ "#"\n"#g' | cut -d'"' -f4)
         while IFS= read -r line;
         do
           cmd_str+="\"$line\","
-        done <<< "$every_command"
+        done <<< "$every_reverse"
         printf "{\"commands\":["%s"]}\n" "${cmd_str::-1}"
-      else
-        echo "Error: incorrect command"
-        log_and_exit1 "Usage: $BASENAME remote commands [json]"
-      fi
       ;;
     "allservices")
       checkargn $# 1

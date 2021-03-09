364f3e1f3480ad40ac72a50a735d1010c8c12d6e
JLKwong <JLeeKwong@gmail.com>
2021-03-08 22:00:20 -0800

    rough draft

--- modules/internet.sh
+++ modules/internet.sh
@@ -8,26 +8,25 @@ function internet {
       exit 0
     fi
     echo "false"
-    ;;  
+    ;;
   "reverse")
     if ! nc -w 10 -z 8.8.8.8 53 >/dev/null 1>&2; then
       log_and_exit1 "Error: no internet found"
     fi
     info="$(curl -s ipinfo.io | grep -o '"[^"]*"\s*:\s*"[^"]*"')"
 
-    postal=$("$info" | grep -E '"(postal)"') #assigns postal value
+    postal=$(echo "$info" | grep -E '"(postal)"') #assigns postal value
+
+    echo "$info" | grep -E '"(ip)"'
 
     if [ -z $postal ]; then #if no postal, echoes n/a for postal
-      echo "$info" | grep -E '"(ip)"'
-      echo "$info" | grep -E '"(city|country)"' | tr '\n' ',' | sed 's/,$/\n/' | sed 's/\",\"/\", \"/g'
-      echo ", \"postal\": \"n/a\""
-      echo "$info" | grep -E '"(org|timezone)"'
-
+      echo -n "$info" | grep -E '"(city|country)"' | tr '\n' ',' | sed 's/,$/\n/' | sed 's/\",\"/\", \"/g'
+      echo ", \"postal\": \"n/a\"".
     else
-      echo "$info" | grep -E '"(ip)"'
       echo "$info" | grep -E '"(city|country|postal)"' | tr '\n' ',' | sed 's/,$/\n/' | sed 's/\",\"/\", \"/g'
-      echo "$info" | grep -E '"(org|timezone)"'
     fi
+
+    echo "$info" | grep -E '"(org|timezone)"'
     ;;
   *)
     log_help_and_exit1 "ERROR: incorrect command" internet

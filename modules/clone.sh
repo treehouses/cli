04e9e16942e3f02148f03c63562125c3df1ab68a
JLKwong <JLeeKwong@gmail.com>
2021-03-10 18:06:03 -0800

    clone.sh options template (fixes #2067)

--- modules/clone.sh
+++ modules/clone.sh
@@ -4,33 +4,38 @@ function clone {
   checkroot
   checkargn $# 1
   device="$1"
-  if [ -z "$device" ]; then
-    device="/dev/sdb"
-  fi
 
-  a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
-  #echo "$a - /dev/mmcblk0"
+  case $options in
+    *)
+      if [ -z "$device" ]; then
+      device="/dev/sdb"
+      fi
 
-  b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
-  #echo "$b - /dev/sdb"
+      a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
+      #echo "$a - /dev/mmcblk0"
 
-  if [ -z "$a" ] || [ -z "$b" ]; then
-    echo "Error: the device $device wasn't detected. Please use 'fdisk -l' to find the device name."
-    return 1
-  fi
+      b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
+      #echo "$b - /dev/sdb"
 
-  if [ $b -lt $a ]; then
-    echo "Error: the device $device is not big enough"
-    return 1
-  fi
+      if [ -z "$a" ] || [ -z "$b" ]; then
+        echo "Error: the device $device wasn't detected. Please use '$BASENAME clone detect' to find the device name."
+        return 1
+      fi
 
-  if [ $a -eq $b ] || [ $a -lt $b ]; then
-    echo "copying...."
-    echo u > /proc/sysrq-trigger
-    dd if=/dev/mmcblk0 bs=1M of="$device" status=progress
-  fi
+      if [ $b -lt $a ]; then
+        echo "Error: the device $device is not big enough"
+        return 1
+      fi
 
-  echo ; echo "A reboot is needed to re-enable write permissions to OS."
+      if [ $a -eq $b ] || [ $a -lt $b ]; then
+        echo "copying...."
+        echo u > /proc/sysrq-trigger
+        dd if=/dev/mmcblk0 bs=1M of="$device" status=progress
+      fi
+
+      echo ; echo "A reboot is needed to re-enable write permissions to OS."
+      ;;
+    esac
 }
 
 function clone_help {

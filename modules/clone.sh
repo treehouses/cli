02dea72dd5a13bd80460abe3937c4d022795515a
JLKwong <JLeeKwong@gmail.com>
2021-03-10 17:21:22 -0800

    add clone sub-subcommand (fixes #2067)

--- modules/clone.sh
+++ modules/clone.sh
@@ -2,35 +2,41 @@ function clone {
   local device a b
   checkrpi
   checkroot
-  checkargn $# 1
+  checkargn $# 2
   device="$1"
-  if [ -z "$device" ]; then
-    device="/dev/sdb"
-  fi
 
-  a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
-  #echo "$a - /dev/mmcblk0"
+  case $1 in
+    "detect")
+    echo 'These are the devices you can clone into (treehouses clone [DEVICE PATH]):'; fdisk -l | grep -o '^/dev/sd[a-z]' | sort -u
+    *)
+      checkargn $# 1
+      if [ -z "$device" ]; then
+        device="/dev/sdb"
+      fi
 
-  b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
-  #echo "$b - /dev/sdb"
+      a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
+      #echo "$a - /dev/mmcblk0"
 
-  if [ -z "$a" ] || [ -z "$b" ]; then
-    echo "Error: the device $device wasn't detected. Please use 'fdisk -l' to find the device name."
-    return 1
-  fi
+      b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
+      #echo "$b - /dev/sdb"
 
-  if [ $b -lt $a ]; then
-    echo "Error: the device $device is not big enough"
-    return 1
-  fi
+      if [ -z "$a" ] || [ -z "$b" ]; then
+        echo "Error: the device $device wasn't detected. Please use 'treehouses clone detect' to display the device name."
+        return 1
+      fi
 
-  if [ $a -eq $b ] || [ $a -lt $b ]; then
-    echo "copying...."
-    echo u > /proc/sysrq-trigger
-    dd if=/dev/mmcblk0 bs=1M of="$device" status=progress
-  fi
+      if [ $b -lt $a ]; then
+        echo "Error: the device $device is not big enough"
+        return 1
+      fi
 
-  echo ; echo "A reboot is needed to re-enable write permissions to OS."
+      if [ $a -eq $b ] || [ $a -lt $b ]; then
+        echo "copying...."
+        echo u > /proc/sysrq-trigger
+        dd if=/dev/mmcblk0 bs=1M of="$device" status=progress
+      fi
+
+      echo ; echo "A reboot is needed to re-enable write permissions to OS."
 }
 
 function clone_help {

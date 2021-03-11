1f1ce1263ecbd7d8199533c0094489981c9160b8
JLKwong <JLeeKwong@gmail.com>
2021-03-10 18:35:24 -0800

    fix cases (fixes #2067)

--- modules/clone.sh
+++ modules/clone.sh
@@ -1,41 +1,41 @@
 function clone {
+  local argument a b
   checkrpi
   checkroot
   checkargn $# 1
-  device="$1"
+  argument="$1"
 
-  case $options in
+  case $argument in
 
-    detect)
+    "detect")
       fdisk -l | grep -o '^/dev/sd[a-z]' | sort -u
       ;;
 
     *)
-      local device a b
-      if [ -z "$device" ]; then
-      device="/dev/sdb"
+      if [ -z "$argument" ]; then
+      argument="/dev/sdb"
       fi
 
       a=$(fdisk -l |grep /dev/mmcblk0: | grep -P '\d+ (?=bytes)' -o)
       #echo "$a - /dev/mmcblk0"
 
-      b=$(fdisk -l |grep "$device": | grep -P '\d+ (?=bytes)' -o)
+      b=$(fdisk -l |grep "$argument": | grep -P '\d+ (?=bytes)' -o)
       #echo "$b - /dev/sdb"
 
       if [ -z "$a" ] || [ -z "$b" ]; then
-        echo "Error: the device $device wasn't detected. Please use '$BASENAME clone detect' to find the device name."
+        echo "Error: the device $argument wasn't detected. Please use '$BASENAME clone detect' to find the device name."
         return 1
       fi
 
       if [ $b -lt $a ]; then
-        echo "Error: the device $device is not big enough"
+        echo "Error: the device $argument is not big enough"
         return 1
       fi
 
       if [ $a -eq $b ] || [ $a -lt $b ]; then
         echo "copying...."
         echo u > /proc/sysrq-trigger
-        dd if=/dev/mmcblk0 bs=1M of="$device" status=progress
+        dd if=/dev/mmcblk0 bs=1M of="$argument" status=progress
       fi
 
       echo ; echo "A reboot is needed to re-enable write permissions to OS."

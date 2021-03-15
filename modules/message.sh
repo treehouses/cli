a54a896e24caaed6da978d34f68d1fef9f00ab32
JLKwong <JLeeKwong@gmail.com>
2021-03-15 11:40:03 -0700

    clarify apitoken error message (fixes #2091)

--- modules/message.sh
+++ modules/message.sh
@@ -271,7 +271,7 @@ function message {
             else
               conf_var_update "slack_apitoken" "$3"
               echo "Your apitoken ($3) has been added to your Slack app"
-              echo "You can now use `$BASENAME message [channels|send|show|read|mark]`"
+              echo "You can now use $BASENAME message [channels|send|show|read|mark]"
             fi
           elif check_apitoken slack; then
             get_apitoken slack

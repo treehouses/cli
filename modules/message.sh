71174317d3ef0da7213e2e4020de6f518cc2c1de
JLKwong <JLeeKwong@gmail.com>
2021-03-17 17:21:37 -0700

    debug

--- modules/message.sh
+++ modules/message.sh
@@ -26,15 +26,25 @@ function message {
     curl -s -F token=$access_token -F types=public_channel,private_channel https://slack.com/api/users.conversations
     echo $?
 	echo 2
-    channel_list=$(curl -s -F token=$access_token -F types=public_channel,private_channel https://slack.com/api/users.conversations; echo $?)
+    channel_list=$(curl -s -F token=$access_token -F types=public_channel,private_channel https://slack.com/api/users.conversations)
+    curl -s -F token=$access_token -F types=public_channel,private_channel https://slack.com/api/users.conversations
+    echo $?
 	echo 3
-    channels=$(echo $channel_list | python -m json.tool | jq '.channels[].name' | tr -d '"'; echo $?)
+    channels=$(echo $channel_list | python -m json.tool | jq '.channels[].name' | tr -d '"')
+    echo $channel_list | python -m json.tool | jq '.channels[].name' | tr -d '"'
+    echo $?
 	echo 4
-    user_list=$(curl -s -F token=$access_token https://slack.com/api/users.list; echo $?)
+    user_list=$(curl -s -F token=$access_token https://slack.com/api/users.list)
+    curl -s -F token=$access_token https://slack.com/api/users.list
+    echo $?
 	echo 5
-    users=$(echo $user_list | python -m json.tool | jq '.members[].name' | tr -d '"'; echo $?)
+    users=$(echo $user_list | python -m json.tool | jq '.members[].name' | tr -d '"')
+    echo $user_list | python -m json.tool | jq '.members[].name' | tr -d '"'
+    echo $?
 	echo 6
-    channel_names=$(echo -e "$channels\n$users"; echo $?)
+    channel_names=$(echo -e "$channels\n$users")
+    echo -e "$channels\n$users"
+    echo $?
 	echo 7
     echo "$channel_names"; echo $?
 	echo 8

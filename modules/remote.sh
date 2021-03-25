c4f90bc8468c7c57c8f84a23f2a6e5d1d6705d00
JLKwong <JLeeKwong@gmail.com>
2021-03-24 20:01:57 -0700

    delete duplicate postal (fixes #2129)

--- modules/remote.sh
+++ modules/remote.sh
@@ -72,7 +72,7 @@ function remote {
     "reverse")
       checkargn $# 2
       reverse=$(internet reverse | sed -e 's#",\ "#"\n"#g' | cut -d'"' -f 2,3,4 | sed 's#\:[[:space:]]\"#:\"#g' | awk '!x[$0]++')
-      IP=$(echo "$reverse")
+      IP=$(echo "$reverse" | awk '/^IP/,/^"/')
       echo $IP
         while IFS= read -r line; do
           cmd_str+="\"$line\","

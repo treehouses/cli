d40cbb7a495de510c6db5187057eb78acacfbe32
JLKwong <JLeeKwong@gmail.com>
2021-03-25 20:13:35 -0700

    internet reverse reorder and keep unique (fixes #2137)

--- modules/internet.sh
+++ modules/internet.sh
@@ -17,7 +17,7 @@ function internet {
     echo "$info" | grep -E '"(ip)"'
     echo "$info" | grep -E '"(org)"'
     echo "1"
-    echo "$info" | grep -E '"(country|city|postal)"'
+    echo "$info" | grep -E '"(country)"' | grep -E '"(city)"' | grep -E '"(postal)"'
     echo "2"
     if ! echo "$info" | grep -E 'postal'; then
       echo "$info" | grep -E '"(country|city)"' | tr '\n' ',' | sed 's/,$/, "postal": "n\/a"\n/' | sed 's/\",\"/\", \"/g'

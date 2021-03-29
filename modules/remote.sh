20e488d31d8e5a7b51388233f7659ab84526810a
JLKwong <JLeeKwong@gmail.com>
2021-03-29 10:48:41 -0700

    testing

--- modules/remote.sh
+++ modules/remote.sh
@@ -71,10 +71,10 @@ function remote {
       ;;
     "reverse")
       checkargn $# 2
+      reverse=$(internet reverse | sed -e 's#",\ "#"\n"#g' | cut -d'"' -f 2,3,4 | sed 's#\:[[:space:]]\"#:\"#g' | awk '!x[$0]++')
 	echo "  START REVERSE"
 	echo "$reverse"
 	echo "  END REVERSE"
-      reverse=$(internet reverse | sed -e 's#",\ "#"\n"#g' | cut -d'"' -f 2,3,4 | sed 's#\:[[:space:]]\"#:\"#g' | awk '!x[$0]++')
       while IFS= read -r line; do
         cmd_str+="\"$line\","
       done <<< "$reverse"

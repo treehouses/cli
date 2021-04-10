7ce67432a75fa25a505f17fda523b75e7ff7fed1
JLKwong <JLeeKwong@gmail.com>
2021-04-09 17:14:47 -0700

    debug debian compare versions

--- modules/changelog.sh
+++ modules/changelog.sh
@@ -31,7 +31,7 @@ case "$displaymode" in
         case "$version2" in
          "")
           checkargn $# 2
-          dpkg --compare-versions "$CURRENT" "gt" "$version1")
+          dpkg --compare-versions "$CURRENT" "gt" "$version1"
           compare=$(echo $?)
           if [[ $compare -eq 0 ]]; then
             sed "/^### $CURRENT/!d;s//&\n/;s/.*\n//;:a;/^### $version1/bb;\$!{n;ba};:b;s//\n&/;P;D" $LOGPATH #grabs text between version numbers, print bottom to top

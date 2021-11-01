9d3de3db39a970004daf4809ffbb10e7d4d459a3
dogi <dogi@users.noreply.github.com>
2021-10-31 21:26:27 -0400

    `treehouses detect` for Zero 2 W (fixes #2253) (#2257)

--- modules/detect.sh
+++ modules/detect.sh
@@ -51,11 +51,12 @@ function detectrpi {
   rpimodels["c03112"]="RPI4B" # 4gb
   rpimodels["c03114"]="RPI4B" # 4gb
   rpimodels["d03114"]="RPI4B" # 8gb
+  rpimodels["c03130"]="RPI400" # 4gb
   rpimodels["a03140"]="CM4" # 1gb
   rpimodels["b03140"]="CM4" # 2gb
   rpimodels["c03140"]="CM4" # 4gb
   rpimodels["d03140"]="CM4" # 8gb
-  rpimodels["c03130"]="RPI400" # 4gb
+  rpimodels["d03114"]="RPIZ2W" # .5gb
   # more at: https://www.raspberrypi.com/documentation/computers/raspberry-pi.html#new-style-revision-codes-in-use
 
   rpimodel=$(grep Revision /proc/cpuinfo | sed 's/.* //g' | tr -d '\n')
